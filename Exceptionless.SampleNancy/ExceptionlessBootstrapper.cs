3e600755fefd28da51fb4858621050f15c4d47e0
Luis Rudge <luis@luisrudge.net>
2014-02-27 00:55:07 -0300

    added nancy integration <3

---
+++ Exceptionless.SampleNancy/ExceptionlessBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+using Nancy.Bootstrapper;
+using Exceptionless;
+
+namespace Exceptionless.SampleNancy
+{
+    public class ExceptionlessBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            ExceptionlessClient.Current.RegisterNancy(pipelines);
+        }
+    }
+}
