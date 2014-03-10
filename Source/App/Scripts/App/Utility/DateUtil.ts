f8c03a583bcf8ddf9ec68f9098cf13a1bb2ba585
Blake Niemyjski <bniemyjski@gmail.com>
2014-03-10 15:04:59 -0500

    Fixed a bug where the date range chart selection tool would choose incorrect ranges in some scenarios.

--- Source/App/Scripts/App/Utility/DateUtil.ts
+++ Source/App/Scripts/App/Utility/DateUtil.ts
@@ -54,5 +54,37 @@ module exceptionless {
         public static formatWithFriendlyMobileMonthDayYear(moment: Moment): string {
             return moment.format(Constants.FRIENDLY_MOBILE_MONTH_DAY_YEAR_FORMAT);
         }
+
+        public static roundToPrevious15Minutes(moment: Moment): Moment {
+            var minutes = moment.minutes();
+            if (minutes < 15)
+                moment.minutes(0);
+            else if (minutes < 30)
+                moment.minutes(15);
+            else if (minutes < 45)
+                moment.minutes(30);
+            else
+                moment.minutes(45);
+
+            moment.seconds(0);
+
+            return moment;
+        }
+
+        public static roundToNext15Minutes(moment: Moment) : Moment {
+            var intervals = Math.floor(moment.minutes() / 15);
+            if (moment.minutes() % 15 != 0)
+                intervals++;
+
+            if (intervals == 4) {
+                moment.add('hours', 1);
+                intervals = 0;
+            }
+
+            moment.minutes(intervals * 15);
+            moment.seconds(0);
+
+            return moment;
+        }
     }
 }
