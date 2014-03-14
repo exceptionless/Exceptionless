ca8f7580bac55e31312db64a01371f539a52a5c1
Eric J. Smith <eric@codesmithtools.com>
2014-03-14 13:09:04 -0500

    Don't migrate dates that were created in UTC.

--- Source/Core/Migrations/v1.0.27.cs
+++ Source/Core/Migrations/v1.0.27.cs
@@ -32,6 +32,10 @@ namespace Exceptionless.Core.Migrations {
 
             var occurrenceDateArray = document.GetValue(ErrorRepository.FieldNames.OccurrenceDate).AsBsonArray;
             var localTicks = occurrenceDateArray[0].AsInt64;
+            var date = new DateTime(localTicks);
+            if (date > new DateTime(2014, 3, 14, 12, 30, 0))
+                return;
+
             var offset = TimeSpan.FromMinutes(occurrenceDateArray[1].AsInt32);
             occurrenceDateArray[0] = localTicks + -offset.Ticks;
 
