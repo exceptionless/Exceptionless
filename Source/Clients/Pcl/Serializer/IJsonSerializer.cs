808d46be15da508e1e47a31a7637874d02b7db9e
Blake Niemyjski <bniemyjski@gmail.com>
2014-04-07 12:08:50 -0500

    Replaced the SimpleJson serializer with json.net

--- Source/Clients/Pcl/Serializer/IJsonSerializer.cs
+++ Source/Clients/Pcl/Serializer/IJsonSerializer.cs
@@ -2,7 +2,7 @@
 
 namespace Exceptionless {
     public interface IJsonSerializer {
-        string Serialize(object model, string[] exclusions = null, int maxDepth = 5);
+        string Serialize(object model, string[] exclusions = null, int maxDepth = 5, bool continueOnSerializationError = true);
         object Deserialize(string json, Type type);
     }
 }
