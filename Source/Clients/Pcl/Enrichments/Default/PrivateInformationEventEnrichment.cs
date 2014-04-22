c0b4ec7970eed1441deec9a20d39a2c06e6cd7b3
Blake Niemyjski <bniemyjski@gmail.com>
2014-04-22 09:19:26 -0500

    Added default enrichments

---
+++ Source/Clients/Pcl/Enrichments/Default/PrivateInformationEventEnrichment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Exceptionless.Models;
+
+namespace Exceptionless.Enrichments.Default {
+    public class PrivateInformationEventEnrichment : IEventEnrichment {
+        /// <summary>
+        /// Enrich the event with additional information.
+        /// </summary>
+        /// <param name="context">Context information.</param>
+        /// <param name="ev">Event to enrich.</param>
+        public void Enrich(EventEnrichmentContext context, Event ev) {
+            if (context.Client.Configuration.IncludePrivateInformation)
+                ev.UserName = Environment.UserName;
+        }
+    }
+}
