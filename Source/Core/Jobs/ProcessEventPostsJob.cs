238dcc0c4301dc7beb0103a99b9c436811d797be
Eric J. Smith <eric@codesmithtools.com>
2014-06-15 18:36:41 -0500

    Remove unneeded pipeline action.

--- Source/Core/Jobs/ProcessEventPostsJob.cs
+++ Source/Core/Jobs/ProcessEventPostsJob.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using CodeSmith.Core.Scheduler;
 using Exceptionless.Core.AppStats;
