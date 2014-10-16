a4130ec0529015013bc5250ae5f93f9738a5cbb7
Blake Niemyjski <bniemyjski@gmail.com>
2014-10-16 09:27:43 -0500

    Added a time zone service

---
+++ Source/Web/components/time-zone/time-zone-service.js
@@ -0,0 +1,23 @@
+﻿/*global moment:false*/
+(function () {
+    'use strict';
+
+    angular.module('exceptionless.time-zone', [])
+        .factory('timeZoneService', [function () {
+            function getCurrentTimeZoneOffset() {
+                return new Date().getTimezoneOffset();
+            }
+
+            function getTimeZones() {
+                return moment.tz.names();
+            }
+
+
+            var service = {
+                getCurrentTimeZoneOffset: getCurrentTimeZoneOffset,
+                getTimeZones: getTimeZones
+            };
+
+            return service;
+        }]);
+}());
