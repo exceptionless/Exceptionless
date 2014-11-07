0c2df80e87d44921e2906f0ab0ebc8b3d4b78693
Blake Niemyjski <bniemyjski@gmail.com>
2014-11-07 17:59:44 -0600

    added a default grunt task.

--- Source/Web/Gruntfile.js
+++ Source/Web/Gruntfile.js
@@ -219,9 +219,10 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('build',['jshint', 'htmlangular', 'clean:before','less','dom_munger','ngtemplates','cssmin','concat','ngAnnotate','uglify','copy','htmlmin','clean:after']);
-  grunt.registerTask('serve', ['dom_munger:read','jshint', 'connect', 'watch']);
-  grunt.registerTask('test',['dom_munger:read','karma:all_tests']);
+  grunt.registerTask('build', ['jshint', 'htmlangular', 'clean:before', 'less', 'dom_munger', 'ngtemplates', 'cssmin', 'concat', 'ngAnnotate', 'uglify', 'copy', 'htmlmin', 'clean:after']);
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('serve', ['dom_munger:read', 'jshint', 'connect', 'watch']);
+  grunt.registerTask('test', ['dom_munger:read', 'karma:all_tests']);
 
   grunt.event.on('watch', function(action, filepath) {
     //https://github.com/gruntjs/grunt-contrib-watch/issues/156
