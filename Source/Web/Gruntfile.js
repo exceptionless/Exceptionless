c06b4b579addc87317241a3ae3ec5b81539a6566
Blake Niemyjski <bniemyjski@gmail.com>
2014-11-11 11:40:39 -0600

    Added the ability to automatically generate the app.config.js during the build process.

--- Source/Web/Gruntfile.js
+++ Source/Web/Gruntfile.js
@@ -8,32 +8,32 @@ var pkg = require('./package.json');
 //this method is used to create a set of inclusive patterns for all subdirectories
 //skipping node_modules, bower_components, dist, and any .dirs
 //This enables users to create any directory structure they desire.
-var createFolderGlobs = function(fileTypePatterns) {
+var createFolderGlobs = function (fileTypePatterns) {
   fileTypePatterns = Array.isArray(fileTypePatterns) ? fileTypePatterns : [fileTypePatterns];
-  var ignore = ['node_modules','bower_components','dist','temp'];
+  var ignore = ['node_modules', 'bower_components', 'dist', 'temp'];
   var fs = require('fs');
   return fs.readdirSync(process.cwd())
-          .map(function(file){
-            if (ignore.indexOf(file) !== -1 ||
-                file.indexOf('.') === 0 ||
-                !fs.lstatSync(file).isDirectory()) {
-              return null;
-            } else {
-              return fileTypePatterns.map(function(pattern) {
-                return file + '/**/' + pattern;
-              });
-            }
-          })
-          .filter(function(patterns){
-            return patterns;
-          })
-          .concat(fileTypePatterns);
+    .map(function (file) {
+      if (ignore.indexOf(file) !== -1 ||
+        file.indexOf('.') === 0 || !fs.lstatSync(file).isDirectory()) {
+        return null;
+      } else {
+        return fileTypePatterns.map(function (pattern) {
+          return file + '/**/' + pattern;
+        });
+      }
+    })
+    .filter(function (patterns) {
+      return patterns;
+    })
+    .concat(fileTypePatterns);
 };
 
 module.exports = function (grunt) {
 
   // load all grunt tasks
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-ng-constant');
   grunt.loadNpmTasks('grunt-html-angular-validate');
 
   // Project configuration.
@@ -48,53 +48,53 @@ module.exports = function (grunt) {
     watch: {
       main: {
         options: {
-            livereload: true,
-            livereloadOnError: false,
-            spawn: false
+          livereload: true,
+          livereloadOnError: false,
+          spawn: false
         },
-        files: [createFolderGlobs(['*.js','*.less','*.html']),'!_SpecRunner.html','!.grunt'],
+        files: [createFolderGlobs(['*.js', '*.less', '*.html']), '!_SpecRunner.html', '!.grunt'],
         tasks: [] //all the tasks are run dynamically during the watch event handler
       }
     },
     jshint: {
       main: {
         options: {
-            jshintrc: '.jshintrc'
+          jshintrc: '.jshintrc'
         },
         src: createFolderGlobs('*.js')
       }
     },
     htmlangular: {
-        main: {
-          options: {
-            customtags: ['events', 'progressbar', 'projects', 'rickshaw', 'search-filter', 'simple-stack-trace', 'stacks', 'stack-trace', 'summary', 'timeago', 'toaster-container'],
-            customattrs: [
-                'ui-*',
-                'checklist-*',
-                'refresh-*',
-                'typeahead',
-                'truncate',
-                'lines'
-            ],
-            tmplext: 'tpl.html',
-            reportpath: null,
-            relaxerror: [
-              'Element img is missing required attribute src.',
-              'Element tabset not allowed as child of element div in this context.',
-              'Element div not allowed as child of element pre in this context.'
-            ]
-          },
-          files: {
-            src: ['index.html', 'app/**/*.html', 'components/**/*.html']
-          }
+      main: {
+        options: {
+          customtags: ['events', 'progressbar', 'projects', 'rickshaw', 'search-filter', 'simple-stack-trace', 'stacks', 'stack-trace', 'summary', 'timeago', 'toaster-container'],
+          customattrs: [
+            'ui-*',
+            'checklist-*',
+            'refresh-*',
+            'typeahead',
+            'truncate',
+            'lines'
+          ],
+          tmplext: 'tpl.html',
+          reportpath: null,
+          relaxerror: [
+            'Element img is missing required attribute src.',
+            'Element tabset not allowed as child of element div in this context.',
+            'Element div not allowed as child of element pre in this context.'
+          ]
+        },
+        files: {
+          src: ['index.html', 'app/**/*.html', 'components/**/*.html']
         }
+      }
     },
     clean: {
-      before:{
-        src:['dist','temp']
+      before: {
+        src: ['dist', 'temp']
       },
       after: {
-        src:['temp']
+        src: ['temp']
       }
     },
     less: {
@@ -105,29 +105,44 @@ module.exports = function (grunt) {
         }
       }
     },
+    ngconstant: {
+      main: {
+        options: {
+          name: 'app.config',
+          space: '  ',
+          wrap: '(function () {\n  "use strict";\n\n  {%= __ngModule %}\n}());',
+          dest: 'dist/app.config.js',
+          constants: {
+            VERSION: process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : '2.0.0',
+            BASE_URL: process.env.API_URL ? process.env.API_URL : 'http://localhost:50000'
+          }
+        },
+        build: {}
+      }
+    },
     ngtemplates: {
       main: {
         options: {
-            module: 'app',
-            htmlmin:'<%= htmlmin.main.options %>'
+          module: 'app',
+          htmlmin: '<%= htmlmin.main.options %>'
         },
-        src: [createFolderGlobs('*.html'),'!index.html','!_SpecRunner.html'],
+        src: [createFolderGlobs('*.html'), '!index.html', '!_SpecRunner.html'],
         dest: 'temp/templates.js'
       }
     },
     copy: {
       main: {
         files: [
-          {src: ['*.png', '*.ico', 'img/**/{*.png,*.jpg,*.ico}', 'app.config.js'], dest: 'dist/'}
+          {src: ['*.png', '*.ico', 'img/**/{*.png,*.jpg,*.ico}'], dest: 'dist/'}
         ]
       }
     },
-    dom_munger:{
+    dom_munger: {
       read: {
         options: {
-          read:[
-            { selector:'script[data-concat!="false"]',attribute:'src', writeto:'appjs' },
-            { selector:'link[rel="stylesheet"][data-concat="true"]', attribute:'href', writeto:'appcss' }
+          read: [
+            {selector: 'script[data-concat!="false"]', attribute: 'src', writeto: 'appjs'},
+            {selector: 'link[rel="stylesheet"][data-concat="true"]', attribute: 'href', writeto: 'appcss'}
           ]
         },
         src: 'index.html'
@@ -136,23 +151,26 @@ module.exports = function (grunt) {
         options: {
           remove: ['script[data-remove!="false"]', 'link[data-remove="true"]'],
           append: [
-            { selector:'body', html:'<script src="app.min.js"></script><script src="app.config.js"></script>' },
-            { selector:'head', html:'<link rel="stylesheet" href="app.min.css">' }
+            {
+              selector: 'body',
+              html: '<script src="app.min.js"></script><script src="app.config.js"></script>'
+            },
+            {selector: 'head', html: '<link rel="stylesheet" href="app.min.css">'}
           ]
         },
-        src:'index.html',
+        src: 'index.html',
         dest: 'dist/index.html'
       }
     },
     cssmin: {
       main: {
-        src:['temp/app.css','<%= dom_munger.data.appcss %>'],
-        dest:'dist/app.min.css'
+        src: ['temp/app.css', '<%= dom_munger.data.appcss %>'],
+        dest: 'dist/app.min.css'
       }
     },
     concat: {
       main: {
-        src: ['<%= dom_munger.data.appjs %>','<%= ngtemplates.main.dest %>'],
+        src: ['<%= dom_munger.data.appjs %>', '<%= ngtemplates.main.dest %>'],
         dest: 'temp/app.full.js'
       }
     },
@@ -176,7 +194,7 @@ module.exports = function (grunt) {
       },
       main: {
         src: 'temp/app.full.js',
-        dest:'dist/app.min.js'
+        dest: 'dist/app.min.js'
       }
     },
     htmlmin: {
@@ -213,8 +231,8 @@ module.exports = function (grunt) {
         preprocessors: {
           'components/summary/**/*.html': 'ng-html2js'
         },
-        logLevel:'ERROR',
-        reporters:['mocha'],
+        logLevel: 'ERROR',
+        reporters: ['mocha'],
         autoWatch: false, //watching is handled by grunt-contrib-watch
         singleRun: true
       },
@@ -227,12 +245,12 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('build', ['jshint', 'htmlangular', 'clean:before', 'less', 'dom_munger', 'ngtemplates', 'cssmin', 'concat', 'ngAnnotate', 'uglify', 'copy', 'htmlmin', 'clean:after']);
+  grunt.registerTask('build', ['jshint', 'htmlangular', 'clean:before', 'less', 'dom_munger', 'ngconstant', 'ngtemplates', 'cssmin', 'concat', 'ngAnnotate', 'uglify', 'copy', 'htmlmin', 'clean:after']);
   grunt.registerTask('default', ['build']);
   grunt.registerTask('serve', ['dom_munger:read', 'jshint', 'connect', 'watch']);
   grunt.registerTask('test', ['dom_munger:read', 'karma:all_tests']);
 
-  grunt.event.on('watch', function(action, filepath) {
+  grunt.event.on('watch', function (action, filepath) {
     //https://github.com/gruntjs/grunt-contrib-watch/issues/156
 
     var tasksToRun = [];
@@ -251,7 +269,7 @@ module.exports = function (grunt) {
       //find the appropriate unit test for the changed file
       var spec = filepath;
       if (filepath.lastIndexOf('-spec.js') === -1 || filepath.lastIndexOf('-spec.js') !== filepath.length - 8) {
-        spec = filepath.substring(0,filepath.length - 3) + '-spec.js';
+        spec = filepath.substring(0, filepath.length - 3) + '-spec.js';
       }
 
       //if the spec exists then lets run it
@@ -270,6 +288,6 @@ module.exports = function (grunt) {
       tasksToRun.push('dom_munger:read');
     }
 
-    grunt.config('watch.main.tasks',tasksToRun);
+    grunt.config('watch.main.tasks', tasksToRun);
   });
 };
