4784a050f244702267408d63e9cebf46edf3b22e
Blake Niemyjski <bniemyjski@gmail.com>
2014-11-14 14:26:08 -0600

    Fixed the back button issue: https://github.com/angular-ui/ui-router/issues/1540#issuecomment-63105738

--- Source/Web/components/ui-loading-bar/ui-loading-bar-directive.js
+++ Source/Web/components/ui-loading-bar/ui-loading-bar-directive.js
@@ -2,14 +2,13 @@
   'use strict';
 
   angular.module('exceptionless.ui-loading-bar', [])
-    .directive('uiLoadingBar', ['$rootScope', '$location', '$anchorScroll', function ($rootScope, $location, $anchorScroll) {
+    .directive('uiLoadingBar', ['$rootScope', '$location', '$anchorScroll', function ($rootScope, $anchorScroll) {
       return {
         restrict: 'AC',
         template: '<span class="bar"></span>',
         link: function (scope, el, attrs) {
           el.addClass('loadingbar hide');
           scope.$on('$stateChangeStart', function (event) {
-            $location.hash('app');
             $anchorScroll();
             el.removeClass('hide').addClass('active');
           });
