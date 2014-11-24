a4fbaa989eae4367597b84c5dff798c2e3e05c6f
Eric J. Smith <eric@codesmithtools.com>
2014-11-24 11:46:32 -0600

    Add log message formatter.

---
+++ Source/Core/Plugins/Formatting/Default/60_LogFormattingPlugin.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using CodeSmith.Core.Component;
+using CodeSmith.Core.Extensions;
+using Exceptionless.Core.Extensions;
+using Exceptionless.Core.Mail.Models;
+using Exceptionless.Core.Models;
+using Exceptionless.Core.Queues.Models;
+using Exceptionless.Extensions;
+using Exceptionless.Models;
+using RazorSharpEmail;
+
+namespace Exceptionless.Core.Plugins.Formatting {
+    [Priority(60)]
+    public class LogFormattingPlugin : FormattingPluginBase {
+        private readonly IEmailGenerator _emailGenerator;
+
+        public LogFormattingPlugin(IEmailGenerator emailGenerator) {
+            _emailGenerator = emailGenerator;
+        }
+
+        private bool ShouldHandle(PersistentEvent ev) {
+            return ev.IsLog();
+        }
+
+        
+        public override SummaryData GetStackSummaryData(Stack stack) {
+            if (!stack.SignatureInfo.ContainsKeyWithValue("Type", Event.KnownTypes.NotFound))
+                return null;
+
+            return new SummaryData { TemplateKey = "stack-log-summary", Data = new { Title = stack.Title } };
+        }
+
+
+        public override string GetStackTitle(PersistentEvent ev) {
+            if (!ShouldHandle(ev))
+                return null;
+
+            return ev.Source;
+        }
+
+        public override SummaryData GetEventSummaryData(PersistentEvent ev) {
+            if (!ShouldHandle(ev))
+                return null;
+
+            return new SummaryData { TemplateKey = "event-log-summary", Data = new { Source = ev.Source } };
+        }
+
+        public override MailMessage GetEventNotificationMailMessage(EventNotification model) {
+            if (!ShouldHandle(model.Event))
+                return null;
+
+            string notificationType = "Log Message";
+            if (model.IsNew)
+                notificationType = "New Log Source";
+            else if (model.IsRegression)
+                notificationType = "Log Regression";
+
+            if (model.IsCritical)
+                notificationType = String.Concat("Critical ", notificationType.ToLower());
+
+           var requestInfo = model.Event.GetRequestInfo();
+            var mailerModel = new EventNotificationModel(model) {
+                BaseUrl = Settings.Current.BaseURL,
+                Subject = String.Concat(notificationType, ": ", model.Event.Source.Truncate(120)),
+                Url = requestInfo != null ? requestInfo.GetFullPath(true, true, true) : model.Event.Source
+            };
+
+            return _emailGenerator.GenerateMessage(mailerModel, "Notice").ToMailMessage();
+        }
+
+        public override string GetEventViewName(PersistentEvent ev) {
+            if (!ShouldHandle(ev))
+                return null;
+
+            return "Event-NotFound";
+        }
+    }
+}
