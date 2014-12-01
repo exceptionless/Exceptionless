6d6b9d489c0b46ae65dba0d7dd04be8a2d891c9b
Eric J. Smith <eric@codesmithtools.com>
2014-12-01 12:22:38 -0600

    Comment out redis queue tests for now.

--- Source/Api.Tests/Queue/RedisQueueTests.cs
+++ Source/Api.Tests/Queue/RedisQueueTests.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using Exceptionless.Core;
-using Exceptionless.Core.Queues;
-using StackExchange.Redis;
+﻿//using System;
+//using Exceptionless.Core;
+//using Exceptionless.Core.Queues;
+//using StackExchange.Redis;
 
-namespace Exceptionless.Api.Tests.Queue {
-    public class RedisQueueTests : InMemoryQueueTests {
-        private ConnectionMultiplexer _muxer;
+//namespace Exceptionless.Api.Tests.Queue {
+//    public class RedisQueueTests : InMemoryQueueTests {
+//        private ConnectionMultiplexer _muxer;
 
-        protected override IQueue<SimpleWorkItem> GetQueue(int retries, TimeSpan? workItemTimeout, TimeSpan? retryDelay) {
-            //if (!Settings.Current.UseAzureServiceBus)
-            //      return;
+//        protected override IQueue<SimpleWorkItem> GetQueue(int retries, TimeSpan? workItemTimeout, TimeSpan? retryDelay) {
+//            //if (!Settings.Current.UseAzureServiceBus)
+//            //      return;
 
-            if (_muxer == null)
-                _muxer = ConnectionMultiplexer.Connect(Settings.Current.RedisConnectionInfo.ToString());
+//            if (_muxer == null)
+//                _muxer = ConnectionMultiplexer.Connect(Settings.Current.RedisConnectionInfo.ToString());
 
-            return new RedisQueue<SimpleWorkItem>(_muxer, workItemTimeout: workItemTimeout, retries: 1);
-        }
-    }
-}
+//            return new RedisQueue<SimpleWorkItem>(_muxer, workItemTimeout: workItemTimeout, retries: 1);
+//        }
+//    }
+//}
