3bf9fd2d0cc583b89b8bcfd0bfdf1fc6e46c361b
Eric J. Smith <eric@codesmithtools.com>
2014-12-04 17:45:08 -0600

    Added default json controller attribute.

---
+++ Source/Api/Serialization/CamelCasedJsonAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web.Http.Controllers;
+using Newtonsoft.Json.Serialization;
+
+namespace Exceptionless.Api.Serialization {
+    public class DefaultJsonSettingsAttribute : Attribute, IControllerConfiguration {
+        public void Initialize(HttpControllerSettings controllerSettings, HttpControllerDescriptor controllerDescriptor) {
+            controllerSettings.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new DefaultContractResolver();
+        }
+    }
+}
