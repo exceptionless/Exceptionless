dc4775b853f4260b00a771d28a7b60650c1ac115
Blake Niemyjski <bniemyjski@gmail.com>
2015-01-26 14:51:01 -0600

    Revert "Updates for GeoIP Resolving"
    This reverts commit 911563f3714435c1867869a20bce4f049b85401f.

--- Source/Core/Geo/MindMaxGeoIPResolver.cs
+++ Source/Core/Geo/MindMaxGeoIPResolver.cs
@@ -9,12 +9,7 @@ namespace Exceptionless.Core.Geo {
         private readonly Lazy<DatabaseReader> _reader = new Lazy<DatabaseReader>(GetDatabase);
 
         public Location ResolveIp(string ip) {
-            if (String.IsNullOrWhiteSpace(ip) || (!ip.Contains(".") && !ip.Contains(":")))
-                return null;
-
-            ip = ip.Trim();
-
-            if (IsPrivateNetwork(ip))
+            if (String.IsNullOrWhiteSpace(ip) || (!ip.Contains(".") || !ip.Contains(":")))
                 return null;
 
             if (_reader.Value == null)
@@ -35,34 +30,14 @@ namespace Exceptionless.Core.Geo {
             }
         }
 
-        private bool IsPrivateNetwork(string ip) {
-            if (String.Equals(ip, "::1") || String.Equals(ip, "127.0.0.1"))
-                return true;
-
-            // 10.0.0.0 – 10.255.255.255 (Class A)
-            if (ip.StartsWith("10."))
-                return true;
-
-            // 172.16.00 – 172.31.255.255 (Class B)
-            if (ip.StartsWith("172.")) {
-                for (var range = 16; range < 32; range++) {
-                    if (ip.StartsWith("172." + range + "."))
-                        return true;
-                }
-            }
-
-            // 192.168.0.0 – 192.168.255.255 (Class C)
-            return ip.StartsWith("192.168.");
-        }
-
         private static DatabaseReader GetDatabase() {
-            if (String.IsNullOrEmpty(Settings.Current.GeoIPDatabasePath) || !File.Exists(Settings.Current.GeoIPDatabasePath)) {
+            if (String.IsNullOrEmpty(Settings.Current.GeoIPDatabase) || !File.Exists(Settings.Current.GeoIPDatabase)) {
                 Log.Warn().Message("No GeoIP database was found.").Write();
                 return null;
             }
 
             try {
-                return new DatabaseReader(Settings.Current.GeoIPDatabasePath, FileAccessMode.Memory);
+                return new DatabaseReader(Settings.Current.GeoIPDatabase, FileAccessMode.Memory);
             } catch (Exception ex) {
                 Log.Error().Exception(ex).Message("Unable to open GeoIP database.").Write();
             }
