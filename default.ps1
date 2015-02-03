9b86a853eeaf568356f680473eb0365dd9619470
Blake Niemyjski <bniemyjski@gmail.com>
2015-02-03 17:43:05 -0600

    added powershell script to download GeoIP database

--- default.ps1
+++ default.ps1
@@ -119,16 +119,6 @@ task BuildServer -depends Init {
     TeamCity-ReportBuildStart "Building Server" 
     exec { msbuild "$sln_file" /p:Configuration="$configuration" /p:Platform="Any CPU" /t:Rebuild }
 	TeamCity-ReportBuildFinish "Finished building Server"
-	
-    TeamCity-ReportBuildStart "Downloading MaxMind database" 
-	
-	$MaxMindDBPath = "$source_dir\Api.IIS\App_Data" 
-	Create-Directory $MaxMindDBPath
-	Start-BitsTransfer -Source "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz" -Destination $MaxMindDBPath
-	Expand-GZip "$MaxMindDBPath\GeoLite2-City.mmdb.gz"
-	Remove-Item -Path "$MaxMindDBPath\GeoLite2-City.mmdb.gz"
-	
-    TeamCity-ReportBuildStart "Finished downloading MaxMind database" 
 }
 
 task Build -depends BuildClient, BuildServer
@@ -258,6 +248,8 @@ task PackageServer -depends TestServer {
 
     robocopy "$source_dir\Api.IIS\App_Data\JobRunner\jobs" "$source_dir\Api.IIS\App_Data\jobs" /MOVE /E /NP
 
+    robocopy "$base_dir" "$source_dir\Api.IIS" DownloadGeoIPDatabase.ps1
+	
     TeamCity-ReportBuildProgress "Building Server NuGet Package: Exceptionless.Api"
     exec { & $base_dir\nuget\NuGet.exe pack "$source_dir\Api.IIS\Exceptionless.Api.nuspec" -OutputDirectory $packageDir -Version $nuget_version -NoPackageAnalysis }
 }
@@ -330,30 +322,4 @@ Function Get-PackagePath ([string] $packageName) {
                         Select-Object -First 1
 
     Return "$packagePath"
-}
-
-Function Expand-GZip ([string]$Source, [string]$OutputPath = ($Source -Replace "\.gz$", "")) {
-	If (!(Test-Path -Path $Source -PathType Leaf)) {
-        Write-Error -Message "$Source does not exist"
-        Return
-    }
-
-    $input = New-Object System.IO.FileStream $Source, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read);
-    $output = New-Object System.IO.FileStream $OutputPath, ([IO.FileMode]::CreateNew), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
-    $gzipStream = New-Object System.IO.Compression.GzipStream $input, ([IO.Compression.CompressionMode]::Decompress)
-    
-	Try {
-        $buffer = New-Object byte[](1024);
-        While ($true) {
-            $read = $gzipStream.Read($buffer, 0, 1024)
-            If ($read -le 0) {
-                Break;
-            }
-            $output.Write($buffer, 0, $read)
-        }
-    } Finally {
-        $gzipStream.Close();
-        $output.Close();
-        $input.Close();
-    }
 }
