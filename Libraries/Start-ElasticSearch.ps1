1bf01bf3b124f061f3505a587fed11518119613d
Eric J. Smith <eric@codesmithtools.com>
2015-02-22 18:13:09 -0600

    Update packages including foundatio.

---
+++ Libraries/Start-ElasticSearch.ps1
@@ -0,0 +1,10 @@
+﻿$env:ElasticSearch_Version = "1.4.4"
+
+if(!(Test-Path -Path "elasticsearch-$env:ElasticSearch_Version" )){
+    wget "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$env:ElasticSearch_Version.zip" -OutFile "elasticsearch-$env:ElasticSearch_Version.zip"
+    7z x "elasticsearch-$env:ElasticSearch_Version.zip"
+    rm "elasticsearch-$env:ElasticSearch_Version.zip"
+}
+
+elasticsearch-1.4.4\bin\service.bat install
+elasticsearch-1.4.4\bin\service.bat start
