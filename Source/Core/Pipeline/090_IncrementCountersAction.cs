b91c1bf09ef719361ce497f760950af11b4e33f4
Blake Niemyjski <bniemyjski@gmail.com>
2015-02-24 11:58:27 -0600

    Increment the stats counters in bulk

--- Source/Core/Pipeline/090_IncrementCountersAction.cs
+++ Source/Core/Pipeline/090_IncrementCountersAction.cs
@@ -10,6 +10,8 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Exceptionless.Core.AppStats;
 using Exceptionless.Core.Billing;
 using Exceptionless.Core.Plugins.EventProcessor;
@@ -26,10 +28,13 @@ namespace Exceptionless.Core.Pipeline {
 
         protected override bool ContinueOnError { get { return true; } }
 
-        public override void Process(EventContext ctx) {
-            _stats.Counter(MetricNames.EventsProcessed);
-            if (ctx.Organization.PlanId != BillingManager.FreePlan.Id)
-                _stats.Counter(MetricNames.EventsPaidProcessed);
+        public override void ProcessBatch(ICollection<EventContext> contexts) {
+            _stats.Counter(MetricNames.EventsProcessed, contexts.Count);
+
+            if (contexts.First().Organization.PlanId != BillingManager.FreePlan.Id)
+                _stats.Counter(MetricNames.EventsPaidProcessed, contexts.Count);
         }
+
+        public override void Process(EventContext ctx) {}
     }
 }
