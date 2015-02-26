41c135b9a6007202646b7e22e073ca3a271606c3
Blake Niemyjski <bniemyjski@gmail.com>
2015-02-26 17:57:42 -0600

    Reverted a previous change. This allows us to control all logging through a central point in case everything goes south.

--- Source/Api/Utility/ExceptionLoggerContext.cs
+++ Source/Api/Utility/ExceptionLoggerContext.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 using System.Web.Http.ExceptionHandling;
-using Exceptionless.Enrichments;
+using Logger = NLog.Fluent.Log;
 
 namespace Exceptionless.Api.Utility {
-    public class ExceptionlessExceptionLogger : ExceptionLogger {
-        private readonly ExceptionlessClient _exceptionlessClient;
-
-        public ExceptionlessExceptionLogger(ExceptionlessClient exceptionlessClient) {
-            _exceptionlessClient = exceptionlessClient;
-        }
-
+    public class NLogExceptionLogger : ExceptionLogger {
         public override void Log(ExceptionLoggerContext context) {
-            var contextData = new ContextData();
-            contextData.MarkAsUnhandledError();
-            contextData.SetSubmissionMethod("ExceptionLogger");
-            contextData.Add("HttpActionContext", context.ExceptionContext.ActionContext);
-
-           context.Exception.ToExceptionless(contextData, _exceptionlessClient).Submit();
+            string loggerName = context.Exception.TargetSite != null && context.Exception.TargetSite.DeclaringType != null ? context.Exception.TargetSite.DeclaringType.Name : "NLogExceptionLogger";
+            Logger.Error().Exception(context.Exception).Message("Unhandled error: {0}", context.Exception.Message).LoggerName(loggerName).Write();
         }
     }
 }
