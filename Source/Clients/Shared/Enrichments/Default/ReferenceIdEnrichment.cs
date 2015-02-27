574cae213860c7c12efe5c54fad8d2098677483f
Blake Niemyjski <bniemyjski@gmail.com>
2015-02-27 16:18:34 -0600

    Don't use the reference id enrichment if it's already been set

--- Source/Clients/Shared/Enrichments/Default/ReferenceIdEnrichment.cs
+++ Source/Clients/Shared/Enrichments/Default/ReferenceIdEnrichment.cs
@@ -4,7 +4,7 @@ using Exceptionless.Models;
 namespace Exceptionless.Enrichments.Default {
     public class ReferenceIdEnrichment : IEventEnrichment {
         public void Enrich(EventEnrichmentContext context, Event ev) {
-            if (ev.Type != Event.KnownTypes.Error)
+            if (!String.IsNullOrEmpty(ev.ReferenceId) || ev.Type != Event.KnownTypes.Error)
                 return;
 
             ev.ReferenceId = Guid.NewGuid().ToString("N").Substring(0, 10);
