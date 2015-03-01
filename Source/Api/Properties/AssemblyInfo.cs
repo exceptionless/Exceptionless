c127d1d5faea3bf29034b1fbfb6cfe060b86ca81
Eric J. Smith <eric@codesmithtools.com>
2015-02-28 22:26:32 -0600

    Merge Api and Api.IIS projects to simplify.

--- Source/Api/Properties/AssemblyInfo.cs
+++ Source/Api/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Exceptionless.Api")]
 [assembly: AssemblyDescription("Exceptionless.Api")]
 [assembly: ComVisible(false)]
-[assembly: Guid("96a7146d-7e14-47ba-9801-b0d0dff42b46")]
+[assembly: Guid("ae1c7511-49b2-45b1-8cfc-6dce4e2c142d")]
