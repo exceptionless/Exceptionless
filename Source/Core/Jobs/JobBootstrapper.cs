fa2d8bafaef21daa92a820210b0a03ec418abd14
Eric J. Smith <eric@codesmithtools.com>
2015-03-02 12:53:34 -0600

    Add insulation bootstrapper to jobs.

--- Source/Core/Jobs/JobBootstrapper.cs
+++ Source/Core/Jobs/JobBootstrapper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Reflection;
 using Exceptionless.Core.Extensions;
 using Exceptionless.Core.Utility;
 using Foundatio.ServiceProvider;
+using NLog.Fluent;
 using SimpleInjector;
 
 namespace Exceptionless.Core.Jobs {
@@ -12,6 +14,16 @@ namespace Exceptionless.Core.Jobs {
             container.Options.PropertySelectionBehavior = new InjectAttributePropertySelectionBehavior();
 
             container.RegisterPackage<Bootstrapper>();
+
+            Assembly insulationAssembly = null;
+            try {
+                insulationAssembly = Assembly.Load("Exceptionless.Insulation");
+            } catch (Exception ex) {
+                Log.Error().Message("Unable to load the insulation asssembly.").Exception(ex).Write();
+            }
+
+            if (insulationAssembly != null)
+                container.RegisterPackages(new[] { insulationAssembly });
 
             return container;
         }
