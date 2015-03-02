ff5c84270948cbc4905d2091dc556a095f1e2749
Eric J. Smith <eric@codesmithtools.com>
2015-03-01 22:40:03 -0600

    Try a new way to run services.

--- Libraries/Start-ElasticSearch.ps1
+++ Libraries/Start-ElasticSearch.ps1
@@ -3,15 +3,14 @@ if ($env:ES_VERSION) {
 	$es_version = $env:ES_VERSION
 }
 
-if (!(Test-Path -Path "elasticsearch-$es_version" )) {
+if (!(Test-Path -Path "elasticsearch-$es_version") -And !(Test-Path -Path "elasticsearch.zip")) {
     wget "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$es_version.zip" -OutFile "elasticsearch.zip"
 }
+
 if (Test-Path -Path "elasticsearch.zip") {
     7z x -y "elasticsearch.zip" > $null
     cp .\elasticsearch.yml .\elasticsearch-$es_version\config -Force
 	rm elasticsearch.zip
 }
 
-$service = ".\elasticsearch-$es_version\bin\service.bat"
-&$service install
-&$service start
+Start-Process -NoNewWindow "$(Get-Location)\elasticsearch-$es_version\bin\elasticsearch.bat"
---
+++ Libraries/Start-MongoDb.ps1
@@ -0,0 +1,26 @@
+$mongo_version = "2.6.8"
+if ($env:MONGO_VERSION) {
+	$mongo_version = $env:MONGO_VERSION
+}
+
+if (!(Test-Path -Path "MongoDB") -And !(Test-Path -Path "mongodb.msi")) {
+    wget "https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-$mongo_version-signed.msi" -OutFile 'mongodb.msi'
+}
+
+if (Test-Path -Path "mongodb.msi") {
+    $list = 
+    @(
+        "/I `"$(Get-Location)\mongodb.msi`"",
+        "/QN",
+        "/L*V `"$(Get-Location)\mongodb-install.log`"",
+        "INSTALLLOCATION=`"$(Get-Location)\MongoDB`""
+    )
+
+    Start-Process -FilePath "msiexec" -ArgumentList $list -Wait
+	rm .\mongodb.msi
+    if (!(Test-Path -Path "$(Get-Location)\MongoDB\data")) {
+        mkdir "$(Get-Location)\MongoDB\data"
+    }
+}
+
+Start-Process -NoNewWindow "$(Get-Location)\MongoDB\bin\mongod.exe" -ArgumentList "-dbpath $(Get-Location)\MongoDB\data"
