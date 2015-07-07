cf982340d855091747814e63cca92b49dfbd1c8b
Blake Niemyjski <bniemyjski@gmail.com>
2015-07-07 17:35:57 -0500

    Added the ability to read "some" mail settings from environmental variables #115
    @ejsmith This will cover our use case, but can you review this.. There
    really isn't a good solution right now as vnext doesn't support this...
    https://github.com/dotnet/corefx/issues/1006

--- Source/Core/Mail/SmtpMailSender.cs
+++ Source/Core/Mail/SmtpMailSender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,10 +13,17 @@ namespace Exceptionless.Core.Mail {
         public long SentCount { get { return _messagesSent; } }
 
         public async Task SendAsync(MailMessage model) {
-            var client = new SmtpClient();
             var message = model.ToMailMessage();
             message.Headers.Add("X-Mailer-Machine", Environment.MachineName);
             message.Headers.Add("X-Mailer-Date", DateTime.Now.ToString());
+
+            var client = new SmtpClient();
+            if (!String.IsNullOrEmpty(Settings.Current.SmtpHost)) {
+                client.Host = Settings.Current.SmtpHost;
+                client.Port = Settings.Current.SmtpPort;
+                client.EnableSsl = Settings.Current.SmtpEnableSsl;
+                client.Credentials = new NetworkCredential(Settings.Current.SmtpUser, Settings.Current.SmtpPassword);
+            }
 
             await client.SendMailAsync(message);
 
