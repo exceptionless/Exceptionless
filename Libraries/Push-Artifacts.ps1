73b81a2fbf5c7b1c0a4c3889037e7b8b4d692c4e
Blake Niemyjski <bniemyjski@gmail.com>
2015-07-18 13:22:28 -0500

    Updated text

--- Libraries/Push-Artifacts.ps1
+++ Libraries/Push-Artifacts.ps1
@@ -45,10 +45,10 @@ If ($LastExitCode -ne 0) {
     Return $LastExitCode
 }
 
-Write-Host "Removing existing files."
+Write-Host "Removing existing files..."
 git rm -r * -q 2>&1 | %{ "$_" }
 
-Write-Host "Copying build artifacts."
+Write-Host "Copying build artifacts..."
 ROBOCOPY "$sourceDir\Api" "$artifactsDir" /XD "$sourceDir\Api\obj" "$sourceDir\Api\App_Data" /S /XF "*.nuspec" "*.settings" "*.cs" "packages.config" "*.csproj" "*.user" "*.suo" "*.xsd" "*.ide" /NFL /NDL /NJH /NJS /nc /ns /np
 ROBOCOPY "$artifactsDir\bin" "$artifactsDir\App_Data\JobRunner\bin\" /S /NFL /NDL /NJH /NJS /nc /ns /np
 Copy-Item -Path "$base_dir\packages\Foundatio.*\tools\Job.exe" -Destination "$artifactsDir\App_Data\JobRunner\" 
@@ -63,7 +63,7 @@ ROBOCOPY "$sourceDir\Migrations\EventMigration\bin\Release" "$artifactsDir\App_D
 ROBOCOPY "$sourceDir\WebJobs\continuous" "$artifactsDir\App_Data\jobs\continuous" /S /NFL /NDL /NJH /NJS /nc /ns /np
 ROBOCOPY "$sourceDir\WebJobs\triggered" "$artifactsDir\App_Data\jobs\triggered" /S /NFL /NDL /NJH /NJS /nc /ns /np
 
-Write-Host "Committing the latest changes...."
+Write-Host "Committing the latest changes..."
 git add * 2>&1 | %{ "$_" }
 git commit -a -m "Build: $env:APPVEYOR_BUILD_VERSION $($env:APPVEYOR_REPO_NAME)@$($env:APPVEYOR_REPO_COMMIT)" -q 2>&1 | %{ "$_" }
 git push origin "$env:APPVEYOR_REPO_BRANCH" -q 2>&1 | %{ "$_" }
