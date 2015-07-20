fdd9d8c01f95665960e580e25d9cd1a498ad0865
ejsmith <eric@codesmithtools.com>
2015-07-20 17:09:48 -0500

    Add reading settings from config file.

--- Source/Core/Utility/SettingsBase.cs
+++ Source/Core/Utility/SettingsBase.cs
@@ -2,15 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using Exceptionless.Core.Extensions;
+using Newtonsoft.Json;
 
 namespace Exceptionless.Core {
-    public abstract class SettingsBase<T>: SingletonBase<T>, IInitializable where T: class {
-        private static Dictionary<string, string> _environmentVariables;
+    public abstract class SettingsBase<T> : SingletonBase<T>, IInitializable where T : class {
+        public abstract void Initialize();
 
         protected static bool GetBool(string name, bool defaultValue = false) {
-            string value = GetEnvironmentalVariable(name);
+            string value = GetEnvironmentVariable(name) ?? GetConfigVariable(name);
             if (String.IsNullOrEmpty(value))
                 return ConfigurationManager.AppSettings.GetBool(name, defaultValue);
 
@@ -18,17 +20,17 @@ namespace Exceptionless.Core {
             return Boolean.TryParse(value, out boolean) ? boolean : defaultValue;
         }
 
-        protected static string GetConnectionString(string name) {
-            string value = GetEnvironmentalVariable(name);
+        protected static string GetConnectionString(string name, string defaultValue = null) {
+            string value = GetEnvironmentVariable(name) ?? GetConfigVariable(name);
             if (!String.IsNullOrEmpty(value))
                 return value;
 
             var connectionString = ConfigurationManager.ConnectionStrings[name];
-            return connectionString != null ? connectionString.ConnectionString : null;
+            return connectionString != null ? connectionString.ConnectionString : defaultValue;
         }
 
         protected static TEnum GetEnum<TEnum>(string name, TEnum? defaultValue = null) where TEnum : struct {
-            string value = GetEnvironmentalVariable(name);
+            string value = GetEnvironmentVariable(name) ?? GetConfigVariable(name);
             if (String.IsNullOrEmpty(value))
                 return ConfigurationManager.AppSettings.GetEnum(name, defaultValue);
 
@@ -44,7 +46,7 @@ namespace Exceptionless.Core {
         }
 
         protected static int GetInt(string name, int defaultValue = 0) {
-            string value = GetEnvironmentalVariable(name);
+            string value = GetEnvironmentVariable(name) ?? GetConfigVariable(name);
             if (String.IsNullOrEmpty(value))
                 return ConfigurationManager.AppSettings.GetInt(name, defaultValue);
 
@@ -52,12 +54,12 @@ namespace Exceptionless.Core {
             return Int32.TryParse(value, out number) ? number : defaultValue;
         }
 
-        protected static string GetString(string name) {
-            return GetEnvironmentalVariable(name) ?? ConfigurationManager.AppSettings[name];
+        protected static string GetString(string name, string defaultValue = null) {
+            return GetEnvironmentVariable(name) ?? GetConfigVariable(name) ?? ConfigurationManager.AppSettings[name] ?? defaultValue;
         }
 
         protected static List<string> GetStringList(string name, string defaultValues = null, char[] separators = null) {
-            string value = GetEnvironmentalVariable(name);
+            string value = GetEnvironmentVariable(name) ?? GetConfigVariable(name);
             if (String.IsNullOrEmpty(value))
                 return ConfigurationManager.AppSettings.GetStringList(name, defaultValues, separators);
 
@@ -67,10 +69,35 @@ namespace Exceptionless.Core {
             return value.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToList();
         }
 
-        private static string GetEnvironmentalVariable(string name) {
+        private static Dictionary<string, string> _configVariables;
+        protected static string GetConfigVariable(string name) {
             if (String.IsNullOrEmpty(name))
                 return null;
-            
+
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin", "config.json");
+            // check to see if environment specific config exists and use that instead
+            if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\", "config.json")))
+                configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\", "config.json");
+
+            if (!File.Exists(configPath))
+                return null;
+
+            if (_configVariables == null)
+                _configVariables = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(configPath));
+
+            if (!_configVariables.ContainsKey(name))
+                return null;
+
+            return _configVariables[name];
+        }
+
+        protected static string EnvironmentVariablePrefix { get; set; }
+
+        private static Dictionary<string, string> _environmentVariables;
+        private static string GetEnvironmentVariable(string name) {
+            if (String.IsNullOrEmpty(name))
+                return null;
+
             if (_environmentVariables == null) {
                 try {
                     _environmentVariables = Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().ToDictionary(e => e.Key.ToString(), e => e.Value.ToString());
@@ -81,15 +108,11 @@ namespace Exceptionless.Core {
                     return null;
                 }
             }
-            
+
             if (!_environmentVariables.ContainsKey(EnvironmentVariablePrefix + name))
                 return null;
 
             return _environmentVariables[EnvironmentVariablePrefix + name];
         }
-
-        protected static string EnvironmentVariablePrefix { get; set; }
-
-        public abstract void Initialize();
     }
 }
