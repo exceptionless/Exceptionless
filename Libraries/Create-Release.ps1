70bc47e04c5527962e44947a5a0c1cbaec6cea6b
Blake Niemyjski <bniemyjski@gmail.com>
2015-07-23 15:49:43 -0500

    Added a readme to the artifacts release zip

--- Libraries/Create-Release.ps1
+++ Libraries/Create-Release.ps1
@@ -45,6 +45,7 @@ Copy-Item -Path "$base_dir\Libraries\Start-ElasticSearch.ps1" -Destination $rele
 Copy-Item -Path "$base_dir\Libraries\elasticsearch.yml" -Destination $releaseTempDir
 Copy-Item -Path "$base_dir\Libraries\Start-Website.ps1" -Destination $releaseTempDir
 "PowerShell .\Start-Elasticsearch.ps1`r`nPowerShell .\Start-Website.ps1" | Out-File "$releaseTempDir\Start.bat" -Encoding "ascii"
+Copy-Item -Path "$base_dir\Libraries\readme.txt" -Destination $releaseTempDir
 
 Write-Host "Merging configuration"
 $webConfig = "$releaseTempDir\wwwroot\web.config"
