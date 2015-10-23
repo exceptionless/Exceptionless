d684dca79517619428bae9163afcbaff43776528
Blake Niemyjski <bniemyjski@gmail.com>
2015-10-23 16:24:40 -0500

    Removed the base calls as they weren't doing anything

--- Source/Api/Hubs/MessageBusConnection.cs
+++ Source/Api/Hubs/MessageBusConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Exceptionless.Core.Component;
 using Exceptionless.Core.Extensions;
 using Foundatio.Logging;
 using Microsoft.AspNet.SignalR;
@@ -18,7 +19,6 @@ namespace Exceptionless.Api.Hubs {
                     await Groups.Add(connectionId, organizationId);
 
                 _userIdConnections.Add(request.User.GetUserId(), connectionId);
-                await base.OnConnected(request, connectionId);
             } catch (Exception ex) {
                 Logger.Error().Exception(ex).Message($"OnConnected Error: {ex.Message}").Tag("SignalR").Write();
                 throw;
@@ -28,12 +28,12 @@ namespace Exceptionless.Api.Hubs {
         protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled) {
             try {
                 _userIdConnections.Remove(request.User.GetUserId(), connectionId);
-
-                return base.OnDisconnected(request, connectionId, stopCalled);
             } catch (Exception ex) {
                 Logger.Error().Exception(ex).Message($"OnDisconnected Error: {ex.Message}").Tag("SignalR").Write();
                 throw;
             }
+
+            return TaskHelper.Completed();
         }
 
         protected override async Task OnReconnected(IRequest request, string connectionId) {
@@ -43,8 +43,6 @@ namespace Exceptionless.Api.Hubs {
 
                 if (!_userIdConnections.GetConnections(request.User.GetUserId()).Contains(connectionId))
                     _userIdConnections.Add(request.User.GetUserId(), connectionId);
-
-                await base.OnReconnected(request, connectionId);
             } catch (Exception ex) {
                 Logger.Error().Exception(ex).Message($"OnReconnected Error: {ex.Message}").Tag("SignalR").Write();
                 throw;
