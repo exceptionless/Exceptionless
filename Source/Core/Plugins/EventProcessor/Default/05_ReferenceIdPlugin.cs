d0d6ad0ae132f3f06015d45899d6dc9ddaae05c7
Blake Niemyjski <bniemyjski@gmail.com>
2015-10-26 14:51:35 -0500

    #144 Enable reliable submission scenarios using ReferenceId

---
+++ Source/Core/Plugins/EventProcessor/Default/05_ReferenceIdPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Exceptionless.Core.Component;
+using Exceptionless.Core.Extensions;
+using Exceptionless.Core.Pipeline;
+using Foundatio.Caching;
+
+namespace Exceptionless.Core.Plugins.EventProcessor {
+    [Priority(5)]
+    public class ReferenceIdPlugin : EventProcessorPluginBase {
+        private readonly ICacheClient _cacheClient;
+
+        public ReferenceIdPlugin(ICacheClient cacheClient) {
+            _cacheClient = cacheClient;
+        }
+
+        public override async Task EventProcessingAsync(EventContext context) {
+            if (String.IsNullOrEmpty(context.Event.ReferenceId))
+                return;
+
+            string key = String.Concat(context.Project.Id, ":", context.Event.ReferenceId);
+            if (!await _cacheClient.AddAsync(key, true, TimeSpan.FromMinutes(1)).AnyContext())
+                context.IsCancelled = true;
+        }
+
+        public override Task EventProcessedAsync(EventContext context) {
+            if (String.IsNullOrEmpty(context.Event.ReferenceId))
+                return TaskHelper.Completed();
+
+            string key = String.Concat(context.Project.Id, ":", context.Event.ReferenceId);
+            return _cacheClient.SetAsync(key, true, TimeSpan.FromHours(12));
+        }
+    }
+}
