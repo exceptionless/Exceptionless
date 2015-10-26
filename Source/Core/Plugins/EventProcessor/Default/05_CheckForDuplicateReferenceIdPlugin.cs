deef061dc5eb133e594be91f69286a5d15d98ef1
Blake Niemyjski <bniemyjski@gmail.com>
2015-10-26 16:22:56 -0500

    Some refactoring to Duplicate reference id plugin

--- Source/Core/Plugins/EventProcessor/Default/05_CheckForDuplicateReferenceIdPlugin.cs
+++ Source/Core/Plugins/EventProcessor/Default/05_CheckForDuplicateReferenceIdPlugin.cs
@@ -7,10 +7,10 @@ using Foundatio.Caching;
 
 namespace Exceptionless.Core.Plugins.EventProcessor {
     [Priority(5)]
-    public class ReferenceIdPlugin : EventProcessorPluginBase {
+    public class CheckForDuplicateReferenceIdPlugin : EventProcessorPluginBase {
         private readonly ICacheClient _cacheClient;
 
-        public ReferenceIdPlugin(ICacheClient cacheClient) {
+        public CheckForDuplicateReferenceIdPlugin(ICacheClient cacheClient) {
             _cacheClient = cacheClient;
         }
 
@@ -18,17 +18,20 @@ namespace Exceptionless.Core.Plugins.EventProcessor {
             if (String.IsNullOrEmpty(context.Event.ReferenceId))
                 return;
 
-            string key = String.Concat(context.Project.Id, ":", context.Event.ReferenceId);
-            if (!await _cacheClient.AddAsync(key, true, TimeSpan.FromMinutes(1)).AnyContext())
+            // TODO: Look into using a lock on reference id so we can ensure there is no race conditions with setting keys
+            if (!await _cacheClient.AddAsync(GetCacheKey(context), true, TimeSpan.FromMinutes(1)).AnyContext())
                 context.IsCancelled = true;
         }
-
+        
         public override Task EventProcessedAsync(EventContext context) {
             if (String.IsNullOrEmpty(context.Event.ReferenceId))
                 return TaskHelper.Completed();
-
-            string key = String.Concat(context.Project.Id, ":", context.Event.ReferenceId);
-            return _cacheClient.SetAsync(key, true, TimeSpan.FromHours(12));
+            
+            return _cacheClient.SetAsync(GetCacheKey(context), true, TimeSpan.FromDays(1));
+        }
+        
+        private string GetCacheKey(EventContext context) {
+            return String.Concat("project-", context.Project.Id, ":", context.Event.ReferenceId);
         }
     }
 }
