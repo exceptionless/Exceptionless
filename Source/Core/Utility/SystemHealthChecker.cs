25c4f6fb4804f05af3b2ead1dfa1969e7c4491dc
Blake Niemyjski <bniemyjski@gmail.com>
2015-12-09 16:03:28 -0600

    Added Locking around queue and messagebus checks

--- Source/Core/Utility/SystemHealthChecker.cs
+++ Source/Core/Utility/SystemHealthChecker.cs
@@ -5,6 +5,7 @@ using Exceptionless.Core.Extensions;
 using Exceptionless.Core.Queues.Models;
 using Foundatio.Caching;
 using Foundatio.Extensions;
+using Foundatio.Lock;
 using Foundatio.Logging;
 using Foundatio.Messaging;
 using Foundatio.Queues;
@@ -17,18 +18,21 @@ namespace Exceptionless.Core.Utility {
         private readonly ICacheClient _cacheClient;
         private readonly IElasticClient _elasticClient;
         private readonly IFileStorage _storage;
+        private readonly ILockProvider _locker;
         private readonly IQueue<StatusMessage> _queue;
         private readonly IMessageBus _messageBus;
         private readonly AsyncManualResetEvent _resetEvent = new AsyncManualResetEvent(false);
 
         public SystemHealthChecker(ICacheClient cacheClient, IElasticClient elasticClient, IFileStorage storage, IQueue<StatusMessage> queue, IMessageBus messageBus) {
-            _cacheClient = cacheClient;
+            _cacheClient = new ScopedCacheClient(cacheClient, "health");
             _elasticClient = elasticClient;
             _storage = storage;
             _queue = queue;
-            _messageBus = messageBus;
 
+            _messageBus = messageBus;
             _messageBus.Subscribe<StatusMessage>(m => _resetEvent.Set());
+            
+            _locker = new CacheLockProvider(_cacheClient, _messageBus);
         }
 
         public async Task<HealthCheckResult> CheckCacheAsync() {
@@ -83,51 +87,61 @@ namespace Exceptionless.Core.Utility {
         }
 
         public async Task<HealthCheckResult> CheckQueueAsync() {
-            var message = new StatusMessage { Id = Guid.NewGuid().ToString() };
+            using (await _locker.AcquireAsync("health")) {
+                var message = new StatusMessage {
+                    Id = Guid.NewGuid().ToString()
+                };
 
-            var sw = Stopwatch.StartNew();
-            var swTotal = Stopwatch.StartNew();
-            try {
-                await _queue.EnqueueAsync(message).AnyContext();
-                Logger.Info().Message($"Check Queue: EnqueueAsync took {sw.ElapsedMilliseconds}ms").Write();
+                var sw = Stopwatch.StartNew();
+                var swTotal = Stopwatch.StartNew();
+                try {
+                    string id = await _queue.EnqueueAsync(message).AnyContext();
+                    Logger.Info().Message($"Check Queue: EnqueueAsync {id} took {sw.ElapsedMilliseconds}ms").Write();
 
-                sw.Restart();
-                var queueStats = await _queue.GetQueueStatsAsync().AnyContext();
-                Logger.Info().Message($"Check Queue: GetQueueStatsAsync took {sw.ElapsedMilliseconds}ms").Write();
-                if (queueStats.Enqueued == 0)
-                    return HealthCheckResult.NotHealthy("Queue Not Working: No items were enqueued.");
+                    sw.Restart();
+                    var queueStats = await _queue.GetQueueStatsAsync().AnyContext();
+                    Logger.Info().Message($"Check Queue: GetQueueStatsAsync {id} took {sw.ElapsedMilliseconds}ms").Write();
+                    if (queueStats.Enqueued == 0)
+                        return HealthCheckResult.NotHealthy("Queue Not Working: No items were enqueued.");
 
-                sw.Restart();
-                var workItem = await _queue.DequeueAsync().AnyContext();
-                Logger.Info().Message($"Check Queue: DequeueAsync took {sw.ElapsedMilliseconds}ms").Write();
-                if (workItem == null)
-                    return HealthCheckResult.NotHealthy("Queue Not Working: No items could be dequeued.");
+                    sw.Restart();
+                    var workItem = await _queue.DequeueAsync().AnyContext();
+                    Logger.Info().Message($"Check Queue: DequeueAsync {id} took {sw.ElapsedMilliseconds}ms").Write();
 
-                sw.Restart();
-                await workItem.CompleteAsync().AnyContext();
-                Logger.Info().Message($"Check Queue: CompleteAsync took {sw.ElapsedMilliseconds}ms").Write();
-            } catch (Exception ex) {
-                return HealthCheckResult.NotHealthy("Queue Not Working: " + ex.Message);
-            } finally {
-                swTotal.Stop();
-                Logger.Info().Message($"Checking queue took {swTotal.ElapsedMilliseconds}ms").Write();
+                    if (workItem == null)
+                        return HealthCheckResult.NotHealthy("Queue Not Working: No items could be dequeued.");
+
+                    sw.Restart();
+                    await workItem.CompleteAsync().AnyContext();
+                    Logger.Info().Message($"Check Queue: CompleteAsync  {id} took {sw.ElapsedMilliseconds}ms").Write();
+
+                    await _queue.DeleteQueueAsync().AnyContext();
+                } catch (Exception ex) {
+                    return HealthCheckResult.NotHealthy("Queue Not Working: " + ex.Message);
+                } finally {
+                    swTotal.Stop();
+                    Logger.Info().Message($"Checking queue took {swTotal.ElapsedMilliseconds}ms").Write();
+                }
             }
 
             return HealthCheckResult.Healthy;
         }
 
          public async Task<HealthCheckResult> CheckMessageBusAsync() {
-            var message = new StatusMessage { Id = Guid.NewGuid().ToString() };
+            using (await _locker.AcquireAsync("health")) {
+                var message = new StatusMessage { Id = Guid.NewGuid().ToString() };
 
-            var sw = Stopwatch.StartNew();
-            try {
-                await _messageBus.PublishAsync(message).AnyContext();
-                await Task.WhenAny(_resetEvent.WaitAsync(), TimeSpan.FromSeconds(2).ToCancellationToken().AsTask()).AnyContext();
-            } catch (Exception ex) {
-                return HealthCheckResult.NotHealthy("MessageBus Not Working: " + ex.Message);
-            } finally {
-                sw.Stop();
-                Logger.Info().Message($"Checking MessageBus took {sw.ElapsedMilliseconds}ms").Write();
+                var sw = Stopwatch.StartNew();
+                try {
+                    await _messageBus.PublishAsync(message).AnyContext();
+                    await Task.WhenAny(_resetEvent.WaitAsync(), TimeSpan.FromSeconds(2).ToCancellationToken().AsTask()).AnyContext();
+                } catch (Exception ex) {
+                    return HealthCheckResult.NotHealthy("MessageBus Not Working: " + ex.Message);
+                } finally {
+                    sw.Stop();
+                    Logger.Info().Message($"Checking MessageBus took {sw.ElapsedMilliseconds}ms").Write();
+                    _resetEvent.Reset();
+                }
             }
 
             return HealthCheckResult.Healthy;
