4ba189f51fd76d9287ee22fb602a2d7e4866fa3d
Blake Niemyjski <bniemyjski@gmail.com>
2016-01-27 14:03:58 -0600

    Updated Query String parameter binding to support different scenarios

--- Source/Api/Utility/QueryStringParametersParameterBinding.cs
+++ Source/Api/Utility/QueryStringParametersParameterBinding.cs
@@ -7,22 +7,33 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Metadata;
-using Foundatio.Extensions;
 
 namespace Exceptionless.Api.Utility {
     public class QueryStringParametersParameterBinding : HttpParameterBinding {
+        private static readonly HashSet<string> _ignoredKeys = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "access_token", "api_key", "apikey" };
+
         public QueryStringParametersParameterBinding(HttpParameterDescriptor descriptor) : base(descriptor) { }
 
         public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken) {
             var binding = actionContext.ActionDescriptor.ActionBinding;
 
             var parameterBinding = binding.ParameterBindings.FirstOrDefault(b => b.Descriptor.ParameterBinderAttribute is QueryStringParametersAttribute);
-            if (parameterBinding == null || !(typeof(IDictionary<string, string>).IsAssignableFrom(parameterBinding.Descriptor.ParameterType)))
+            if (parameterBinding == null || !(typeof(IDictionary<string, string[]>).IsAssignableFrom(parameterBinding.Descriptor.ParameterType)))
                 return Task.FromResult(0);
 
-            var parameters = new Dictionary<string, string>();
-            parameters.AddRange(actionContext.Request.GetQueryNameValuePairs().Where(kvp => !kvp.Key.Equals("access_token", StringComparison.OrdinalIgnoreCase)));
-            SetValue(actionContext, parameters);
+            var parameters = new Dictionary<string, List<string>>();
+            foreach (var pair in actionContext.Request.GetQueryNameValuePairs()) {
+                if (_ignoredKeys.Contains(pair.Key))
+                    continue;
+
+                string value = pair.Value != null ? Uri.UnescapeDataString(pair.Value) : null;
+                if (parameters.ContainsKey(pair.Key))
+                    parameters[pair.Key].Add(value);
+                else
+                    parameters.Add(pair.Key, new List<string> { value });
+            }
+
+            SetValue(actionContext, parameters.ToDictionary(k => k.Key, v => v.Value.ToArray()));
 
             return Task.FromResult(0);
         }
