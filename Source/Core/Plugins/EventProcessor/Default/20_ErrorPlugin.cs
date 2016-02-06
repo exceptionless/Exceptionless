157cda016b8c178a5035c8eba82efc55d0b4566b
Adam Zolotarev <eugenezolo@gmail.com>
2016-02-05 20:17:05 -0500

    Added ManualStackingPluging to allow custom stacking; still process ErrorPlugin, since it adds message and rarget info, but don't add stack signature

--- Source/Core/Plugins/EventProcessor/Default/20_ErrorPlugin.cs
+++ Source/Core/Plugins/EventProcessor/Default/20_ErrorPlugin.cs
@@ -9,8 +9,7 @@ using Exceptionless.Core.Models.Data;
 namespace Exceptionless.Core.Plugins.EventProcessor {
     [Priority(20)]
     public class ErrorPlugin : EventProcessorPluginBase {
-        public override Task EventProcessingAsync(EventContext context)
-        {
+        public override Task EventProcessingAsync(EventContext context) {
             if (!context.Event.IsError())
                 return Task.CompletedTask;
 
@@ -19,7 +18,7 @@ namespace Exceptionless.Core.Plugins.EventProcessor {
                 return Task.CompletedTask;
 
             if (String.IsNullOrWhiteSpace(context.Event.Message))
-                context.Event.Message = error.Message;
+                context.Event.Message = error.Message;            
 
             string[] commonUserMethods = { "DataContext.SubmitChanges", "Entities.SaveChanges" };
             if (context.HasProperty("CommonMethods"))
@@ -29,7 +28,6 @@ namespace Exceptionless.Core.Plugins.EventProcessor {
             if (context.HasProperty("UserNamespaces"))
                 userNamespaces = context.GetProperty<string>("UserNamespaces").SplitAndTrim(',');
 
-            AddManualStackingKeyToSignatureIfExists(context);
             var signature = new ErrorSignature(error, userCommonMethods: commonUserMethods, userNamespaces: userNamespaces);
             if (signature.SignatureInfo.Count <= 0)
                 return Task.CompletedTask;
@@ -38,24 +36,16 @@ namespace Exceptionless.Core.Plugins.EventProcessor {
             var stackingTarget = error.GetStackingTarget();
             if (stackingTarget?.Error?.StackTrace != null && stackingTarget.Error.StackTrace.Count > 0 && !targetInfo.ContainsKey("Message"))
                 targetInfo["Message"] = stackingTarget.Error.Message;
-                        
-            if(context.StackSignatureData.Count > 0)
+
+            error.Data[Error.KnownDataKeys.TargetInfo] = targetInfo;
+
+            if (context.StackSignatureData.Count > 0)
                 return Task.CompletedTask;
-            
+
             foreach (var key in signature.SignatureInfo.Keys)
                 context.StackSignatureData.Add(key, signature.SignatureInfo[key]);
-            
-            return Task.CompletedTask;
-        }
 
-        private void AddManualStackingKeyToSignatureIfExists(EventContext context)
-        {
-            var data = context.Event.Data;
-            if (!data.ContainsKey(Event.KnownDataKeys.ManualStackingKey) 
-                || data[Event.KnownDataKeys.ManualStackingKey] == null)
-                return;
-            
-            context.StackSignatureData.AddItemIfNotEmpty(nameof(Event.KnownDataKeys.ManualStackingKey), data[Event.KnownDataKeys.ManualStackingKey].ToString());
+            return Task.CompletedTask;
         }
     }
 }
