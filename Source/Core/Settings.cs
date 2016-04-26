6f0d03a1807233c93c72f25f8bb556d04157667f
Nate Hitze <nate@nhitze.com>
2016-04-25 22:15:07 -0400

    Added settings

--- Source/Core/Settings.cs
+++ Source/Core/Settings.cs
@@ -56,6 +56,10 @@ namespace Exceptionless.Core {
 
         public bool EnableElasticsearchTracing { get; private set; }
 
+		public string LdapConnectionString { get; private set; }
+
+		public bool EnableLdapAuth { get; private set; }
+
         public bool EnableSignalR { get; private set; }
 
         public string Version { get; private set; }
@@ -170,6 +174,9 @@ namespace Exceptionless.Core {
 
             RedisConnectionString = GetConnectionString(nameof(RedisConnectionString));
             EnableRedis = GetBool(nameof(EnableRedis), !String.IsNullOrEmpty(RedisConnectionString));
+
+	        LdapConnectionString = GetConnectionString(nameof(LdapConnectionString));
+	        EnableLdapAuth = GetBool(nameof(EnableLdapAuth));
 
             EnableSignalR = GetBool(nameof(EnableSignalR), true);
 
