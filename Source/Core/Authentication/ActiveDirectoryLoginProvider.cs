9680c3a8a12ec685d31540eaf4d6869a403ce146
Nate Hitze <nate@nhitze.com>
2016-05-13 01:03:20 -0400

    First pass at AD auth

---
+++ Source/Core/Authentication/ActiveDirectoryLoginProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace Exceptionless.Core.Authentication
+{
+	public class ActiveDirectoryLoginProvider : IDomainLoginProvider {
+		private const string AD_EMAIL = "mail";
+		private const string AD_FIRSTNAME = "givenName";
+		private const string AD_LASTNAME = "sn";
+		private const string AD_USERNAME = "distinguishedName";
+
+		public bool IsLoginValid(string username, string password) {
+			using (DirectoryEntry de = new DirectoryEntry(Settings.Current.LdapConnectionString, username, password, AuthenticationTypes.Secure)) {
+				using (DirectorySearcher ds = new DirectorySearcher(de, "(&(sAMAccountName=" + username + "))", new[] { AD_USERNAME })) {
+					try {
+						SearchResult result = ds.FindOne();
+						return result != null;
+					}
+					// Catch "username and password are invalid"
+					catch (DirectoryServicesCOMException ex) when (ex.ErrorCode == -2147023570) {
+						return false;
+					}
+				}
+			}
+		}
+
+		public string GetEmailForLogin(string username) {
+			SearchResult searchResult = LookupUser(username);
+
+			return searchResult.Properties[AD_EMAIL][0].ToString();
+		}
+
+		public string GetNameForLogin(string username) {
+			SearchResult searchResult = LookupUser(username);
+
+			return searchResult.Properties[AD_FIRSTNAME][0] + " " + searchResult.Properties[AD_LASTNAME];
+		}
+
+		private SearchResult LookupUser(string username) {
+			using (DirectoryEntry entry = new DirectoryEntry(Settings.Current.LdapConnectionString)) {
+				using (DirectorySearcher searcher = new DirectorySearcher(entry, $"(&(sAMAccountName={username}))", new[] { AD_FIRSTNAME, AD_LASTNAME, AD_EMAIL })) {
+					return searcher.FindOne();
+				}
+			}
+		}
+	}
+}
--- Source/Tests/Controllers/AuthControllerTests.cs
+++ Source/Tests/Controllers/AuthControllerTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Exceptionless.Api.Controllers;
 using Exceptionless.Api.Models;
+using Exceptionless.Api.Tests.Authentication;
 using Exceptionless.Api.Tests.Utility;
 using Exceptionless.Core;
 using Exceptionless.Core.Extensions;
@@ -32,6 +33,9 @@ namespace Exceptionless.Api.Tests.Controllers {
         private readonly IOrganizationRepository _organizationRepository = IoC.GetInstance<IOrganizationRepository>();
         private readonly IProjectRepository _projectRepository = IoC.GetInstance<IProjectRepository>();
 
+	    private const string SIGNUP_ENDPOINT = "signup";
+	    private const string LOGIN_ENDPOINT = "login";
+
         public AuthControllerTests(ITestOutputHelper output) {
             _output = output;
         }
@@ -41,7 +45,7 @@ namespace Exceptionless.Api.Tests.Controllers {
             await ResetAsync();
 
             try {
-                _authController.Request = CreateRequestMessage(null, false, false);
+                _authController.Request = CreateRequestMessage(SIGNUP_ENDPOINT, null, false, false);
                 Settings.Current.EnableAccountCreation = false;
 
                 var signupModel = new SignupModel {
@@ -69,7 +73,7 @@ namespace Exceptionless.Api.Tests.Controllers {
             await ResetAsync();
 
             try {
-                _authController.Request = CreateRequestMessage(null, false, false);
+                _authController.Request = CreateRequestMessage(SIGNUP_ENDPOINT, null, false, false);
                 Settings.Current.EnableAccountCreation = false;
 
                 var signupModel = new SignupModel {
@@ -97,7 +101,7 @@ namespace Exceptionless.Api.Tests.Controllers {
             await ResetAsync();
 
             try {
-                _authController.Request = CreateRequestMessage(null, false, false);
+                _authController.Request = CreateRequestMessage(SIGNUP_ENDPOINT, null, false, false);
                 Settings.Current.EnableAccountCreation = false;
 
                 var orgs = await _organizationRepository.GetAllAsync();
@@ -138,11 +142,11 @@ namespace Exceptionless.Api.Tests.Controllers {
         }
 
         [Fact]
-        public async Task CanSignupWhenAccountCreationEnabledWithNoToken() {
+        public async Task CanSignupWhenAccountCreationEnabledWithNoTokenAsync() {
             await ResetAsync();
 
             try {
-                _authController.Request = CreateRequestMessage(null, false, false);
+                _authController.Request = CreateRequestMessage(SIGNUP_ENDPOINT, null, false, false);
                 Settings.Current.EnableAccountCreation = true;
 
                 var signupModel = new SignupModel {
@@ -169,11 +173,11 @@ namespace Exceptionless.Api.Tests.Controllers {
         }
 
         [Fact]
-        public async Task CanSignupWhenAccountCreationEnabledWithValidToken() {
+        public async Task CanSignupWhenAccountCreationEnabledWithValidTokenAsync() {
             await ResetAsync();
 
             try {
-                _authController.Request = CreateRequestMessage(null, false, false);
+                _authController.Request = CreateRequestMessage(SIGNUP_ENDPOINT, null, false, false);
                 Settings.Current.EnableAccountCreation = true;
 
                 var orgs = await _organizationRepository.GetAllAsync();
@@ -213,14 +217,349 @@ namespace Exceptionless.Api.Tests.Controllers {
             }
         }
 
-        private HttpRequestMessage CreateRequestMessage(ClaimsPrincipal user, bool isCompressed, bool isJson, string charset = "utf-8") {
+	    [Fact]
+	    public async Task LoginValidAsync() {
+		    await ResetAsync();
+
+		    try {
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+			    Settings.Current.EnableActiveDirectoryAuth = false;
+
+				// add user
+			    const string email = "test6@exceptionless.io";
+			    const string password = "Test6 password";
+			    const string salt = "1234567890123456";
+			    string passwordHash = password.ToSaltedHash(salt);
+				var user = new User {
+				    EmailAddress = email,
+				    Password = passwordHash,
+				    Salt = salt,
+					IsEmailAddressVerified = true,
+					FullName = "User 6"
+			    };
+			    await _userRepository.AddAsync(user);
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// create model
+				var loginModel = new LoginModel {
+				    Email = email,
+				    Password = password
+			    };
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.True(result.IsSuccessStatusCode, "Status Code is failure.");
+				Assert.Equal(System.Net.HttpStatusCode.OK, result.StatusCode);
+				var tokenResult = GetResult<TokenResult>(result);
+				Assert.NotNull(tokenResult);
+				Assert.False(string.IsNullOrEmpty(tokenResult.Token));
+			} finally {
+			    await ResetAsync();
+		    }
+	    }
+
+		[Fact]
+		public async Task LoginInvalidPasswordAsync()
+		{
+			await ResetAsync();
+
+			try
+			{
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+				Settings.Current.EnableActiveDirectoryAuth = false;
+
+				// add user
+				const string email = "test7@exceptionless.io";
+				const string password = "Test7 password";
+				const string salt = "1234567890123456";
+				string passwordHash = password.ToSaltedHash(salt);
+				var user = new User
+				{
+					EmailAddress = email,
+					Password = passwordHash,
+					Salt = salt,
+					IsEmailAddressVerified = true,
+					FullName = "User 7"
+				};
+				await _userRepository.AddAsync(user);
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// create model
+				var loginModel = new LoginModel
+				{
+					Email = email,
+					Password = "This password ain't right"
+				};
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.Equal(System.Net.HttpStatusCode.Unauthorized, result.StatusCode);
+			}
+			finally
+			{
+				await ResetAsync();
+			}
+		}
+
+		[Fact]
+		public async Task LoginNoSuchUserAsync()
+		{
+			await ResetAsync();
+
+			try
+			{
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+				Settings.Current.EnableActiveDirectoryAuth = false;
+
+				// add user
+				const string email = "test8@exceptionless.io";
+				const string password = "Test8 password";
+				const string salt = "1234567890123456";
+				string passwordHash = password.ToSaltedHash(salt);
+				var user = new User
+				{
+					EmailAddress = email,
+					Password = passwordHash,
+					Salt = salt,
+					IsEmailAddressVerified = true,
+					FullName = "User 8"
+				};
+				await _userRepository.AddAsync(user);
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// create model
+				var loginModel = new LoginModel
+				{
+					Email = "Thisguydoesntexist@exceptionless.io",
+					Password = "This password ain't right"
+				};
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.Equal(System.Net.HttpStatusCode.Unauthorized, result.StatusCode);
+			}
+			finally
+			{
+				await ResetAsync();
+			}
+		}
+
+		[Fact]
+		public async Task LoginValidExistingActiveDirectoryAsync()
+		{
+			await ResetAsync();
+
+			try
+			{
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+				Settings.Current.EnableActiveDirectoryAuth = true;
+
+				// add user
+				var provider = new TestDomainLoginProvider();
+				string email = provider.GetEmailForLogin(TestDomainLoginProvider.ValidUsername);
+				var user = new User
+				{
+					EmailAddress = email,
+					IsEmailAddressVerified = true,
+					FullName = "User 6",
+					IsFromActiveDirectory = true
+				};
+				await _userRepository.AddAsync(user);
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// create model
+				var loginModel = new LoginModel
+				{
+					Email = TestDomainLoginProvider.ValidUsername,
+					Password = TestDomainLoginProvider.ValidPassword
+				};
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.True(result.IsSuccessStatusCode, "Status Code is failure.");
+				Assert.Equal(System.Net.HttpStatusCode.OK, result.StatusCode);
+				var tokenResult = GetResult<TokenResult>(result);
+				Assert.NotNull(tokenResult);
+				Assert.False(string.IsNullOrEmpty(tokenResult.Token));
+			}
+			finally
+			{
+				await ResetAsync();
+			}
+		}
+
+		[Fact]
+		public async Task LoginValidNonExistantActiveDirectoryAsync()
+		{
+			await ResetAsync();
+
+			try
+			{
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+				Settings.Current.EnableActiveDirectoryAuth = true;
+
+				// create model
+				var loginModel = new LoginModel
+				{
+					Email = TestDomainLoginProvider.ValidUsername,
+					Password = TestDomainLoginProvider.ValidPassword
+				};
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.True(result.IsSuccessStatusCode, "Status Code is failure.");
+				Assert.Equal(System.Net.HttpStatusCode.OK, result.StatusCode);
+				var tokenResult = GetResult<TokenResult>(result);
+				Assert.NotNull(tokenResult);
+				Assert.False(string.IsNullOrEmpty(tokenResult.Token));
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// Verify that a user account was added
+				var provider = new TestDomainLoginProvider();
+				string email = provider.GetEmailForLogin(TestDomainLoginProvider.ValidUsername);
+				var user = await _userRepository.GetByEmailAddressAsync(email);
+				Assert.NotNull(user);
+				Assert.Equal(provider.GetNameForLogin(TestDomainLoginProvider.ValidUsername), user.FullName);
+				Assert.Equal(email, user.EmailAddress);
+				Assert.True(user.IsFromActiveDirectory);
+			}
+			finally
+			{
+				await ResetAsync();
+			}
+		}
+
+		[Fact]
+		public async Task LoginInvalidNonExistantActiveDirectoryAsync()
+		{
+			await ResetAsync();
+
+			try
+			{
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+				Settings.Current.EnableActiveDirectoryAuth = true;
+
+				// create model
+				var loginModel = new LoginModel
+				{
+					Email = TestDomainLoginProvider.ValidUsername + ".au",
+					Password = "Totallywrongpassword1234"
+				};
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.Equal(System.Net.HttpStatusCode.Unauthorized, result.StatusCode);
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// Verify that a user account was not added
+				var provider = new TestDomainLoginProvider();
+				string email = provider.GetEmailForLogin(TestDomainLoginProvider.ValidUsername);
+				var user = await _userRepository.GetByEmailAddressAsync(email + ".au");
+				Assert.Null(user);
+			}
+			finally
+			{
+				await ResetAsync();
+			}
+		}
+
+		[Fact]
+		public async Task LoginInvalidExistingActiveDirectoryAsync()
+		{
+			await ResetAsync();
+
+			try
+			{
+				_authController.Request = CreateRequestMessage(LOGIN_ENDPOINT, null, false, false);
+				Settings.Current.EnableActiveDirectoryAuth = true;
+
+				// add user
+				var provider = new TestDomainLoginProvider();
+				string email = provider.GetEmailForLogin(TestDomainLoginProvider.ValidUsername);
+				var user = new User
+				{
+					EmailAddress = email,
+					IsEmailAddressVerified = true,
+					FullName = "User 6",
+					IsFromActiveDirectory = true
+				};
+				await _userRepository.AddAsync(user);
+
+				await _client.RefreshAsync(r => r.Force());
+
+				// create model
+				var loginModel = new LoginModel
+				{
+					Email = TestDomainLoginProvider.ValidUsername,
+					Password = "Totallywrongpassword1234"
+				};
+
+				var actionResult = await _authController.LoginAsync(loginModel);
+				var cancellation = new CancellationToken();
+				var result = await actionResult.ExecuteAsync(cancellation);
+				var error = GetResult<HttpError>(result);
+				if (error != null)
+				{
+					_output.WriteLine("Error: {0}", error.Message);
+				}
+				Assert.Equal(System.Net.HttpStatusCode.Unauthorized, result.StatusCode);
+			}
+			finally
+			{
+				await ResetAsync();
+			}
+		}
+
+		private HttpRequestMessage CreateRequestMessage(string endpoint, ClaimsPrincipal user, bool isCompressed, bool isJson, string charset = "utf-8") {
             var request = new HttpRequestMessage();
 
             var context = new OwinContext();
             context.Request.User = Thread.CurrentPrincipal = user;
             request.SetOwinContext(context);
             request.SetConfiguration(new HttpConfiguration());
-            request.Content = new HttpMessageContent(new HttpRequestMessage(HttpMethod.Post, "/api/auth/signup"));
+            request.Content = new HttpMessageContent(new HttpRequestMessage(HttpMethod.Post, $"/api/auth/{endpoint}"));
             if (isCompressed)
                 request.Content.Headers.ContentEncoding.Add("gzip");
             request.Content.Headers.ContentType.MediaType = isJson ? "application/json" : "text/plain";
@@ -237,7 +576,7 @@ namespace Exceptionless.Api.Tests.Controllers {
         }
 
         private static T GetResult<T>(HttpResponseMessage message) where T : class {
-            var content = message.Content as System.Net.Http.ObjectContent<T>;
+            var content = message.Content as ObjectContent<T>;
             if (content == null)
                 return null;
             var result = content.Value as T;
