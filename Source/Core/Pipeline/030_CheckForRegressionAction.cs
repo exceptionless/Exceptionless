1d0d7fc42c758a82001f2e6d490d5215afa2900a
Blake Niemyjski <bniemyjski@gmail.com>
2016-07-11 16:11:55 -0500

    WIP - Updated Regresion pipeline action to check the semantic version!

--- Source/Core/Pipeline/030_CheckForRegressionAction.cs
+++ Source/Core/Pipeline/030_CheckForRegressionAction.cs
@@ -9,6 +9,7 @@ using Exceptionless.Core.Repositories;
 using Foundatio.Jobs;
 using Foundatio.Logging;
 using Foundatio.Queues;
+using McSherry.SemanticVersioning;
 
 namespace Exceptionless.Core.Pipeline {
     [Priority(30)]
@@ -23,23 +24,41 @@ namespace Exceptionless.Core.Pipeline {
         }
 
         public override async Task ProcessBatchAsync(ICollection<EventContext> contexts) {
-            var stacks = contexts.Where(c => c.Stack?.DateFixed != null && c.Stack.DateFixed.Value < c.Event.Date.UtcDateTime).GroupBy(c => c.Event.StackId);
+            var stacks = contexts.Where(c => !c.Stack.IsRegressed && c.Stack.FixedInVersion != null).OrderBy(c => c.Event.Date).GroupBy(c => c.Event.StackId);
             foreach (var stackGroup in stacks) {
                 try {
-                    var context = stackGroup.First();
-                    _logger.Trace("Marking stack and events as regression.");
-                    await _stackRepository.MarkAsRegressedAsync(context.Stack.Id).AnyContext();
-                    await _workItemQueue.EnqueueAsync(new StackWorkItem { OrganizationId = context.Event.OrganizationId, StackId = context.Stack.Id, UpdateIsFixed = true, IsFixed = false }).AnyContext();
-                    await _stackRepository.InvalidateCacheAsync(context.Event.ProjectId, context.Event.StackId, context.SignatureHash).AnyContext();
+                    var stack = stackGroup.First().Stack;
+                    var fixedInVersion = GetSemanticVersion(stack.FixedInVersion);
 
-                    bool isFirstEvent = true;
+                    SemanticVersion regressedVersion = null;
+                    EventContext regressedContext = null;
+                    // Group all stack events by version.
+                    var versions = stackGroup.GroupBy(c => c.Event.GetVersion());
+                    foreach (var versionGroup in versions) {
+                        var version = GetSemanticVersion(versionGroup.Key);
+                        if (version == null || version <= fixedInVersion)
+                            continue;
+
+                        if (regressedVersion != null && regressedVersion >= version)
+                            continue;
+
+                        regressedVersion = version;
+                        regressedContext = versionGroup.First();
+                    }
+
+                    if (regressedVersion == null)
+                        return;
+
+                    _logger.Trace("Marking stack and events as regressed in version: {version}", regressedVersion);
+                    await _stackRepository.MarkAsRegressedAsync(stack.Id).AnyContext();
+                    await _workItemQueue.EnqueueAsync(new StackWorkItem { OrganizationId = stack.OrganizationId, StackId = stack.Id, UpdateIsFixed = true, IsFixed = false }).AnyContext();
+                    
                     foreach (var ctx in stackGroup) {
                         ctx.Event.IsFixed = false;
-
-                        // Only mark the first event context as regressed.
-                        ctx.IsRegression = isFirstEvent;
-                        isFirstEvent = false;
+                        ctx.IsRegression = false;
                     }
+                    
+                    regressedContext.IsRegression = true;
                 } catch (Exception ex) {
                     foreach (var context in stackGroup) {
                         bool cont = false;
@@ -56,6 +75,17 @@ namespace Exceptionless.Core.Pipeline {
 
         public override Task ProcessAsync(EventContext ctx) {
             return Task.CompletedTask;
+        }
+        
+        private SemanticVersion GetSemanticVersion(string version) {
+            if (String.IsNullOrEmpty(version))
+                return null;
+
+            SemanticVersion semanticVersion;
+            if (SemanticVersion.TryParse(version, out semanticVersion))
+                return semanticVersion;
+
+            return null;
         }
     }
 }
