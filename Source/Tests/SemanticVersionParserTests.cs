83c9626323e2299bc806d1a3de478ef4ae7d3bf9
Blake Niemyjski <bniemyjski@gmail.com>
2016-07-15 23:56:41 -0500

    Updated the semantic version parser to handle more cases.

--- Source/Tests/SemanticVersionParserTests.cs
+++ Source/Tests/SemanticVersionParserTests.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using Exceptionless.Core.Utility;
 using Foundatio.Logging.Xunit;
-using McSherry.SemanticVersioning;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -15,9 +14,9 @@ namespace Exceptionless.Api.Tests {
         
         [Theory]
         [InlineData(null, null)]
-        [InlineData("1", null)]
+        [InlineData("1", "1.0.0")]
         [InlineData("1.2", "1.2.0")]
-        [InlineData("1.2 7ab3b4da18", null)]
+        [InlineData("1.2 7ab3b4da18", "1.2.0")]
         [InlineData("1.2.3", "1.2.3")]
         [InlineData("1.2.3 7ab3b4da18", "1.2.3")]
         [InlineData("1.2.3-beta2", "1.2.3-beta2")]
