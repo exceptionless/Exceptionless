3c343c2346fa5fb94ddf46739d7dd3b54f835d82
Blake Niemyjski <bniemyjski@gmail.com>
2016-09-16 20:49:43 -0500

    Added new elasticsearch.prod.yml & ability to not drop old indexes

--- Source/Core/Repositories/Configuration/Indexes/EventIndex.cs
+++ Source/Core/Repositories/Configuration/Indexes/EventIndex.cs
@@ -9,6 +9,7 @@ namespace Exceptionless.Core.Repositories.Configuration {
         public EventIndex(IElasticConfiguration configuration) : base(configuration, Settings.Current.AppScopePrefix + "events", 1) {
             DateFormat = "yyyyMM";
             MaxIndexAge = TimeSpan.FromDays(180);
+            DiscardExpiredIndexes = false;
 
             AddType(Event = new EventIndexType(this));
             AddAlias($"{Name}-today", TimeSpan.FromDays(1));
