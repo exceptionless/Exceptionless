1a8ff41991aaf56e714f31303b65fe299c899899
Blake Niemyjski <bniemyjski@gmail.com>
2016-10-10 21:36:54 -0500

    Fixed an issue with the push artifacts script

--- build/Push-Artifacts.ps1
+++ build/Push-Artifacts.ps1
@@ -36,7 +36,7 @@ Write-Host "Removing existing files..."
 git rm -r * -q 2>&1 | %{ "$_" }
 
 Write-Host "Copying build artifacts..."
-ROBOCOPY "$sourceDir\Api" "$artifactsDir" /XD "$sourceDir\Exceptionless.Api\obj" "$sourceDir\Exceptionless.Api\App_Data" /S /XF "*.nuspec" "*.settings" "*.cs" "packages.config" "*.csproj" "*.user" "*.suo" "*.xsd" "*.ide" /NFL /NDL /NJH /NJS /nc /ns /np
+ROBOCOPY "$sourceDir\Exceptionless.Api" "$artifactsDir" /XD "$sourceDir\Exceptionless.Api\obj" "$sourceDir\Exceptionless.Api\App_Data" /S /XF "*.nuspec" "*.settings" "*.cs" "packages.config" "*.csproj" "*.user" "*.suo" "*.xsd" "*.ide" /NFL /NDL /NJH /NJS /nc /ns /np
 
 Write-Host "Copying CloseInactiveSession job..."
 ROBOCOPY "$sourceDir\Jobs\CloseInactiveSession\bin\Release" "$artifactsDir\App_Data\jobs\continuous\CloseInactiveSession" /XD "$sourceDir\Jobs\CloseInactiveSession\bin\Release\bin" /S /NFL /NDL /NJH /NJS /nc /ns /np
