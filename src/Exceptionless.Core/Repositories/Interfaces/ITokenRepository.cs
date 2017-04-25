d676fd60e54eb8e95fb543b526613eaa0a17beb0
Blake Niemyjski <bniemyjski@gmail.com>
2017-04-24 22:23:40 -0500

    WIP - Require login after changing password

--- src/Exceptionless.Core/Repositories/Interfaces/ITokenRepository.cs
+++ src/Exceptionless.Core/Repositories/Interfaces/ITokenRepository.cs
@@ -6,8 +6,9 @@ using Foundatio.Repositories.Models;
 
 namespace Exceptionless.Core.Repositories {
     public interface ITokenRepository : IRepositoryOwnedByOrganizationAndProject<Token> {
-        Task<FindResults<Token>> GetByUserIdAsync(string userId);
+        Task<FindResults<Token>> GetByUserIdAsync(string userId, CommandOptionsDescriptor<Token> options = null);
         Task<FindResults<Token>> GetByTypeAndOrganizationIdAsync(TokenType type, string organizationId, CommandOptionsDescriptor<Token> options = null);
         Task<FindResults<Token>> GetByTypeAndProjectIdAsync(TokenType type, string projectId, CommandOptionsDescriptor<Token> options = null);
+        Task<long> RemoveAllByUserIdAsync(string userId, CommandOptionsDescriptor<Token> options = null);
     }
 }
