4e59d17802e0a15441f1602a032ae7eb80c7f804
Blake Niemyjski <bniemyjski@gmail.com>
2017-10-10 07:47:59 -0500

    Run the jobs with dotnet..

--- src/Jobs/run.bat
+++ src/Jobs/run.bat
@@ -6,13 +6,14 @@ IF NOT EXIST %bindir% (
   SET bindir=%WEBROOT_PATH%\bin
 )
 
-FOR %%F IN (*.exe) DO (
+FOR %%F IN (*Job.dll) DO (
  SET jobname=%%F
  goto done
 )
 :done
 
+robocopy %bindir% .\ "Exceptionless.Core*" "Exceptionless.Insulation*" /NFL /NDL /NJH /NJS /nc /ns /np
 robocopy %bindir% bin\ /S /NFL /NDL /NJH /NJS /nc /ns /np
 IF %ERRORLEVEL% GEQ 8 exit 1
 
-%jobname% %*
+dotnet %jobname% %*
