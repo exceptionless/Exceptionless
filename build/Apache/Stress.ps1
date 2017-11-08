e992d319fc0c71d9138d5cd188aaa3eacae6e819
Blake Niemyjski <bniemyjski@gmail.com>
2017-11-08 06:58:38 -0600

    Changed port back to 50000

--- build/Apache/Stress.ps1
+++ build/Apache/Stress.ps1
@@ -1,1 +1,1 @@
-.\ab.exe -c 10 -n 50000 -p events.json -T application/json -A client:LhhP1C9gijpSKCslHHCvwdSIz298twx271n1l6xw http://localhost:5000/api/v2/events
+.\ab.exe -c 10 -n 50000 -p events.json -T application/json -A client:LhhP1C9gijpSKCslHHCvwdSIz298twx271n1l6xw http://localhost:50000/api/v2/events
