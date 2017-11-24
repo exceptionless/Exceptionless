6bd1c8f97550414c3db8a6c2b490df3752bc0043
Blake Niemyjski <bniemyjski@gmail.com>
2017-11-24 08:49:55 -0600

    Try to update build artifacts

--- build/Push-Artifacts.ps1
+++ build/Push-Artifacts.ps1
@@ -40,39 +40,39 @@ ROBOCOPY "$sourceDir\Exceptionless.Web" "$artifactsDir" /XD "$sourceDir\Exceptio
 ROBOCOPY "$sourceDir\Exceptionless.Web\bin\Release\netcoreapp2.0\publish" "$artifactsDir\bin" /XD "$sourceDir\Exceptionless.Web\bin\Release\netcoreapp2.0\publish\wwwroot" /S /XF "*.yml" "Web.config" /NFL /NDL /NJH /NJS /nc /ns /np
 
 Write-Host "Copying CleanupSnapshot job..."
-ROBOCOPY "$sourceDir\Jobs\CleanupSnapshot\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\triggered\CleanupSnapshot" /XD "$sourceDir\Jobs\CleanupSnapshot\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\CleanupSnapshot\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\triggered\CleanupSnapshot" "CleanupSnapshot*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying CloseInactiveSession job..."
-ROBOCOPY "$sourceDir\Jobs\CloseInactiveSession\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\CloseInactiveSession" /XD "$sourceDir\Jobs\CloseInactiveSession\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\CloseInactiveSession\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\CloseInactiveSession" "CloseInactiveSession*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying DailySummary job..."
-ROBOCOPY "$sourceDir\Jobs\DailySummary\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\DailySummary" /XD "$sourceDir\Jobs\DailySummary\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\DailySummary\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\DailySummary" "DailySummary*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying DownloadGeoIPDatabase job..."
-ROBOCOPY "$sourceDir\Jobs\DownloadGeoIPDatabase\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\triggered\DownloadGeoIPDatabase" /XD "$sourceDir\Jobs\DownloadGeoIPDatabase\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\DownloadGeoIPDatabase\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\triggered\DownloadGeoIPDatabase" "DownloadGeoIPDatabase*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying EventNotification job..."
-ROBOCOPY "$sourceDir\Jobs\EventNotification\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\EventNotification" /XD "$sourceDir\Jobs\EventNotification\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\EventNotification\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\EventNotification" "EventNotification*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying EventPost job..."
-ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\EventPost" /XD "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish"  /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
-ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\EventPost2" /XD "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
-ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\EventPost3" /XD "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
-ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\EventPost4" /XD "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\EventPost" "EventPost*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
+ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\EventPost2" "EventPost*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
+ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\EventPost3" "EventPost*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
+ROBOCOPY "$sourceDir\Jobs\EventPost\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\EventPost4" "EventPost*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying EventSnapshot job..."
-ROBOCOPY "$sourceDir\Jobs\EventSnapshot\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\triggered\EventSnapshot" /XD "$sourceDir\Jobs\EventSnapshot\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\EventSnapshot\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\triggered\EventSnapshot" "EventSnapshot*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying EventUserDescription job..."
-ROBOCOPY "$sourceDir\Jobs\EventUserDescription\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\EventUserDescription" /XD "$sourceDir\Jobs\EventUserDescription\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\EventUserDescription\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\EventUserDescription" "EventUserDescription*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying MailMessage job..."
-ROBOCOPY "$sourceDir\Jobs\MailMessage\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\MailMessage" /XD "$sourceDir\Jobs\MailMessage\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\MailMessage\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\MailMessage" "MailMessage*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying MaintainIndexes job..."
-ROBOCOPY "$sourceDir\Jobs\MaintainIndexes\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\triggered\MaintainIndexes" /XD "$sourceDir\Jobs\MaintainIndexes\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\MaintainIndexes\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\triggered\MaintainIndexes" "MaintainIndexes*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying OrganizationSnapshot job..."
-ROBOCOPY "$sourceDir\Jobs\OrganizationSnapshot\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\triggered\OrganizationSnapshot" /XD "$sourceDir\Jobs\OrganizationSnapshot\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\OrganizationSnapshot\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\triggered\OrganizationSnapshot" "OrganizationSnapshot*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying RetentionLimit job..."
-ROBOCOPY "$sourceDir\Jobs\RetentionLimit\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\RetentionLimit" /XD "$sourceDir\Jobs\RetentionLimit\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\RetentionLimit\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\RetentionLimit" "RetentionLimit*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying StackSnapshot job..."
-ROBOCOPY "$sourceDir\Jobs\StackSnapshot\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\triggered\StackSnapshot" /XD "$sourceDir\Jobs\StackSnapshot\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\StackSnapshot\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\triggered\StackSnapshot" "StackSnapshot*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying WebHook job..."
-ROBOCOPY "$sourceDir\Jobs\WebHook\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\WebHook" /XD "$sourceDir\Jobs\WebHook\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\WebHook\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\WebHook" "WebHook*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 Write-Host "Copying WorkItem job..."
-ROBOCOPY "$sourceDir\Jobs\WorkItem\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\WorkItem" /XD "$sourceDir\Jobs\WorkItem\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
-ROBOCOPY "$sourceDir\Jobs\WorkItem\bin\Release\netcoreapp2.0" "$artifactsDir\App_Data\jobs\continuous\WorkItem2" /XD "$sourceDir\Jobs\WorkItem\bin\Release\netcoreapp2.0\publish" /S /NFL /NDL /NJH /NJS /nc /ns /np /XF "Exceptionless.*" "System.*"
+ROBOCOPY "$sourceDir\Jobs\WorkItem\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\WorkItem" "WorkItem*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
+ROBOCOPY "$sourceDir\Jobs\WorkItem\bin\Release\netcoreapp2.0\publish" "$artifactsDir\App_Data\jobs\continuous\WorkItem2" "WorkItem*" "appsettings*.yml" "run.bat" /NFL /NDL /NJH /NJS /nc /ns /np
 
 git add * 2>&1 | %{ "$_" }
 $res = git diff --cached --numstat | wc -l
