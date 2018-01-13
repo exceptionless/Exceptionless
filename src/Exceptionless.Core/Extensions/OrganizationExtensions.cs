f0f0fcfd4e3fadbdfe5d8c7a63e62a2a2e15ee0f
Blake Niemyjski <bniemyjski@gmail.com>
2018-01-13 11:41:27 -0600

    Normalize the config values and apply styling recommendations (in help window)

--- src/Exceptionless.Core/Extensions/OrganizationExtensions.cs
+++ src/Exceptionless.Core/Extensions/OrganizationExtensions.cs
@@ -21,7 +21,6 @@ namespace Exceptionless.Core.Extensions {
             return organization.GetRetentionUtcCutoff(project.CreatedUtc.SafeSubtract(TimeSpan.FromDays(3)));
         }
 
-
         public static DateTime GetRetentionUtcCutoff(this Organization organization, Stack stack) {
             return organization.GetRetentionUtcCutoff(stack.FirstOccurrence);
         }
@@ -77,9 +76,8 @@ namespace Exceptionless.Core.Extensions {
         }
 
         public static async Task<bool> IsOverRequestLimitAsync(string organizationId, ICacheClient cacheClient, int apiThrottleLimit) {
-            if (apiThrottleLimit == int.MaxValue) {
+            if (apiThrottleLimit == Int32.MaxValue)
                 return false;
-            }
 
             string cacheKey = String.Concat("api", ":", organizationId, ":", SystemClock.UtcNow.Floor(TimeSpan.FromMinutes(15)).Ticks);
             var limit = await cacheClient.GetAsync<long>(cacheKey).AnyContext();
