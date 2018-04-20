4571b99e574f92f099a31ae9c0e2b46750ef621a
Blake Niemyjski <bniemyjski@gmail.com>
2018-04-20 08:45:22 -0500

    #370 - Ensure retention job is only for the current plan period.

--- tests/Exceptionless.Api.Tests/Billing/BillingManagerTests.cs
+++ tests/Exceptionless.Api.Tests/Billing/BillingManagerTests.cs
@@ -13,26 +13,5 @@ namespace Exceptionless.Api.Tests.Billing {
         public void GetBillingPlan() {
             Assert.Equal(BillingManager.FreePlan.Id, BillingManager.GetBillingPlan(BillingManager.FreePlan.Id).Id);
         }
-
-        [Fact]
-        public void GetBillingPlanByUpsellingRetentionPeriod() {
-            var plan = BillingManager.GetBillingPlanByUpsellingRetentionPeriod(BillingManager.FreePlan.RetentionDays);
-            Assert.NotNull(plan);
-            Assert.Equal(BillingManager.SmallPlan.Id, plan.Id);
-            Assert.Equal(BillingManager.SmallPlan.RetentionDays, plan.RetentionDays);
-
-            plan = BillingManager.GetBillingPlanByUpsellingRetentionPeriod(BillingManager.SmallPlan.RetentionDays);
-            Assert.NotNull(plan);
-            Assert.Equal(BillingManager.MediumPlan.Id, plan.Id);
-            Assert.Equal(BillingManager.MediumPlan.RetentionDays, plan.RetentionDays);
-
-            plan = BillingManager.GetBillingPlanByUpsellingRetentionPeriod(BillingManager.MediumPlan.RetentionDays);
-            Assert.NotNull(plan);
-            Assert.Equal(BillingManager.LargePlan.Id, plan.Id);
-            Assert.Equal(BillingManager.LargePlan.RetentionDays, plan.RetentionDays);
-
-            plan = BillingManager.GetBillingPlanByUpsellingRetentionPeriod(BillingManager.LargePlan.RetentionDays);
-            Assert.Null(plan);
-        }
     }
 }
