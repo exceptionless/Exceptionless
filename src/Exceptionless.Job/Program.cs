c7968dd077e5acf19a1f7c06f25440116a77767f
Eric J. Smith <eric@codesmithtools.com>
2018-05-04 02:19:57 -0500

    Fix job runner and update packages

--- src/Exceptionless.Job/Program.cs
+++ src/Exceptionless.Job/Program.cs
@@ -7,17 +7,16 @@ using Exceptionless.Insulation.Jobs;
 namespace Exceptionless.Job {
     public class Program {
         public static int Main(string[] args) {
-            IServiceProvider serviceProvider = null;
+            var serviceProvider = JobServiceProvider.GetServiceProvider();
 
             try {
-                int result = JobCommands.Run(args, () => JobServiceProvider.GetServiceProvider(), app => {
+                int result = JobCommands.Run(args, serviceProvider, app => {
                     app.JobConfiguration.Assemblies = new List<string> { "Exceptionless.Core", "Foundatio" };
                 });
 
                 return result;
             } catch (Exception ex) {
                 Log.Fatal(ex, "Job terminated unexpectedly");
-                Console.WriteLine(ex.ToString());
                 return 1;
             } finally {
                 Log.CloseAndFlush();
