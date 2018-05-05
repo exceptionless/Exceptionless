e36c5f804989eb21a5d028ef4d66f9719ec9f101
Eric J. Smith <eric@codesmithtools.com>
2018-05-04 23:05:56 -0500

    Slight bootstrap change

--- src/Exceptionless.Job/Program.cs
+++ src/Exceptionless.Job/Program.cs
@@ -7,9 +7,11 @@ using Exceptionless.Insulation.Jobs;
 namespace Exceptionless.Job {
     public class Program {
         public static int Main(string[] args) {
-            var serviceProvider = JobServiceProvider.GetServiceProvider();
+            IServiceProvider serviceProvider = null;
 
             try {
+                serviceProvider = JobServiceProvider.GetServiceProvider();
+
                 int result = JobCommands.Run(args, serviceProvider, app => {
                     app.JobConfiguration.Assemblies = new List<string> { "Exceptionless.Core", "Foundatio" };
                 });
