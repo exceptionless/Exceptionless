014e2a0ab632aa00223677fb89a488c5f7688e3e
Blake Niemyjski <bniemyjski@gmail.com>
2018-05-07 08:11:01 -0500

    Fixed merge

--- src/Exceptionless.Web/Controllers/StripeController.cs
+++ src/Exceptionless.Web/Controllers/StripeController.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Exceptionless.Core;
 using Exceptionless.Core.Billing;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using Stripe;
-#pragma warning disable 1998
 
 namespace Exceptionless.Web.Controllers {
     [Route(API_PREFIX + "/stripe")]
@@ -24,24 +23,19 @@ namespace Exceptionless.Web.Controllers {
 
         [AllowAnonymous]
         [HttpPost]
-        public async Task<IActionResult> PostAsync(JObject data) {
-            using (_logger.BeginScope(new ExceptionlessState().SetHttpContext(HttpContext).Property("event", data))) {
-                string json = data?.ToString();
+        public async Task<IActionResult> PostAsync() {
+            string json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            using (_logger.BeginScope(new ExceptionlessState().SetHttpContext(HttpContext).Property("event", json))) {
                 if (String.IsNullOrEmpty(json)) {
                     _logger.LogWarning("Unable to get json of incoming event.");
                     return BadRequest();
                 }
 
-                if (!Request.Headers.TryGetValue("Stripe-Signature", out var signature) || String.IsNullOrEmpty(signature)) {
-                    _logger.LogWarning("No Stripe-Signature header was sent with incoming event.");
-                    return BadRequest();
-                }
-
                 StripeEvent stripeEvent;
                 try {
-                    stripeEvent = StripeEventUtility.ConstructEvent(json, signature, Settings.Current.StripeWebHookSigningSecret);
+                    stripeEvent = StripeEventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], Settings.Current.StripeWebHookSigningSecret);
                 } catch (Exception ex) {
-                    _logger.LogError(ex, "Unable to parse incoming event: {Message}", ex.Message);
+                    _logger.LogError(ex, "Unable to parse incoming event with {Signature}: {Message}", Request.Headers["Stripe-Signature"], ex.Message);
                     return BadRequest();
                 }
 
