a4780e4fd40ded029acdb1ccc0caee409cc7a24e
Blake Niemyjski <bniemyjski@gmail.com>
2018-08-07 08:43:51 -0500

    Added the ability to discard user data server side

---
+++ src/Exceptionless.Core/Plugins/EventProcessor/Default/90_RemovePrivateInformationPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exceptionless.Core.Extensions;
+using Exceptionless.Core.Models;
+using Exceptionless.Core.Models.Data;
+using Exceptionless.Core.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Exceptionless.Core.Plugins.EventProcessor.Default {
+    [Priority(90)]
+    public sealed class RemovePrivateInformationPlugin : EventProcessorPluginBase {
+        public RemovePrivateInformationPlugin(ILoggerFactory loggerFactory = null) : base(loggerFactory) { }
+
+        public override Task EventProcessingAsync(EventContext context) {
+            if (context.IncludePrivateInformation)
+                return Task.CompletedTask;
+
+            context.Event.Data.Remove(Event.KnownDataKeys.UserInfo);
+            var description = context.Event.GetUserDescription();
+
+            if (description != null) {
+                description.EmailAddress = null;
+                if (!String.IsNullOrEmpty(description.Description))
+                    context.Event.SetUserDescription(description);
+                else
+                    context.Event.Data.Remove(Event.KnownDataKeys.UserDescription);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
