01169c4d3c8d4fbe87a599ab5cb16246e3eb3fc3
Blake Niemyjski <bniemyjski@gmail.com>
2018-09-04 07:11:36 -0500

    Updated stripe

--- src/Exceptionless.Web/Models/Organization/InvoiceLineItem.cs
+++ src/Exceptionless.Web/Models/Organization/InvoiceLineItem.cs
@@ -4,6 +4,6 @@ namespace Exceptionless.Web.Models {
     public class InvoiceLineItem {
         public string Description { get; set; }
         public string Date { get; set; }
-        public double Amount { get; set; }
+        public decimal Amount { get; set; }
     }
 }
