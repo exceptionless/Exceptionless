41d03976ff49978932a8a801e0c598f77c566d77
Blake Niemyjski <bniemyjski@gmail.com>
2019-01-23 18:46:21 -0600

    Updated Foundatio for additional queue time metrics/healthchecks  + echo end point

--- k8s/azure-k8s.sh
+++ k8s/azure-k8s.sh
@@ -44,6 +44,11 @@ helm init --service-account tiller
 # install nginx ingress
 helm install stable/nginx-ingress --namespace kube-system --name nginx-ingress --set controller.replicaCount=2
 
+#nginx ingress configmap
+#    "ssl-ciphers": "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA",
+#    "ssl-protocols": "TLSv1 TLSv1.1 TLSv1.2",
+#    "use-forwarded-headers": "true"
+
 helm upgrade --set "controller.stats.enabled=true" --set "controller.metrics.enabled=true" --reuse-values nginx-ingress nginx-ingress stable/nginx-ingress
 
 # wait for external ip to be assigned
