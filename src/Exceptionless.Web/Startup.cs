c28c71254171f04a89427218870fcab078abe242
Blake Niemyjski <bniemyjski@gmail.com>
2019-02-18 07:52:43 -0600

    Updated deps

--- src/Exceptionless.Web/Startup.cs
+++ src/Exceptionless.Web/Startup.cs
@@ -127,6 +127,9 @@ namespace Exceptionless.Web {
                 Predicate = hcr => options.RunJobsInProcess || hcr.Tags.Contains("Critical") || (!options.EventSubmissionDisabled && hcr.Tags.Contains("Storage"))
             });
 
+            if (options.EnableBootstrapStartupActions)
+                app.UseWaitForStartupActionsBeforeServingRequests();
+            
             app.UseCsp(csp => {
                 csp.ByDefaultAllow.FromSelf()
                     .From("https://js.stripe.com");
@@ -163,8 +166,6 @@ namespace Exceptionless.Web {
             app.UseForwardedHeaders();
             app.UseAuthentication();
             
-            if (options.EnableBootstrapStartupActions)
-                app.UseWaitForStartupActionsBeforeServingRequests();
             
             app.UseMiddleware<ProjectConfigMiddleware>();
             app.UseMiddleware<RecordSessionHeartbeatMiddleware>();
