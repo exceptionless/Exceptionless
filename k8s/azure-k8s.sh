0a6e14178a6a560cc35c70d954f6a0d8b2a18599
Eric J. Smith <eric@codesmithtools.com>
2019-07-01 23:19:51 -0500

    cert-manager updates

--- k8s/azure-k8s.sh
+++ k8s/azure-k8s.sh
@@ -54,11 +54,11 @@ PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ip
 az network public-ip update --ids $PUBLICIPID --dns-name $CLUSTER
 
 # install cert-manager
-helm install stable/cert-manager \
-    --namespace kube-system \
-    --name cert-manager \
-    --set ingressShim.defaultIssuerName=letsencrypt-prod \
-    --set ingressShim.defaultIssuerKind=ClusterIssuer
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
+kubectl label namespace kube-system certmanager.k8s.io/disable-validation=true
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install jetstack/cert-manager --version v0.8.1 --namespace kube-system --name cert-manager --set ingressShim.defaultIssuerName=letsencrypt-prod --set ingressShim.defaultIssuerKind=ClusterIssuer
 
 kubectl apply -f cluster-issuer.yaml
 
