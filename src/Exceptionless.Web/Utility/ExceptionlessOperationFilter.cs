bb36cefbe839b85ab2f5a828f9d5c59dfa2f8cb8
Blake Niemyjski <bniemyjski@gmail.com>
2019-10-04 09:43:43 -0500

    Updated Swagger (just comment out the operational filter for now)

--- src/Exceptionless.Web/Utility/ExceptionlessOperationFilter.cs
+++ src/Exceptionless.Web/Utility/ExceptionlessOperationFilter.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Exceptionless.Web.Utility {
    public class ExceptionlessOperationFilter : IOperationFilter {
-        public void Apply(Operation operation, OperationFilterContext context) {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context) { 
             operation.Consumes.Clear();
             operation.Consumes.Add("application/json");
             
