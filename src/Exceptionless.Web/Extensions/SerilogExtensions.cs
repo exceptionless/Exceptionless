f3931b868160f7deb669ff1eda61d508941f960c
Eric J. Smith <eric@codesmithtools.com>
2019-10-04 09:04:27 -0500

    Fixed startup logging

---
+++ src/Exceptionless.Web/Extensions/SerilogExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Exceptionless.Web.Extensions {
+    public static class SerilogExtensions {
+        public static ILoggerFactory ToLoggerFactory(this Serilog.ILogger logger) {
+            return new Serilog.Extensions.Logging.SerilogLoggerFactory(logger);
+        }
+    }
+}
