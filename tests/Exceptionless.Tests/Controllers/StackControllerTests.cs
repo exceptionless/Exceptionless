fd24c26eaeb5afddf3e531016a7c58ea7e370910
Eric J. Smith <eric@ericjsmith.com>
2020-03-02 19:37:02 -0600

    Fixing some tests

--- tests/Exceptionless.Tests/Controllers/StackControllerTests.cs
+++ tests/Exceptionless.Tests/Controllers/StackControllerTests.cs
@@ -45,60 +45,21 @@ namespace Exceptionless.Tests.Controllers {
         public async Task CanMarkFixed(string version) {
             var ev = await SubmitErrorEventAsync();
             Assert.NotNull(ev.StackId);
-            Assert.False(ev.IsFixed); 
 
             var stack = await _stackRepository.GetByIdAsync(ev.StackId);
             Assert.NotNull(stack);
             Assert.False(stack.IsFixed());
             
-            await _workItemQueue.DeleteQueueAsync();
-            
-            var workIds = await SendRequestAsAsync<WorkInProgressResult>(r => r
+            await SendRequestAsAsync<WorkInProgressResult>(r => r
                 .Post()
                 .AsGlobalAdminUser()
                 .AppendPath($"stacks/{stack.Id}/mark-fixed")
                 .QueryStringIf(() => !String.IsNullOrEmpty(version), "version", version)
-                .StatusCodeShouldBeAccepted());
-
-            Assert.Single(workIds.Workers);
-            
-            var stats = await _workItemQueue.GetQueueStatsAsync();
-            Assert.Equal(1, stats.Enqueued);
-            Assert.Equal(0, stats.Completed);
-
-            var job = GetService<WorkItemJob>();
-            await job.RunAsync();
-            await RefreshDataAsync();
-
-            stats = await _workItemQueue.GetQueueStatsAsync();
-            Assert.Equal(1, stats.Completed);
+                .StatusCodeShouldBeOk());
             
             stack = await _stackRepository.GetByIdAsync(ev.StackId);
             Assert.NotNull(stack);
             Assert.True(stack.IsFixed()); 
-            
-            ev = await _eventRepository.GetByIdAsync(ev.Id);
-            Assert.NotNull(ev);
-            Assert.True(ev.IsFixed); 
-            
-            workIds = await SendRequestAsAsync<WorkInProgressResult>(r => r
-                .Delete()
-                .AsGlobalAdminUser()
-                .AppendPath($"stacks/{stack.Id}/mark-fixed")
-                .QueryStringIf(() => !String.IsNullOrEmpty(version), "version", version)
-                .StatusCodeShouldBeAccepted());
-            
-            Assert.Single(workIds.Workers);
-            
-            stack = await _stackRepository.GetByIdAsync(ev.StackId);
-            Assert.NotNull(stack);
-            Assert.False(stack.IsFixed()); 
-            
-            await job.RunAsync();
-            
-            ev = await _eventRepository.GetByIdAsync(ev.Id);
-            Assert.NotNull(ev);
-            Assert.False(ev.IsFixed); 
         }
 
         private async Task<PersistentEvent> SubmitErrorEventAsync() {
