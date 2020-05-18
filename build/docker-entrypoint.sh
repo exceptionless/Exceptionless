95be1977d27a7b8f7a6e48923b15d8e336c0dd8c
Eric J. Smith <eric@codesmithtools.com>
2020-05-18 10:39:37 -0500

    More work all in one docker image

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -12,6 +12,10 @@ else
     mkdir -p /var/log/supervisor
     mkdir -p Temp/
 
+    sed -i -E "s/app\//app\/wwwroot\//" /usr/local/bin/bootstrap
+    sed -i -E "s/nginx//" /usr/local/bin/bootstrap
+    /usr/local/bin/bootstrap
+
     supervisord -c /etc/supervisord.conf
     sleep 3
 
