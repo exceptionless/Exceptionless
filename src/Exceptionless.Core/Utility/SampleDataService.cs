20548c86b4b920095cd34996be71f64193cbcd9a
Eric J. Smith <eric@codesmithtools.com>
2020-06-04 21:47:47 -0500

    Reduce default ES memory to 1g. Update http file to use variables.

--- src/Exceptionless.Core/Utility/SampleDataService.cs
+++ src/Exceptionless.Core/Utility/SampleDataService.cs
@@ -18,7 +18,7 @@ namespace Exceptionless.Core.Utility {
         private readonly IUserRepository _userRepository;
         private readonly ILogger<SampleDataService> _logger;
 
-        public const string TEST_USER_EMAIL = "test@exceptionless.io";
+        public const string TEST_USER_EMAIL = "test@test.com";
         public const string TEST_USER_PASSWORD = "tester";
         public const string TEST_ORG_ID = "537650f3b77efe23a47914f3";
         public const string TEST_PROJECT_ID = "537650f3b77efe23a47914f4";
