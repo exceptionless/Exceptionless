9cbab35fe15c8490220f10389496189e6b09180a
Blake Niemyjski <bniemyjski@gmail.com>
2020-07-16 21:06:04 -0500

    Added migrations for new mappings and setting stack status.

---
+++ src/Exceptionless.Core/Migrations/001_UpdateIndexMappings.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Exceptionless.Core.Models;
+using Exceptionless.Core.Repositories;
+using Exceptionless.Core.Repositories.Configuration;
+using Foundatio.Repositories.Elasticsearch.Extensions;
+using Foundatio.Repositories.Migrations;
+using Microsoft.Extensions.Logging;
+using Nest;
+
+namespace Exceptionless.Core.Migrations {
+    public class UpdateIndexMappings : MigrationBase {
+        private readonly IElasticClient _client;
+        private readonly ExceptionlessElasticConfiguration _config;
+
+        public UpdateIndexMappings(ExceptionlessElasticConfiguration configuration, ILoggerFactory loggerFactory) : base(loggerFactory) {
+            _config = configuration;
+            _client = configuration.Client;
+        }
+
+        public override int? Version => 1;
+
+        public override async Task RunAsync() {
+            _logger.LogInformation("Start migration for adding index mappings...");
+            var response = await _client.MapAsync<Organization>(d => {
+                d.Index(_config.Organizations.VersionedName);
+                d.Properties(p => p
+                    .Date(f => f.Name(s => s.LastEventDateUtc))
+                    .Boolean(f => f.Name(s => s.IsDeleted)).FieldAlias(a => a.Path(p1 => p1.IsDeleted).Name("deleted")));
+                    
+                return d;
+            });
+            _logger.LogTraceRequest(response); 
+            
+            response = await _client.MapAsync<Project>(d => {
+                d.Index(_config.Projects.VersionedName);
+                d.Properties(p => p
+                    .Date(f => f.Name(s => s.LastEventDateUtc))
+                    .Boolean(f => f.Name(s => s.IsDeleted)).FieldAlias(a => a.Path(p1 => p1.IsDeleted).Name("deleted")));
+                    
+                return d;
+            });
+            _logger.LogTraceRequest(response);
+            
+            response = await _client.MapAsync<Stack>(d => {
+                    d.Index(_config.Stacks.VersionedName);
+                    d.Properties(p => p
+                        .Keyword(f => f.Name(s => s.Status))
+                        .Date(f => f.Name(s => s.SnoozeUntilUtc))
+                        .Boolean(f => f.Name(s => s.IsDeleted)).FieldAlias(a => a.Path(p1 => p1.IsDeleted).Name("deleted")));
+                    
+                return d;
+            });
+            _logger.LogTraceRequest(response);
+            _logger.LogInformation("Finished adding mappings.");
+        }
+    }
+}
