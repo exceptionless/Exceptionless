780533c3fa1d4421b14689e4d2bab73b4a49832b
Blake Niemyjski <bniemyjski@gmail.com>
2020-07-28 09:07:23 -0500

    Added cleanup job

--- src/Exceptionless.Core/Jobs/WorkItemHandlers/RemoveBotEventsWorkItemHandler.cs
+++ src/Exceptionless.Core/Jobs/WorkItemHandlers/RemoveBotEventsWorkItemHandler.cs
@@ -31,7 +31,7 @@ namespace Exceptionless.Core.Jobs.WorkItemHandlers
             using (Log.BeginScope(new ExceptionlessState().Organization(wi.OrganizationId))) {
                 Log.LogInformation("Received remove bot events work item OrganizationId={OrganizationId}, ClientIpAddress={ClientIpAddress}, UtcStartDate={UtcStartDate}, UtcEndDate={UtcEndDate}", wi.OrganizationId, wi.ClientIpAddress, wi.UtcStartDate, wi.UtcEndDate);
                 await context.ReportProgressAsync(0, "Starting deleting of bot events...").AnyContext();
-                long deleted = await _eventRepository.RemoveAllAsync(new [] { wi.OrganizationId }, null, null, null, wi.ClientIpAddress, wi.UtcStartDate, wi.UtcEndDate).AnyContext();
+                long deleted = await _eventRepository.RemoveAllAsync(wi.OrganizationId, wi.ClientIpAddress, wi.UtcStartDate, wi.UtcEndDate).AnyContext();
                 await context.ReportProgressAsync(100, $"Bot events deleted: {deleted}").AnyContext();
             }
         }
