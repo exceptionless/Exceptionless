ab4548876540ebd9266d6a010f54c1a399140770
Blake Niemyjski <bniemyjski@gmail.com>
2020-08-02 20:28:10 -0500

    Updated api error to pass a reference id.

--- src/Exceptionless.Web/Utility/Handlers/ApiError.cs
+++ src/Exceptionless.Web/Utility/Handlers/ApiError.cs
@@ -6,14 +6,17 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace Exceptionless.Web.Utility.Handlers {
     public class ApiError {
         public string Message { get; set; }
+        public string ReferenceId { get; set; }
         public bool IsError => true;
         public string Detail { get; set; }
         public ICollection<ApiErrorItem> Errors { get; set; }
 
-        public ApiError(string message) {
+        public ApiError(string message, string referenceId) {
             Message = message;
+            ReferenceId = referenceId;
             Errors = new List<ApiErrorItem>();
         }
+
 
         public ApiError(ModelStateDictionary modelState) {
             if (modelState != null && modelState.Any(m => m.Value.Errors.Count > 0)) {
@@ -23,8 +26,9 @@ namespace Exceptionless.Web.Utility.Handlers {
                 //errors = modelState.SelectMany(m => m.Value.Errors.Select(me => new ModelError { FieldName = m.Key, ErrorMessage = me.ErrorMessage }));
             }
         }
-        public ApiError(ValidationException ex) {
+        public ApiError(ValidationException ex, string referenceId) {
             Message = "Please correct the specified errors and try again.";
+            ReferenceId = referenceId;
             Errors = ex.Errors.Select(error => new ApiErrorItem {
                 PropertyName = error.PropertyName,
                 Message = error.ErrorMessage,
