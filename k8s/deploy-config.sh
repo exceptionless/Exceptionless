af087ac04b7af5cb60c05072a8da44b2147a9306
Eric J. Smith <eric@ericjsmith.com>
2020-08-24 20:52:16 -0500

    Fix ssl cert cluster issuer. Remove snapshot jobs.

--- k8s/deploy-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-CHECKSUM=$(kubectl get configmaps exceptionless-config -o yaml | shasum -a 256 | awk '{print $1}')
-kubectl patch deployment exceptionless-statsd -p '{"spec":{"template":{"metadata":{"annotations":{"checksum/config":"$CHECKSUM"}}}}}'
