cac1ecf1c51d33bd448e31ddc3c3549e00ae128b
Blake Niemyjski <bniemyjski@gmail.com>
2020-11-17 08:23:46 -0600

    Fixed a health check condition for the Stack Status Job #779

--- src/Exceptionless.Core/Jobs/StackStatusJob.cs
+++ src/Exceptionless.Core/Jobs/StackStatusJob.cs
@@ -58,10 +58,10 @@ namespace Exceptionless.Core.Jobs {
             if (!_lastRun.HasValue)
                 return Task.FromResult(HealthCheckResult.Healthy("Job has not been run yet."));
 
-            if (SystemClock.UtcNow.Subtract(_lastRun.Value) > TimeSpan.FromSeconds(15))
-                return Task.FromResult(HealthCheckResult.Unhealthy("Job has not run in the last 15 seconds."));
+            if (SystemClock.UtcNow.Subtract(_lastRun.Value) > TimeSpan.FromMinutes(1))
+                return Task.FromResult(HealthCheckResult.Unhealthy("Job has not run in the last minute."));
 
-            return Task.FromResult(HealthCheckResult.Healthy("Job has run in the last 15 seconds."));
+            return Task.FromResult(HealthCheckResult.Healthy("Job has run in the last minute."));
         }
     }
 }
