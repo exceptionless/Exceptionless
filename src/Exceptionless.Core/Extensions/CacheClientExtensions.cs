45bbdc32ffa61077b3f85610ec9a540ea248c854
Blake Niemyjski <bniemyjski@gmail.com>
2021-12-30 06:41:39 -0600

    Simplified IncrementIf after negative usage data was resolved.

--- src/Exceptionless.Core/Extensions/CacheClientExtensions.cs
+++ src/Exceptionless.Core/Extensions/CacheClientExtensions.cs
@@ -5,44 +5,34 @@ namespace Exceptionless.Extensions;
 
 public static class CacheClientExtensions {
     /// <summary>
-    /// Increment a value if condition is true. Ensure any negative increments have a negative value.
+    /// Increment a value if condition is true.
     /// </summary>
-    public static async Task<double> PositiveIncrementIfAsync(this ICacheClient client, string key, int value, TimeSpan timeToLive, bool shouldIncrement, long? startingValue = null) {
+    public static async Task<double> IncrementIfAsync(this ICacheClient client, string key, int value, TimeSpan timeToLive, bool shouldIncrement, long? startingValue = null) {
         startingValue ??= 0;
 
         var count = await client.GetAsync<long>(key).AnyContext();
         if (!shouldIncrement)
             return count.HasValue ? count.Value : startingValue.Value;
 
-        if (count.HasValue) {
-            var incrementedValue = await client.IncrementAsync(key, value, timeToLive).AnyContext();
-            if (incrementedValue >= 0)
-                return incrementedValue;
+        if (count.HasValue)
+            return await client.IncrementAsync(key, value, timeToLive).AnyContext();
 
-            return await client.IncrementAsync(key, Math.Abs(incrementedValue), timeToLive).AnyContext();
-        }
-
-        long newValue = Math.Max(0, startingValue.Value + value);
+        long newValue = startingValue.Value + value;
         await client.SetAsync(key, newValue, timeToLive).AnyContext();
         return newValue;
     }
 
     /// <summary>
-    /// Increment a value if condition is true. Ensure any negative increments have a negative value.
+    /// Increment a value if condition is true.
     /// </summary>
-    public static async Task<double> PositiveIncrementAsync(this ICacheClient client, string key, int value, TimeSpan timeToLive, long? startingValue = null) {
+    public static async Task<double> IncrementAsync(this ICacheClient client, string key, int value, TimeSpan timeToLive, long? startingValue = null) {
         startingValue ??= 0;
 
         var count = await client.GetAsync<long>(key).AnyContext();
-        if (count.HasValue) {
-            var incrementedValue = await client.IncrementAsync(key, value, timeToLive).AnyContext();
-            if (incrementedValue >= 0)
-                return incrementedValue;
+        if (count.HasValue)
+            return await client.IncrementAsync(key, value, timeToLive).AnyContext();
 
-            return await client.IncrementAsync(key, Math.Abs(incrementedValue), timeToLive).AnyContext();
-        }
-
-        long newValue = Math.Max(0, startingValue.Value + value);
+        long newValue = startingValue.Value + value;
         await client.SetAsync(key, newValue, timeToLive).AnyContext();
         return newValue;
     }
