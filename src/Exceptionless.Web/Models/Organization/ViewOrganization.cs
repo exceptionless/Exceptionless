d5258edb7226082f00f8ddf29f90c38348a319c9
Eric J. Smith <eric@codesmithtools.com>
2022-06-29 20:53:41 -0500

    Fill out 12 months of usage on org and project

--- src/Exceptionless.Web/Models/Organization/ViewOrganization.cs
+++ src/Exceptionless.Web/Models/Organization/ViewOrganization.cs
@@ -1,5 +1,7 @@
 ﻿using Exceptionless.Core.Models;
+using Exceptionless.DateTimeExtensions;
 using Foundatio.Repositories.Models;
+using Foundatio.Utility;
 
 namespace Exceptionless.Web.Models;
 
@@ -33,9 +35,63 @@ public class ViewOrganization : IIdentity, IData, IHaveCreatedDate {
     public ICollection<Invite> Invites { get; set; }
     public ICollection<UsageHourInfo> UsageHours { get; set; }
     public ICollection<UsageInfo> Usage { get; set; }
-    public DataDictionary Data { get; set; }
+    public Core.Models.DataDictionary Data { get; set; }
 
     public bool IsThrottled { get; set; }
     public bool IsOverMonthlyLimit { get; set; }
     public bool IsOverRequestLimit { get; set; }
+}
+
+public static class ViewOrganizationExtensions {
+    public static UsageHourInfo GetHourlyUsage(this ViewOrganization organization, DateTime date) {
+        var overage = organization.UsageHours.FirstOrDefault(o => o.Date == date.StartOfHour());
+        if (overage != null)
+            return overage;
+
+        overage = new UsageHourInfo {
+            Date = date.StartOfHour()
+        };
+        organization.UsageHours.Add(overage);
+
+        return overage;
+    }
+
+    public static UsageHourInfo GetCurrentHourlyUsage(this ViewOrganization organization) {
+        return organization.GetHourlyUsage(SystemClock.UtcNow);
+    }
+
+    public static void EnsureUsage(this ViewOrganization organization) {
+        var startDate = SystemClock.UtcNow.SubtractYears(1).StartOfMonth();
+
+        while (startDate < SystemClock.UtcNow.StartOfMonth()) {
+            organization.GetUsage(startDate);
+            startDate = startDate.AddMonths(1).StartOfMonth();
+        }
+    }
+
+    public static UsageInfo GetCurrentUsage(this ViewOrganization organization) {
+        return organization.GetUsage(SystemClock.UtcNow);
+    }
+
+    public static UsageInfo GetUsage(this ViewOrganization organization, DateTime date) {
+        var usage = organization.Usage.FirstOrDefault(o => o.Date == date.StartOfMonth());
+        if (usage != null)
+            return usage;
+
+        usage = new UsageInfo {
+            Date = date.StartOfMonth(),
+            Limit = organization.GetMaxEventsPerMonthWithBonus()
+        };
+        organization.Usage.Add(usage);
+
+        return usage;
+    }
+
+    public static int GetMaxEventsPerMonthWithBonus(this ViewOrganization organization) {
+        if (organization.MaxEventsPerMonth <= 0)
+            return -1;
+
+        int bonusEvents = organization.BonusExpiration.HasValue && organization.BonusExpiration > SystemClock.UtcNow ? organization.BonusEventsPerMonth : 0;
+        return organization.MaxEventsPerMonth + bonusEvents;
+    }
 }
--- src/Exceptionless.Web/Models/Project/ViewProject.cs
+++ src/Exceptionless.Web/Models/Project/ViewProject.cs
@@ -1,5 +1,7 @@
 ﻿using Exceptionless.Core.Models;
+using Exceptionless.DateTimeExtensions;
 using Foundatio.Repositories.Models;
+using Foundatio.Utility;
 
 namespace Exceptionless.Web.Models;
 
@@ -10,7 +12,7 @@ public class ViewProject : IIdentity, IData, IHaveCreatedDate {
     public string OrganizationName { get; set; }
     public string Name { get; set; }
     public bool DeleteBotDataEnabled { get; set; }
-    public DataDictionary Data { get; set; }
+    public Core.Models.DataDictionary Data { get; set; }
     public HashSet<string> PromotedTabs { get; set; }
     public bool? IsConfigured { get; set; }
     public long StackCount { get; set; }
@@ -19,4 +21,49 @@ public class ViewProject : IIdentity, IData, IHaveCreatedDate {
     public bool HasSlackIntegration { get; set; }
     public ICollection<UsageHourInfo> UsageHours { get; set; }
     public ICollection<UsageInfo> Usage { get; set; }
+}
+
+public static class ViewProjectExtensions {
+    public static UsageHourInfo GetHourlyUsage(this ViewProject project, DateTime date) {
+        var overage = project.UsageHours.FirstOrDefault(o => o.Date == date.StartOfHour());
+        if (overage != null)
+            return overage;
+
+        overage = new UsageHourInfo {
+            Date = date.StartOfHour()
+        };
+        project.UsageHours.Add(overage);
+
+        return overage;
+    }
+
+    public static UsageHourInfo GetCurrentHourlyUsage(this ViewProject project) {
+        return project.GetHourlyUsage(SystemClock.UtcNow);
+    }
+
+    public static void EnsureUsage(this ViewProject project) {
+        var startDate = SystemClock.UtcNow.SubtractYears(1).StartOfMonth();
+
+        while (startDate < SystemClock.UtcNow.StartOfMonth()) {
+            project.GetUsage(startDate);
+            startDate = startDate.AddMonths(1).StartOfMonth();
+        }
+    }
+
+    public static UsageInfo GetCurrentUsage(this ViewProject project) {
+        return project.GetUsage(SystemClock.UtcNow);
+    }
+
+    public static UsageInfo GetUsage(this ViewProject project, DateTime date) {
+        var usage = project.Usage.FirstOrDefault(o => o.Date == date.StartOfMonth());
+        if (usage != null)
+            return usage;
+
+        usage = new UsageInfo {
+            Date = date.StartOfMonth(),
+        };
+        project.Usage.Add(usage);
+
+        return usage;
+    }
 }
