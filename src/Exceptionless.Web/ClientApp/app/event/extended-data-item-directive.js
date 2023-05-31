16b78ca2060c19875350bfe15737601813003251
Blake Niemyjski <bniemyjski@gmail.com>
2023-05-31 08:03:58 -0500

    Fixes #1316 Preserve extended data titles (#1334)
    * Fixes #1316 Preserve extended data titles
    
    * Fixes #1316 Preserve extended data titles, ensure tab names are respected

--- src/Exceptionless.Web/ClientApp/app/event/extended-data-item-directive.js
+++ src/Exceptionless.Web/ClientApp/app/event/extended-data-item-directive.js
@@ -26,21 +26,14 @@
         }
 
         function getData(data, exclusions) {
-          function toSpacedWords(value) {
-            value = value.replace(/_/g, ' ').replace(/\s+/g, ' ').trim();
-            value = value.replace(/([a-z0-9])([A-Z0-9])/g, '$1 $2');
-            return value.length > 1 ? value.charAt(0).toUpperCase() + value.slice(1) : value;
-          }
-
           exclusions = exclusions && exclusions.length ? exclusions : [];
-
           if (typeof data !== 'object' || !(data instanceof Object)) {
             return data;
           }
 
           return Object.keys(data)
             .filter(function(value) { return value && value.length && exclusions.indexOf(value) < 0; })
-            .map(function(value) { return { key: value, name: toSpacedWords(value) }; })
+            .map(function(value) { return { key: value, name: value }; })
             .sort(function(a, b) { return a.name - b.name; })
             .reduce(function(a, b) {
               a[b.name] = data[b.key];
