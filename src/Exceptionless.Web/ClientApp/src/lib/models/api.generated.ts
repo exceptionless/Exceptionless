4c5fdc307c70e5aa977aeb19d7b9c4d05dbe052e
Blake Niemyjski <bniemyjski@gmail.com>
2023-08-27 18:25:26 -0500

    more generation improvements

--- src/Exceptionless.Web/ClientApp/src/lib/models/api.generated.ts
+++ src/Exceptionless.Web/ClientApp/src/lib/models/api.generated.ts
@@ -9,24 +9,24 @@
  * ---------------------------------------------------------------
  */
 
-import { IsDate, IsEmail } from 'class-validator';
+import { IsDate, IsDefined, IsEmail, IsMongoId, IsOptional, IsUrl } from 'class-validator';
 
 export class BillingPlan {
-	id?: string;
-	name?: string;
-	description?: string;
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() name?: string;
+	@IsOptional() description?: string;
 	/** @format double */
-	price?: number;
+	@IsOptional() price?: number;
 	/** @format int32 */
-	max_projects?: number;
+	@IsOptional() max_projects?: number;
 	/** @format int32 */
-	max_users?: number;
+	@IsOptional() max_users?: number;
 	/** @format int32 */
-	retention_days?: number;
+	@IsOptional() retention_days?: number;
 	/** @format int32 */
-	max_events_per_month?: number;
-	has_premium_features?: boolean;
-	is_hidden?: boolean;
+	@IsOptional() max_events_per_month?: number;
+	@IsOptional() has_premium_features?: boolean;
+	@IsOptional() is_hidden?: boolean;
 }
 
 /**
@@ -52,63 +52,63 @@ export enum BillingStatus {
 }
 
 export class ChangePasswordModel {
-	current_password?: string | null;
-	password?: string | null;
+	@IsOptional() current_password?: string | null;
+	@IsOptional() password?: string | null;
 }
 
 export class ChangePlanResult {
-	success?: boolean;
-	message?: string | null;
+	@IsOptional() success?: boolean;
+	@IsOptional() message?: string | null;
 }
 
 export class ClientConfiguration {
 	/** @format int32 */
-	version?: number;
-	settings?: Record<string, string>;
+	@IsOptional() version?: number;
+	@IsOptional() settings?: Record<string, string>;
 }
 
 export class CountResult {
 	/** @format int64 */
-	total?: number;
-	aggregations?: Record<string, IAggregate>;
-	data?: Record<string, unknown>;
+	@IsOptional() total?: number;
+	@IsOptional() aggregations?: Record<string, IAggregate>;
+	@IsOptional() data?: Record<string, unknown>;
 }
 
-export class IAggregate {
-	data?: Record<string, unknown>;
+export interface IAggregate {
+	data?: Record<string, any>;
 }
 
 export class Invite {
-	token?: string;
-	email_address?: string;
+	@IsOptional() token?: string;
+	@IsOptional() email_address?: string;
 	/** @format date-time */
-	@IsDate() date_added?: string;
+	@IsOptional() @IsDate() date_added?: string;
 }
 
 export class Invoice {
-	id?: string;
-	organization_id?: string;
-	organization_name?: string;
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() organization_name?: string;
 	/** @format date-time */
-	@IsDate() date?: string;
-	paid?: boolean;
+	@IsOptional() @IsDate() date?: string;
+	@IsOptional() paid?: boolean;
 	/** @format double */
-	total?: number;
-	items?: InvoiceLineItem[];
+	@IsOptional() total?: number;
+	@IsOptional() items?: InvoiceLineItem[];
 }
 
 export class InvoiceGridModel {
-	id?: string;
+	@IsOptional() @IsMongoId() id?: string;
 	/** @format date-time */
-	@IsDate() date?: string;
-	paid?: boolean;
+	@IsOptional() @IsDate() date?: string;
+	@IsOptional() paid?: boolean;
 }
 
 export class InvoiceLineItem {
-	description?: string;
-	date?: string | null;
+	@IsOptional() description?: string;
+	@IsOptional() date?: string | null;
 	/** @format double */
-	amount?: number;
+	@IsOptional() amount?: number;
 }
 
 export class LoginModel {
@@ -118,83 +118,83 @@ export class LoginModel {
 	}
 
 	/** @format email */
-	@IsEmail({ require_tld: false }) email: string;
-	password: string;
-	invite_token?: string | null;
+	@IsDefined() @IsEmail({ require_tld: false }) email: string;
+	@IsDefined() password: string;
+	@IsOptional() invite_token?: string | null;
 }
 
 export class NewOrganization {
-	name?: string;
+	@IsOptional() name?: string;
 }
 
 export class NewProject {
-	organization_id?: string;
-	name?: string;
-	delete_bot_data_enabled?: boolean;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() name?: string;
+	@IsOptional() delete_bot_data_enabled?: boolean;
 }
 
 export class NewToken {
-	organization_id?: string;
-	project_id?: string;
-	default_project_id?: string | null;
-	scopes?: string[];
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() @IsMongoId() project_id?: string;
+	@IsOptional() @IsMongoId() default_project_id?: string | null;
+	@IsOptional() scopes?: string[];
 	/** @format date-time */
-	@IsDate() expires_utc?: string | null;
-	notes?: string | null;
+	@IsOptional() @IsDate() expires_utc?: string | null;
+	@IsOptional() notes?: string | null;
 }
 
 export class NewWebHook {
-	organization_id?: string;
-	project_id?: string;
-	url?: string;
-	event_types?: string[];
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() @IsMongoId() project_id?: string;
+	@IsOptional() @IsUrl() url?: string;
+	@IsOptional() event_types?: string[];
 	/** The schema version that should be used. */
-	version?: string;
+	@IsOptional() version?: string;
 }
 
 export class NotificationSettings {
-	send_daily_summary?: boolean;
-	report_new_errors?: boolean;
-	report_critical_errors?: boolean;
-	report_event_regressions?: boolean;
-	report_new_events?: boolean;
-	report_critical_events?: boolean;
+	@IsOptional() send_daily_summary?: boolean;
+	@IsOptional() report_new_errors?: boolean;
+	@IsOptional() report_critical_errors?: boolean;
+	@IsOptional() report_event_regressions?: boolean;
+	@IsOptional() report_new_events?: boolean;
+	@IsOptional() report_critical_events?: boolean;
 }
 
 export class OAuthAccount {
-	provider?: string;
-	provider_user_id?: string;
-	username?: string;
-	extra_data?: Record<string, string>;
+	@IsOptional() provider?: string;
+	@IsOptional() @IsMongoId() provider_user_id?: string;
+	@IsOptional() username?: string;
+	@IsOptional() extra_data?: Record<string, string>;
 }
 
 export class PersistentEvent {
-	id?: string;
-	organization_id?: string;
-	project_id?: string;
-	stack_id?: string;
-	is_first_occurrence?: boolean;
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() @IsMongoId() project_id?: string;
+	@IsOptional() @IsMongoId() stack_id?: string;
+	@IsOptional() is_first_occurrence?: boolean;
 	/** @format date-time */
-	@IsDate() created_utc?: string;
-	idx?: Record<string, unknown>;
-	type?: string | null;
-	source?: string | null;
+	@IsOptional() @IsDate() created_utc?: string;
+	@IsOptional() idx?: Record<string, unknown>;
+	@IsOptional() type?: string | null;
+	@IsOptional() source?: string | null;
 	/** @format date-time */
-	@IsDate() date?: string;
-	tags?: string[] | null;
-	message?: string | null;
-	geo?: string | null;
+	@IsOptional() @IsDate() date?: string;
+	@IsOptional() tags?: string[] | null;
+	@IsOptional() message?: string | null;
+	@IsOptional() geo?: string | null;
 	/** @format double */
-	value?: number | null;
+	@IsOptional() value?: number | null;
 	/** @format int32 */
-	count?: number | null;
-	data?: Record<string, unknown>;
-	reference_id?: string | null;
+	@IsOptional() count?: number | null;
+	@IsOptional() data?: Record<string, unknown>;
+	@IsOptional() @IsMongoId() reference_id?: string | null;
 }
 
 export class ResetPasswordModel {
-	password_reset_token?: string | null;
-	password?: string | null;
+	@IsOptional() password_reset_token?: string | null;
+	@IsOptional() password?: string | null;
 }
 
 export class SignupModel {
@@ -203,18 +203,18 @@ export class SignupModel {
 		this.password = password;
 	}
 
-	name?: string;
+	@IsOptional() name?: string;
 	/** @format email */
-	@IsEmail({ require_tld: false }) email: string;
-	password: string;
-	invite_token?: string | null;
+	@IsDefined() @IsEmail({ require_tld: false }) email: string;
+	@IsDefined() password: string;
+	@IsOptional() invite_token?: string | null;
 }
 
 export class Stack {
-	id?: string;
-	organization_id?: string;
-	project_id?: string;
-	type?: string;
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() @IsMongoId() project_id?: string;
+	@IsOptional() type?: string;
 	/**
 	 *
 	 * open
@@ -224,32 +224,32 @@ export class Stack {
 	 * ignored
 	 * discarded
 	 */
-	status?: StackStatus;
+	@IsOptional() status?: StackStatus;
 	/** @format date-time */
-	@IsDate() snooze_until_utc?: string | null;
-	signature_hash?: string;
-	signature_info?: Record<string, string>;
-	fixed_in_version?: string | null;
+	@IsOptional() @IsDate() snooze_until_utc?: string | null;
+	@IsOptional() signature_hash?: string;
+	@IsOptional() signature_info?: Record<string, string>;
+	@IsOptional() fixed_in_version?: string | null;
 	/** @format date-time */
-	@IsDate() date_fixed?: string | null;
-	title?: string;
+	@IsOptional() @IsDate() date_fixed?: string | null;
+	@IsOptional() title?: string;
 	/** @format int32 */
-	total_occurrences?: number;
+	@IsOptional() total_occurrences?: number;
 	/** @format date-time */
-	@IsDate() first_occurrence?: string;
+	@IsOptional() @IsDate() first_occurrence?: string;
 	/** @format date-time */
-	@IsDate() last_occurrence?: string;
-	description?: string | null;
-	occurrences_are_critical?: boolean;
-	references?: string[];
-	tags?: string[];
-	duplicate_signature?: string;
+	@IsOptional() @IsDate() last_occurrence?: string;
+	@IsOptional() description?: string | null;
+	@IsOptional() occurrences_are_critical?: boolean;
+	@IsOptional() references?: string[];
+	@IsOptional() tags?: string[];
+	@IsOptional() duplicate_signature?: string;
 	/** @format date-time */
-	@IsDate() created_utc?: string;
+	@IsOptional() @IsDate() created_utc?: string;
 	/** @format date-time */
-	@IsDate() updated_utc?: string;
-	is_deleted?: boolean;
-	allow_notifications?: boolean;
+	@IsOptional() @IsDate() updated_utc?: string;
+	@IsOptional() is_deleted?: boolean;
+	@IsOptional() allow_notifications?: boolean;
 }
 
 /**
@@ -277,48 +277,48 @@ export enum StackStatus {
 }
 
 export class StringStringValuesKeyValuePair {
-	key?: string;
-	value?: string[];
+	@IsOptional() key?: string;
+	@IsOptional() value?: string[];
 }
 
 export class StringValueFromBody {
-	value?: string;
+	@IsOptional() value?: string;
 }
 
 export class TokenResult {
-	token?: string;
+	@IsOptional() token?: string;
 }
 
 export class UpdateEmailAddressResult {
-	is_verified?: boolean;
+	@IsOptional() is_verified?: boolean;
 }
 
 export class UsageHourInfo {
 	/** @format date-time */
-	@IsDate() date?: string;
+	@IsOptional() @IsDate() date?: string;
 	/** @format int32 */
-	total?: number;
+	@IsOptional() total?: number;
 	/** @format int32 */
-	blocked?: number;
+	@IsOptional() blocked?: number;
 	/** @format int32 */
-	discarded?: number;
+	@IsOptional() discarded?: number;
 	/** @format int32 */
-	too_big?: number;
+	@IsOptional() too_big?: number;
 }
 
 export class UsageInfo {
 	/** @format date-time */
-	@IsDate() date?: string;
+	@IsOptional() @IsDate() date?: string;
 	/** @format int32 */
-	limit?: number;
+	@IsOptional() limit?: number;
 	/** @format int32 */
-	total?: number;
+	@IsOptional() total?: number;
 	/** @format int32 */
-	blocked?: number;
+	@IsOptional() blocked?: number;
 	/** @format int32 */
-	discarded?: number;
+	@IsOptional() discarded?: number;
 	/** @format int32 */
-	too_big?: number;
+	@IsOptional() too_big?: number;
 }
 
 export class User {
@@ -327,28 +327,28 @@ export class User {
 		this.email_address = email_address;
 	}
 
-	id?: string;
-	organization_ids?: string[];
-	password?: string | null;
-	salt?: string | null;
-	password_reset_token?: string | null;
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() organization_ids?: string[];
+	@IsOptional() password?: string | null;
+	@IsOptional() salt?: string | null;
+	@IsOptional() password_reset_token?: string | null;
 	/** @format date-time */
-	@IsDate() password_reset_token_expiration?: string;
-	o_auth_accounts?: OAuthAccount[];
-	full_name: string;
+	@IsOptional() @IsDate() password_reset_token_expiration?: string;
+	@IsOptional() o_auth_accounts?: OAuthAccount[];
+	@IsDefined() full_name: string;
 	/** @format email */
-	@IsEmail({ require_tld: false }) email_address: string;
-	email_notifications_enabled?: boolean;
-	is_email_address_verified?: boolean;
-	verify_email_address_token?: string | null;
+	@IsDefined() @IsEmail({ require_tld: false }) email_address: string;
+	@IsOptional() email_notifications_enabled?: boolean;
+	@IsOptional() is_email_address_verified?: boolean;
+	@IsOptional() verify_email_address_token?: string | null;
 	/** @format date-time */
-	@IsDate() verify_email_address_token_expiration?: string;
-	is_active?: boolean;
-	roles?: string[];
+	@IsOptional() @IsDate() verify_email_address_token_expiration?: string;
+	@IsOptional() is_active?: boolean;
+	@IsOptional() roles?: string[];
 	/** @format date-time */
-	@IsDate() created_utc?: string;
+	@IsOptional() @IsDate() created_utc?: string;
 	/** @format date-time */
-	@IsDate() updated_utc?: string;
+	@IsOptional() @IsDate() updated_utc?: string;
 }
 
 export class UserDescription {
@@ -356,25 +356,25 @@ export class UserDescription {
 		this.description = description;
 	}
 
-	email_address?: string | null;
-	description: string;
-	data?: Record<string, unknown>;
+	@IsOptional() email_address?: string | null;
+	@IsDefined() description: string;
+	@IsOptional() data?: Record<string, unknown>;
 }
 
 export class ViewOrganization {
-	id?: string;
+	@IsOptional() @IsMongoId() id?: string;
 	/** @format date-time */
-	@IsDate() created_utc?: string;
-	name?: string;
-	plan_id?: string;
-	plan_name?: string;
-	plan_description?: string;
-	'card_last4'?: string | null;
+	@IsOptional() @IsDate() created_utc?: string;
+	@IsOptional() name?: string;
+	@IsOptional() @IsMongoId() plan_id?: string;
+	@IsOptional() plan_name?: string;
+	@IsOptional() plan_description?: string;
+	@IsOptional() 'card_last4'?: string | null;
 	/** @format date-time */
-	@IsDate() subscribe_date?: string | null;
+	@IsOptional() @IsDate() subscribe_date?: string | null;
 	/** @format date-time */
-	@IsDate() billing_change_date?: string | null;
-	billing_changed_by_user_id?: string | null;
+	@IsOptional() @IsDate() billing_change_date?: string | null;
+	@IsOptional() @IsMongoId() billing_changed_by_user_id?: string | null;
 	/**
 	 *
 	 * 0 = Trialing
@@ -383,91 +383,91 @@ export class ViewOrganization {
 	 * 3 = Canceled
 	 * 4 = Unpaid
 	 */
-	billing_status?: BillingStatus;
+	@IsOptional() billing_status?: BillingStatus;
 	/** @format double */
-	billing_price?: number;
+	@IsOptional() billing_price?: number;
 	/** @format int32 */
-	max_events_per_month?: number;
+	@IsOptional() max_events_per_month?: number;
 	/** @format int32 */
-	bonus_events_per_month?: number;
+	@IsOptional() bonus_events_per_month?: number;
 	/** @format date-time */
-	@IsDate() bonus_expiration?: string | null;
+	@IsOptional() @IsDate() bonus_expiration?: string | null;
 	/** @format int32 */
-	retention_days?: number;
-	is_suspended?: boolean;
-	suspension_code?: string | null;
-	suspension_notes?: string | null;
+	@IsOptional() retention_days?: number;
+	@IsOptional() is_suspended?: boolean;
+	@IsOptional() suspension_code?: string | null;
+	@IsOptional() suspension_notes?: string | null;
 	/** @format date-time */
-	@IsDate() suspension_date?: string | null;
-	has_premium_features?: boolean;
+	@IsOptional() @IsDate() suspension_date?: string | null;
+	@IsOptional() has_premium_features?: boolean;
 	/** @format int32 */
-	max_users?: number;
+	@IsOptional() max_users?: number;
 	/** @format int32 */
-	max_projects?: number;
+	@IsOptional() max_projects?: number;
 	/** @format int64 */
-	project_count?: number;
+	@IsOptional() project_count?: number;
 	/** @format int64 */
-	stack_count?: number;
+	@IsOptional() stack_count?: number;
 	/** @format int64 */
-	event_count?: number;
-	invites?: Invite[];
-	usage_hours?: UsageHourInfo[];
-	usage?: UsageInfo[];
-	data?: Record<string, unknown>;
-	is_throttled?: boolean;
-	is_over_monthly_limit?: boolean;
-	is_over_request_limit?: boolean;
+	@IsOptional() event_count?: number;
+	@IsOptional() invites?: Invite[];
+	@IsOptional() usage_hours?: UsageHourInfo[];
+	@IsOptional() usage?: UsageInfo[];
+	@IsOptional() data?: Record<string, unknown>;
+	@IsOptional() is_throttled?: boolean;
+	@IsOptional() is_over_monthly_limit?: boolean;
+	@IsOptional() is_over_request_limit?: boolean;
 }
 
 export class ViewProject {
-	id?: string;
+	@IsOptional() @IsMongoId() id?: string;
 	/** @format date-time */
-	@IsDate() created_utc?: string;
-	organization_id?: string;
-	organization_name?: string;
-	name?: string;
-	delete_bot_data_enabled?: boolean;
-	data?: Record<string, unknown>;
-	promoted_tabs?: string[];
-	is_configured?: boolean | null;
+	@IsOptional() @IsDate() created_utc?: string;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() organization_name?: string;
+	@IsOptional() name?: string;
+	@IsOptional() delete_bot_data_enabled?: boolean;
+	@IsOptional() data?: Record<string, unknown>;
+	@IsOptional() promoted_tabs?: string[];
+	@IsOptional() is_configured?: boolean | null;
 	/** @format int64 */
-	stack_count?: number;
+	@IsOptional() stack_count?: number;
 	/** @format int64 */
-	event_count?: number;
-	has_premium_features?: boolean;
-	has_slack_integration?: boolean;
-	usage_hours?: UsageHourInfo[];
-	usage?: UsageInfo[];
+	@IsOptional() event_count?: number;
+	@IsOptional() has_premium_features?: boolean;
+	@IsOptional() has_slack_integration?: boolean;
+	@IsOptional() usage_hours?: UsageHourInfo[];
+	@IsOptional() usage?: UsageInfo[];
 }
 
 export class ViewToken {
-	id?: string;
-	organization_id?: string;
-	project_id?: string;
-	user_id?: string | null;
-	default_project_id?: string | null;
-	scopes?: string[];
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() @IsMongoId() project_id?: string;
+	@IsOptional() @IsMongoId() user_id?: string | null;
+	@IsOptional() @IsMongoId() default_project_id?: string | null;
+	@IsOptional() scopes?: string[];
 	/** @format date-time */
-	@IsDate() expires_utc?: string | null;
-	notes?: string | null;
-	is_disabled?: boolean;
-	is_suspended?: boolean;
+	@IsOptional() @IsDate() expires_utc?: string | null;
+	@IsOptional() notes?: string | null;
+	@IsOptional() is_disabled?: boolean;
+	@IsOptional() is_suspended?: boolean;
 	/** @format date-time */
-	@IsDate() created_utc?: string;
+	@IsOptional() @IsDate() created_utc?: string;
 	/** @format date-time */
-	@IsDate() updated_utc?: string;
+	@IsOptional() @IsDate() updated_utc?: string;
 }
 
 export class ViewUser {
-	id?: string;
-	organization_ids?: string[];
-	full_name?: string;
-	email_address?: string;
-	email_notifications_enabled?: boolean;
-	is_email_address_verified?: boolean;
-	is_active?: boolean;
-	is_invite?: boolean;
-	roles?: string[];
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() organization_ids?: string[];
+	@IsOptional() full_name?: string;
+	@IsOptional() email_address?: string;
+	@IsOptional() email_notifications_enabled?: boolean;
+	@IsOptional() is_email_address_verified?: boolean;
+	@IsOptional() is_active?: boolean;
+	@IsOptional() is_invite?: boolean;
+	@IsOptional() roles?: string[];
 }
 
 export class WebHook {
@@ -477,13 +477,13 @@ export class WebHook {
 		this.version = version;
 	}
 
-	id?: string;
-	organization_id?: string;
-	project_id?: string;
-	url: string;
-	event_types: string[];
-	is_enabled?: boolean;
-	version: string;
+	@IsOptional() @IsMongoId() id?: string;
+	@IsOptional() @IsMongoId() organization_id?: string;
+	@IsOptional() @IsMongoId() project_id?: string;
+	@IsDefined() @IsUrl() url: string;
+	@IsDefined() event_types: string[];
+	@IsOptional() is_enabled?: boolean;
+	@IsDefined() version: string;
 	/** @format date-time */
-	@IsDate() created_utc?: string;
+	@IsOptional() @IsDate() created_utc?: string;
 }
