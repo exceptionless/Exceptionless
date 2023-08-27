7f4bc3e52d432d9209be6e33cf4eda0f4bddbd35
Blake Niemyjski <bniemyjski@gmail.com>
2023-08-27 13:57:37 -0500

    Added default templates
    npx swagger-typescript-api generate-templates -o api-templates

--- src/Exceptionless.Web/Models/Auth/LoginModel.cs
+++ src/Exceptionless.Web/Models/Auth/LoginModel.cs
@@ -9,7 +9,7 @@ public class LoginModelValidator : AbstractValidator<LoginModel>
     public LoginModelValidator()
     {
         RuleFor(u => u.Email).NotEmpty().EmailAddress().WithMessage("Please specify a valid email address.");
-        RuleFor(u => u.Password).MinimumLength(6).WithMessage("Please specify a valid password.");
+        RuleFor(u => u.Password).NotEmpty().MinimumLength(6).MaximumLength(100).WithMessage("Please specify a valid password.");
         RuleFor(u => u.InviteToken).Length(40).When(m => m.InviteToken is not null).WithMessage("Please specify a valid invite token.");
     }
 }
