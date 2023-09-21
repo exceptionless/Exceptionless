2da4cd8bba47432024b2f784d4e87ac73b517005
Blake Niemyjski <bniemyjski@gmail.com>
2023-09-20 22:49:47 -0500

    Added empty table message.

--- src/Exceptionless.Web/ClientApp/src/lib/components/table/Table.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/table/Table.svelte
@@ -51,6 +51,13 @@
 		{/each}
 	</thead>
 	<tbody>
+        {#if !$table.getRowModel().rows.length}
+            <tr>
+                <td colspan={$table.getVisibleLeafColumns().length} class="text-center">
+                    No data was found with the current filter.
+                </td>
+            </tr>
+        {/if}
 		{#each $table.getRowModel().rows as row}
 			<tr
 				class="hover cursor-pointer"
