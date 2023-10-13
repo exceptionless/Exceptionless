bfde132afe80eb99c2f8304847c83f763817c146
Bianco Veigel <bianco@unicorn-development.de>
2023-10-13 18:15:36 +0200

    create vendor.min.css and vendor.min.js

--- src/Exceptionless.Web/ClientApp.angular/grunt/task-configs/uglify.js
+++ src/Exceptionless.Web/ClientApp.angular/grunt/task-configs/uglify.js
@@ -10,4 +10,12 @@ module.exports = {
         src: "dist/app.js",
         dest: "dist/app.min.js",
     },
+    vendor: {
+        src: [
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/bootstrap/dist/js/bootstrap.min.js",
+            "node_modules/lodash/lodash.min.js",
+        ],
+        dest: "dist/vendor.min.js",
+    },
 };
