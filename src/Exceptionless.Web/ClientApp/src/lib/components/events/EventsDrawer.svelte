1c8ecce5dc2e8a8e990100062a305d79c1bd7fa3
Blake Niemyjski <bniemyjski@gmail.com>
2023-10-13 21:43:24 -0500

    rearranged loading rendering order in component due to global loader.

--- src/Exceptionless.Web/ClientApp/src/lib/components/events/EventsDrawer.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/events/EventsDrawer.svelte
@@ -18,14 +18,14 @@
 	loadData();
 </script>
 
-{#if $loading}
-	<p>Loading...</p>
-{:else if response?.data}
+{#if response?.data}
 	<h1 class="text-xl">Event Details</h1>
 	<Overview event={response.data}></Overview>
 	<div class="flex justify-center mt-2">
 		<a href="/next/event/{id}" class="btn btn-primary btn-sm"> View Event </a>
 	</div>
+{:else if $loading}
+	<p>Loading...</p>
 {:else}
 	<ErrorMessage message={response?.problem?.errors.general}></ErrorMessage>
 {/if}
