27d2e3c0dc1e1965005b1c1186f063bc2cfb6046
Blake Niemyjski <bniemyjski@gmail.com>
2023-10-16 09:13:10 -0500

    Fixed issue with font awesome fonts not resolving after vendor bundle

--- src/Exceptionless.Web/ClientApp.angular/grunt/task-configs/copy.js
+++ src/Exceptionless.Web/ClientApp.angular/grunt/task-configs/copy.js
@@ -1,5 +1,8 @@
 module.exports = {
     main: {
-        files: [{ src: ["app.config.js", "*.png", "*.ico", "img/**/{*.png,*.jpg,*.ico}", "lang/**"], dest: "dist/" }],
+        files: [
+            { src: ["app.config.js", "*.png", "*.ico", "img/**/{*.png,*.jpg,*.ico}", "lang/**"], dest: "dist/" },
+            { src: ["node_modules/font-awesome/fonts/**"], dest: "dist/fonts/", expand: true, flatten: true, filter: "isFile" },
+        ],
     },
 };
