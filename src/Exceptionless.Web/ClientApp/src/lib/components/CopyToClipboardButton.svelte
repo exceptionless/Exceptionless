595e955ea00a47ae651917cc0c6a5aef9cadf4d1
Blake Niemyjski <bniemyjski@gmail.com>
2023-10-21 07:55:00 -0500

    Added copy to clipboard button component

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/CopyToClipboardButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import IconContentCopy from '~icons/mdi/content-copy';
+	import { clickToCopyAction } from 'svelte-legos';
+
+	export let title: string = 'Copy to Clipboard';
+	export let value: string | null | undefined;
+
+	function handleCopyDone() {
+		console.log('Copy to clipboard succeeded');
+	}
+
+	function handleCopyError() {
+		console.error('Copy to clipboard failed');
+	}
+</script>
+
+<button
+	{title}
+	use:clickToCopyAction={() => value || ''}
+	on:copy-done={handleCopyDone}
+	on:copy-error={handleCopyError}
+>
+	<slot><IconContentCopy /></slot>
+</button>
