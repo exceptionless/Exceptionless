e9813e5b18273a77c657f009fb79252001306844
Blake Niemyjski <bniemyjski@gmail.com>
2023-10-30 22:13:25 -0500

    Added tabs to the event drawer

--- src/Exceptionless.Web/ClientApp/src/lib/components/events/EventsDrawer.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/events/EventsDrawer.svelte
@@ -5,14 +5,44 @@
 		globalLoading as loading
 	} from '$api/FetchClient';
 	import ErrorMessage from '$comp/ErrorMessage.svelte';
+	import { hasErrorOrSimpleError } from '$lib/helpers/persistent-event';
 	import type { PersistentEvent } from '$lib/models/api';
+	import { writable, type Writable } from 'svelte/store';
+	import Error from './views/Error.svelte';
 	import Overview from './views/Overview.svelte';
 
 	export let id: string;
 	let response: FetchClientResponse<PersistentEvent>;
 
+	type TabType = 'Overview' | 'Exception' | string | null;
+	let activeTab: TabType = null;
+	const tabs: Writable<TabType[]> = writable([]);
+	tabs.subscribe((items) => {
+		if (!items) {
+			activeTab = null;
+		}
+
+		if (!items.includes(activeTab)) {
+			activeTab = items[0];
+		}
+	});
+
+	function getTabs(event?: PersistentEvent | null): TabType[] {
+		if (!event) {
+			return [];
+		}
+
+		const tabs = ['Overview'];
+		if (hasErrorOrSimpleError(event)) {
+			tabs.push('Exception');
+		}
+
+		return tabs;
+	}
+
 	async function loadData() {
 		response = await api.getJSON<PersistentEvent>(`events/${id}`);
+		tabs.set(getTabs(response?.data));
 	}
 
 	loadData();
@@ -20,9 +50,28 @@
 
 {#if response?.data}
 	<h1 class="text-xl">Event Details</h1>
-	<Overview event={response.data}></Overview>
+
+	<div class="tabs mt-4">
+		{#each $tabs as tab}
+			<button
+				class="tab tab-bordered"
+				class:tab-active={activeTab === tab}
+				on:click={() => (activeTab = tab)}
+				title="Select {tab}">{tab}</button
+			>
+		{/each}
+	</div>
+
+	<div class="border mt-2">
+		{#if activeTab === 'Overview'}
+			<Overview event={response.data}></Overview>
+		{:else if activeTab === 'Exception'}
+			<Error event={response.data}></Error>
+		{/if}
+	</div>
+
 	<div class="flex justify-center mt-2">
-		<a href="/event/{id}" class="btn btn-primary btn-sm"> View Event </a>
+		<a href="/event/{id}" class="btn btn-primary btn-sm">View Event</a>
 	</div>
 {:else if $loading}
 	<p>Loading...</p>
--- src/Exceptionless.Web/ClientApp/src/lib/components/events/views/Overview.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/events/views/Overview.svelte
@@ -11,7 +11,8 @@
 		getMessage,
 		getRequestInfoPath,
 		getRequestInfoUrl,
-		getStackTrace
+		getStackTrace,
+		hasErrorOrSimpleError
 	} from '$lib/helpers/persistent-event';
 	import SimpleStackTrace from '../SimpleStackTrace.svelte';
 	import StackTrace from '../StackTrace.svelte';
@@ -27,14 +28,14 @@
 	export let event: PersistentEvent;
 	//let project: ViewProject = {}; // TODO
 
-	const hasError = !!event.data?.['@error'] || !!event.data?.['@simple_error'];
+	const hasError = hasErrorOrSimpleError(event);
 	const errorType = hasError ? getErrorType(event) : null;
 	const stackTrace = hasError ? getStackTrace(event) : null;
 
 	const isSessionStart = event.type === 'session';
 	//let referenceId = isSessionStart ? event.reference_id : null;
 
-	const message = event.message || getMessage(event);
+	const message = getMessage(event);
 	let references: { id: string; name: string }[] = [];
 	const referencePrefix = '@ref:';
 	Object.entries(event.data || {}).forEach(([key, value]) => {
@@ -63,208 +64,213 @@
 </script>
 
 <table class="table table-zebra table-xs border">
-	<tr>
-		<th class="whitespace-nowrap">Occurred On</th>
-		<td
-			><ClickableDateFilter term="date" value={event.date}
-				><DateTime value={event.date}></DateTime> (<TimeAgo value={event.date}
-				></TimeAgo>)</ClickableDateFilter
-			></td
-		>
-	</tr>
-	{#if isSessionStart}
+	<tbody>
 		<tr>
-			<th class="whitespace-nowrap">Duration</th>
-			<td>
-				{#if !event.data?.sessionend}
-					<span class="text-green-500">•</span>
-				{/if}
-				<Duration value={event.date}></Duration>
-				{#if event.data?.sessionend}
-					(ended <TimeAgo value={event.data.sessionend}></TimeAgo>)
-				{/if}
-			</td>
+			<th class="whitespace-nowrap">Occurred On</th>
+			<td
+				><ClickableDateFilter term="date" value={event.date}
+					><DateTime value={event.date}></DateTime> (<TimeAgo value={event.date}
+					></TimeAgo>)</ClickableDateFilter
+				></td
+			>
 		</tr>
-	{/if}
-	<!-- <tr>
+		{#if isSessionStart}
+			<tr>
+				<th class="whitespace-nowrap">Duration</th>
+				<td>
+					{#if !event.data?.sessionend}
+						<span class="text-green-500">•</span>
+					{/if}
+					<Duration value={event.date}></Duration>
+					{#if event.data?.sessionend}
+						(ended <TimeAgo value={event.data.sessionend}></TimeAgo>)
+					{/if}
+				</td>
+			</tr>
+		{/if}
+		<!-- <tr>
 		<th class="whitespace-nowrap">Project</th>
 		<td><a href="/app.project-frequent/{event.project_id}">{project.name}</a></td>
 	</tr> -->
-	{#if event.reference_id}
-		<tr>
-			<th class="whitespace-nowrap">Reference</th>
-			<td>
-				{#if isSessionStart}
-					<ClickableSessionFilter sessionId={event.reference_id}
-						>{event.reference_id}</ClickableSessionFilter
-					>
-				{:else}
-					<ClickableReferenceFilter referenceId={event.reference_id}
-						>{event.reference_id}</ClickableReferenceFilter
-					>
-				{/if}
-			</td>
-		</tr>
-	{/if}
-	{#each references as reference (reference.id)}
-		<tr>
-			<th class="whitespace-nowrap">{reference.name}</th>
-			<td
-				><ClickableReferenceFilter referenceId={reference.id}
-					>{reference.id}</ClickableReferenceFilter
-				></td
-			>
-		</tr>
-	{/each}
-	{#if level}
-		<tr>
-			<th class="whitespace-nowrap">Level</th>
-			<td
-				><ClickableStringFilter term="level" value={level}
-					><LogLevel {level}></LogLevel></ClickableStringFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if event.type !== 'error'}
-		<tr>
-			<th class="whitespace-nowrap">Event Type</th>
-			<td
-				><ClickableStringFilter term="type" value={event.type}
-					>{event.type}</ClickableStringFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if hasError}
-		<tr>
-			<th class="whitespace-nowrap">Error Type</th>
-			<td
-				><ClickableStringFilter term="error.type" value={errorType}
-					>{errorType}</ClickableStringFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if event.source}
-		<tr>
-			<th class="whitespace-nowrap">Source</th>
-			<td
-				><ClickableStringFilter term="source" value={event.source}
-					>{event.source}</ClickableStringFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if !isSessionStart && event.value}
-		<tr>
-			<th class="whitespace-nowrap">Value</th>
-			<td
-				><ClickableNumberFilter term="value" value={event.value}
-					>{event.value}</ClickableNumberFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if message}
-		<tr>
-			<th class="whitespace-nowrap">Message</th>
-			<td
-				><ClickableStringFilter term="message" value={message}
-					>{message}</ClickableStringFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if version}
-		<tr>
-			<th class="whitespace-nowrap">Version</th>
-			<td
-				><ClickableVersionFilter term="version" value={version}
-					>{version}</ClickableVersionFilter
-				></td
-			>
-		</tr>
-	{/if}
-	{#if location}
-		<tr>
-			<th class="whitespace-nowrap">Geo</th>
-			<td>{location}</td>
-		</tr>
-	{/if}
-	{#if event.tags?.length}
-		<tr>
-			<th class="whitespace-nowrap">Tags</th>
-			<td class="flex flex-wrap justify-start gap-2 overflow-auto">
-				{#each event.tags as tag}
-					<ClickableStringFilter term="tag" value={tag}
-						><div class="badge badge-neutral">{tag}</div></ClickableStringFilter
-					>
-				{/each}
-			</td>
-		</tr>
-	{/if}
-	{#if requestUrl}
-		<tr>
-			<th class="whitespace-nowrap">URL</th>
-			<td>
-				<ClickableStringFilter term="path" value={requestUrlPath}
-					>{requestUrl}</ClickableStringFilter
+		{#if event.reference_id}
+			<tr>
+				<th class="whitespace-nowrap">Reference</th>
+				<td>
+					{#if isSessionStart}
+						<ClickableSessionFilter sessionId={event.reference_id}
+							>{event.reference_id}</ClickableSessionFilter
+						>
+					{:else}
+						<ClickableReferenceFilter referenceId={event.reference_id}
+							>{event.reference_id}</ClickableReferenceFilter
+						>
+					{/if}
+				</td>
+			</tr>
+		{/if}
+		{#each references as reference (reference.id)}
+			<tr>
+				<th class="whitespace-nowrap">{reference.name}</th>
+				<td
+					><ClickableReferenceFilter referenceId={reference.id}
+						>{reference.id}</ClickableReferenceFilter
+					></td
 				>
+			</tr>
+		{/each}
+		{#if level}
+			<tr>
+				<th class="whitespace-nowrap">Level</th>
+				<td
+					><ClickableStringFilter term="level" value={level}
+						><LogLevel {level}></LogLevel></ClickableStringFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if event.type !== 'error'}
+			<tr>
+				<th class="whitespace-nowrap">Event Type</th>
+				<td
+					><ClickableStringFilter term="type" value={event.type}
+						>{event.type}</ClickableStringFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if hasError}
+			<tr>
+				<th class="whitespace-nowrap">Error Type</th>
+				<td
+					><ClickableStringFilter term="error.type" value={errorType}
+						>{errorType}</ClickableStringFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if event.source}
+			<tr>
+				<th class="whitespace-nowrap">Source</th>
+				<td
+					><ClickableStringFilter term="source" value={event.source}
+						>{event.source}</ClickableStringFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if !isSessionStart && event.value}
+			<tr>
+				<th class="whitespace-nowrap">Value</th>
+				<td
+					><ClickableNumberFilter term="value" value={event.value}
+						>{event.value}</ClickableNumberFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if message}
+			<tr>
+				<th class="whitespace-nowrap">Message</th>
+				<td
+					><ClickableStringFilter term="message" value={message}
+						>{message}</ClickableStringFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if version}
+			<tr>
+				<th class="whitespace-nowrap">Version</th>
+				<td
+					><ClickableVersionFilter term="version" value={version}
+						>{version}</ClickableVersionFilter
+					></td
+				>
+			</tr>
+		{/if}
+		{#if location}
+			<tr>
+				<th class="whitespace-nowrap">Geo</th>
+				<td>{location}</td>
+			</tr>
+		{/if}
+		{#if event.tags?.length}
+			<tr>
+				<th class="whitespace-nowrap">Tags</th>
+				<td class="flex flex-wrap justify-start gap-2 overflow-auto">
+					{#each event.tags as tag}
+						<ClickableStringFilter term="tag" value={tag}
+							><div class="badge badge-neutral">{tag}</div></ClickableStringFilter
+						>
+					{/each}
+				</td>
+			</tr>
+		{/if}
+		{#if requestUrl}
+			<tr>
+				<th class="whitespace-nowrap">URL</th>
+				<td>
+					<ClickableStringFilter term="path" value={requestUrlPath}
+						>{requestUrl}</ClickableStringFilter
+					>
 
-				<a href={requestUrl} target="_blank" class="link" title="Open in new window"
-					><IconOpenInNew /></a
-				></td
-			>
-		</tr>
-	{/if}
+					<a href={requestUrl} target="_blank" class="link" title="Open in new window"
+						><IconOpenInNew /></a
+					></td
+				>
+			</tr>
+		{/if}
+	</tbody>
 </table>
 
 {#if userEmail || userIdentity || userName || userDescription}
 	<h4 class="text-lg">User Info</h4>
 	<table class="table table-zebra table-xs border">
-		{#if userEmail}
-			<tr>
-				<th class="whitespace-nowrap">User Email</th>
-				<td
-					><ClickableStringFilter term="user.email" value={userEmail}
-						>{userEmail}</ClickableStringFilter
+		<tbody>
+			{#if userEmail}
+				<tr>
+					<th class="whitespace-nowrap">User Email</th>
+					<td
+						><ClickableStringFilter term="user.email" value={userEmail}
+							>{userEmail}</ClickableStringFilter
+						>
+						<a href="mailto:{userEmail}" title="Send email to {userEmail}"
+							><IconEmail /></a
+						></td
 					>
-					<a href="mailto:{userEmail}" title="Send email to {userEmail}"><IconEmail /></a
-					></td
-				>
-			</tr>
-		{/if}
-		{#if userIdentity}
-			<tr>
-				<th class="whitespace-nowrap">User Identity</th>
-				<td
-					><ClickableStringFilter term="user" value={userIdentity}
-						>{userIdentity}</ClickableStringFilter
-					></td
-				>
-			</tr>
-		{/if}
-		{#if userName}
-			<tr>
-				<th class="whitespace-nowrap">User Name</th>
-				<td
-					><ClickableStringFilter term="user.name" value={userName}
-						>{userName}</ClickableStringFilter
-					></td
-				>
-			</tr>
-		{/if}
-		{#if userDescription}
-			<tr>
-				<th class="whitespace-nowrap">User Description</th>
-				<td
-					><ClickableStringFilter term="user.description" value={userDescription}
-						>{userDescription}</ClickableStringFilter
-					></td
-				>
-			</tr>
-		{/if}
+				</tr>
+			{/if}
+			{#if userIdentity}
+				<tr>
+					<th class="whitespace-nowrap">User Identity</th>
+					<td
+						><ClickableStringFilter term="user" value={userIdentity}
+							>{userIdentity}</ClickableStringFilter
+						></td
+					>
+				</tr>
+			{/if}
+			{#if userName}
+				<tr>
+					<th class="whitespace-nowrap">User Name</th>
+					<td
+						><ClickableStringFilter term="user.name" value={userName}
+							>{userName}</ClickableStringFilter
+						></td
+					>
+				</tr>
+			{/if}
+			{#if userDescription}
+				<tr>
+					<th class="whitespace-nowrap">User Description</th>
+					<td
+						><ClickableStringFilter term="user.description" value={userDescription}
+							>{userDescription}</ClickableStringFilter
+						></td
+					>
+				</tr>
+			{/if}</tbody
+		>
 	</table>
 {/if}
 
