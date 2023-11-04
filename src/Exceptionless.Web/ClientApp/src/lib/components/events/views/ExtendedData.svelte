922a84c7ada4313ece4255b7d6cb8a04e80599e4
Blake Niemyjski <bniemyjski@gmail.com>
2023-11-04 14:17:44 -0500

    WIP: Promoting and Demoting tabs

--- src/Exceptionless.Web/ClientApp/src/lib/components/events/views/ExtendedData.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/events/views/ExtendedData.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
+	import { toast } from 'svoast';
 	import type { PersistentEvent, ViewProject } from '$lib/models/api';
 	import ExtendedDataItem from '../ExtendedDataItem.svelte';
 	import { getExtendedDataItems } from '$lib/helpers/persistent-event';
+	import { mutatePromoteTab } from '$api/queries/projects';
 
 	export let event: PersistentEvent;
 	export let project: ViewProject | undefined;
 
 	const items = getExtendedDataItems(event, project);
+
+	const promoteTab = mutatePromoteTab(event.project_id ?? '');
+	promoteTab.subscribe((response) => {
+        if (response.isError) {
+		    toast.error(`An error occurred promoting tab ${response.variables.name}`);
+        }
+	});
+
+	function onPromote({ detail }: CustomEvent<string>): void {
+		$promoteTab.mutate({ name: detail });
+	}
 </script>
 
-{#each items as { title, promoted, data }}
-	{#if promoted === false}
-		<ExtendedDataItem {title} {data}></ExtendedDataItem>
-	{/if}
-{/each}
+<div class="space-y-4">
+	{#each items as { title, promoted, data }}
+		{#if promoted === false}
+			<div data-id={title}>
+				<ExtendedDataItem {title} {data} on:promote={onPromote}></ExtendedDataItem>
+			</div>
+		{/if}
+	{/each}
+</div>
--- src/Exceptionless.Web/ClientApp/src/lib/components/events/views/PromotedExtendedData.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/events/views/PromotedExtendedData.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+	import { toast } from 'svoast';
+	import { mutateDemoteTab } from "$api/queries/projects";
 	import type { PersistentEvent } from '$lib/models/api';
 	import ExtendedDataItem from '../ExtendedDataItem.svelte';
 
 	export let event: PersistentEvent;
 	export let title: string;
+
+    const demoteTab = mutateDemoteTab(event.project_id ?? '');
+	demoteTab.subscribe((response) => {
+        if (response.isError) {
+		    toast.error(`An error occurred demoting tab ${response.variables.name}`);
+        }
+	});
+
+	function onDemote({ detail }: CustomEvent<string>): void {
+		$demoteTab.mutate({ name: detail });
+	}
 </script>
 
-<ExtendedDataItem {title} isPromoted={true} data={event.data?.[title]}></ExtendedDataItem>
+<ExtendedDataItem {title} isPromoted={true} data={event.data?.[title]} on:demote={onDemote}
+></ExtendedDataItem>
