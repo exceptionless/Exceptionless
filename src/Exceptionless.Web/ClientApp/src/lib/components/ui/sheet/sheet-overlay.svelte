9738e4131231fae69fda1866a0688c497ec905bb
Blake Niemyjski <bniemyjski@gmail.com>
2023-11-16 21:01:39 -0600

    Added more controls

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/ui/sheet/sheet-overlay.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Dialog as SheetPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils";
+	import { fade } from "svelte/transition";
+
+	type $$Props = SheetPrimitive.OverlayProps;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+	export let transition: $$Props["transition"] = fade;
+	export let transitionConfig: $$Props["transitionConfig"] = {
+		duration: 150
+	};
+</script>
+
+<SheetPrimitive.Overlay
+	{transition}
+	{transitionConfig}
+	class={cn(
+		"fixed inset-0 z-50 bg-background/80 backdrop-blur-sm",
+		className
+	)}
+	{...$$restProps}
+/>
