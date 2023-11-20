991d626ef24af83cf3eff6e9eace28dd2695e78b
Blake Niemyjski <bniemyjski@gmail.com>
2023-11-20 07:39:39 -0600

    WIP Shadcn

--- src/Exceptionless.Web/ClientApp/src/lib/components/DateRangeDropdown.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/DateRangeDropdown.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Select } from '$comp/ui/select';
+	import * as Select from '$comp/ui/select';
 	export let value: string;
 </script>
 
--- src/Exceptionless.Web/ClientApp/src/lib/utils.ts
+++ src/Exceptionless.Web/ClientApp/src/lib/utils.ts
@@ -3,6 +3,8 @@ import { twMerge } from 'tailwind-merge';
 import { cubicOut } from 'svelte/easing';
 import type { TransitionConfig } from 'svelte/transition';
 
+export const nameof = <T>(name: keyof T) => name;
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
