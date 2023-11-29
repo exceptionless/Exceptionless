15cfb24f2ea1cfeaa0ee565acf3a3ba4c7b14607
Blake Niemyjski <bniemyjski@gmail.com>
2023-11-29 07:24:38 -0600

    Fixed linting rules
    https://github.com/huntabyte/shadcn-svelte/issues/489

--- src/Exceptionless.Web/ClientApp/src/lib/components/ui/tabs/tabs-trigger.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/ui/tabs/tabs-trigger.svelte
@@ -3,7 +3,6 @@
 	import { cn } from '$lib/utils';
 
 	type $$Props = TabsPrimitive.TriggerProps;
-	type $$Events = TabsPrimitive.TriggerEvents;
 
 	let className: $$Props['class'] = undefined;
 	export let value: $$Props['value'];
