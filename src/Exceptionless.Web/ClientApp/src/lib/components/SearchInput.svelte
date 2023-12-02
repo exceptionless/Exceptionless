14196cfc0adca6c1ff2739cb10c57dea01960737
Blake Niemyjski <bniemyjski@gmail.com>
2023-12-02 15:51:28 -0600

    Greatly improved theme colors

--- src/Exceptionless.Web/ClientApp/src/lib/components/SearchInput.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/SearchInput.svelte
@@ -11,7 +11,7 @@
 
 <label for={id} class="sr-only">Search</label>
 <div class="relative">
-	<div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+	<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
 		<IconSearch class="w-5 h-5" />
 	</div>
 	<Input
--- src/Exceptionless.Web/ClientApp/src/lib/layouts/Sidebar.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/layouts/Sidebar.svelte
@@ -26,18 +26,18 @@
 
 <aside
 	id="sidebar"
-	class="bg-secondary text-secondary-foreground flex fixed top-0 left-0 z-20 flex-col flex-shrink-0 pt-16 w-64 h-full duration-75 lg:flex transition-width {$isSidebarOpen
+	class="flex fixed top-0 left-0 z-20 flex-col flex-shrink-0 pt-16 w-64 h-full duration-75 lg:flex transition-width {$isSidebarOpen
 		? 'lg:w-64'
 		: 'lg:w-16 hidden'}"
 	aria-label="Sidebar"
 >
 	<div
-		class="flex relative flex-col flex-1 pt-0 min-h-0 border-r"
+		class="relative flex flex-col flex-1 min-h-0 pt-0 border-r"
 		on:mouseenter={() => onSidebarMouseEnter()}
 		on:mouseleave={() => onSidebarMouseLeave()}
 		role="none"
 	>
-		<div class="flex overflow-y-auto flex-col flex-1 pt-5 pb-4">
+		<div class="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
 			<div class="flex-1 px-3 space-y-1 divide-y">
 				<ul class="pb-2 space-y-2">
 					<li>
@@ -48,9 +48,11 @@
 					<li>
 						<a
 							href="/next"
-							class="flex items-center p-2 text-base font-normal rounded-lg group"
+							class="flex items-center p-2 text-base font-normal rounded-lg group hover:bg-accent hover:text-accent-foreground"
 						>
-							<IconChartPie class="w-6 h-6 transition duration-75 " />
+							<IconChartPie
+								class="w-6 h-6 transition duration-75 text-muted-foreground group-hover:text-foreground"
+							/>
 							<span
 								class="ml-3 {!$isSidebarOpen && $isLargeScreen
 									? 'lg:hidden lg:absolute'
