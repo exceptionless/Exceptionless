424befd43707025dc45b32b9c753d1cfc3288ea4
Eric J. Smith <eric@ericjsmith.com>
2023-12-02 22:36:04 -0600

    Some dark theme color tweaks

--- src/Exceptionless.Web/ClientApp/src/lib/components/ui/card/card.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/ui/card/card.svelte
@@ -10,7 +10,7 @@
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-	class={cn('rounded-xl border bg-card text-card-foreground shadow', className)}
+	class={cn('rounded-lg border bg-card text-card-foreground shadow', className)}
 	{...$$restProps}
 	on:click
 	on:focusin
