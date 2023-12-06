b8b6a5e644a7df826298d5b037259ca05ad813e0
Blake Niemyjski <bniemyjski@gmail.com>
2023-12-06 08:06:24 -0600

    Added typography components
    https://www.shadcn-svelte.com/docs/typography

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/typography/A.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils';
+
+	let className: HTMLAnchorAttributes['class'] = undefined;
+	export { className as class };
+	export let href: HTMLAnchorAttributes['href'] = undefined;
+</script>
+
+<a
+    {href}
+	class={cn('underline underline-offset-4 hover:text-primary', className)}
+	{...$$restProps}
+>
+	<slot />
+</a>
---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/typography/H3.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<h3
+	class={cn(
+		'scroll-m-20 text-2xl font-semibold tracking-tight',
+		className
+	)}
+	{...$$restProps}
+>
+	<slot />
+</h3>
