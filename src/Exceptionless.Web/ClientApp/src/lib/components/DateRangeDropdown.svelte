4d0f4e702587907753da2cb66b0ef66d85b3f6c4
Blake Niemyjski <bniemyjski@gmail.com>
2023-12-09 09:58:02 -0600

    Fixed date range dropdown.

--- src/Exceptionless.Web/ClientApp/src/lib/components/DateRangeDropdown.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/DateRangeDropdown.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
 	import * as Select from '$comp/ui/select';
+
 	export let value: string;
+
+	const items = [
+		{ value: 'last hour', label: 'Last Hour' },
+		{ value: 'last 24 hours', label: 'Last 24 Hours' },
+		{ value: 'last week', label: 'Last Week' },
+		{ value: 'last 30 days', label: 'Last 30 Days' },
+		{ value: '', label: 'All Time' }
+	];
+
+	let selected = items.find((item) => item.value === value) || items[items.length - 1];
+
+	function onSelectedChange(item: { value: string; label: string }) {
+		value = item.value;
+	}
 </script>
 
-<Select.Root bind:selected={value}>
+<Select.Root {items} bind:selected {onSelectedChange}>
 	<Select.Trigger class="w-[180px]">
 		<Select.Value placeholder="" />
 	</Select.Trigger>
 	<Select.Content>
-		<Select.Item value="last hour">Last Hour</Select.Item>
-		<Select.Item value="last 24 hours">Last 24 Hours</Select.Item>
-		<Select.Item value="last week">Last Week</Select.Item>
-		<Select.Item value="last 30 days">Last 30 Days</Select.Item>
-		<Select.Item value="">All Time</Select.Item>
+		{#each items as item}
+			<Select.Item value={item.value}>{item.label}</Select.Item>
+		{/each}
 	</Select.Content>
 </Select.Root>
