1ac7f9e7fb05f7815b96ab3da9938ddc4bea25e7
Eric J. Smith <eric@ericjsmith.com>
2023-12-13 09:40:22 -0600

    Change docker port

--- samples/hosting-samples.sh
+++ samples/hosting-samples.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # simple instance for testing
-docker run --rm -it -p 5200:80 exceptionless/exceptionless:latest
+docker run --rm -it -p 5200:8080 exceptionless/exceptionless:latest
 
 # persist data
-docker run --rm -it -p 5200:80 \
+docker run --rm -it -p 5200:8080 \
     -v ~/esdata:/usr/share/elasticsearch/data \
     exceptionless/exceptionless:latest
 
 # persist data, use ssl, enable mail sending
-docker run --rm -it -p 5200:80 -p 5201:443 \
+docker run --rm -it -p 5200:8080 -p 5201:443 \
     -e EX_ConnectionStrings__Email=smtps://user:password@smtp.host.com:587 \
     -e ASPNETCORE_URLS="https://+;http://+" \
     -e ASPNETCORE_HTTPS_PORT=5201 \
