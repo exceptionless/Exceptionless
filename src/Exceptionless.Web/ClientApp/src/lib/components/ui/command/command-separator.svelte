1fc4e796522a78d0ba1f67cad3da81fc33e7a903
Blake Niemyjski <bniemyjski@gmail.com>
2023-12-16 15:45:18 -0600

    Updated shadcn components, added new shad components

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/ui/command/command-separator.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Command as CommandPrimitive } from 'cmdk-sv';
+	import { cn } from '$lib/utils';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<CommandPrimitive.Separator class={cn('-mx-1 h-px bg-border', className)} {...$$restProps} />
--- src/Exceptionless.Web/ClientApp/src/lib/components/ui/context-menu/context-menu-checkbox-item.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/components/ui/context-menu/context-menu-checkbox-item.svelte
@@ -27,7 +27,7 @@
 >
 	<span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
 		<ContextMenuPrimitive.CheckboxIndicator>
-			<Check class="w-4 h-4" />
+			<Check class="h-4 w-4" />
 		</ContextMenuPrimitive.CheckboxIndicator>
 	</span>
 	<slot />
