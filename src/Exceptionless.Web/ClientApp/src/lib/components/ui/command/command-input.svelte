8cc2e1728ce1e940700b45ef2542f0ed394884a7
Blake Niemyjski <bniemyjski@gmail.com>
2023-12-16 15:45:18 -0600

    Updated shadcn components, added new shad components

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/ui/command/command-input.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Command as CommandPrimitive } from 'cmdk-sv';
+	import { MagnifyingGlass } from 'radix-icons-svelte';
+	import { cn } from '$lib/utils';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+	export let value: string = '';
+</script>
+
+<div class="flex items-center px-3 border-b" data-cmdk-input-wrapper="">
+	<MagnifyingGlass class="w-4 h-4 mr-2 opacity-50 shrink-0" />
+	<CommandPrimitive.Input
+		class={cn(
+			'flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
+			className
+		)}
+		{...$$restProps}
+		bind:value
+	/>
+</div>
