d54b02be7f80774642523a02cd77ee2266662e06
Blake Niemyjski <bniemyjski@gmail.com>
2024-01-21 14:37:15 -0600

    Early WIP of what my account page could look like (missing external logins and password forms and more)

---
+++ src/Exceptionless.Web/ClientApp/src/routes/(app)/account/manage/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import * as Card from '$comp/ui/card';
+	import { Separator } from '$comp/ui/separator';
+	import SidebarNav from './(components)/sidebar-nav.svelte';
+
+	const sidebarNavItems = [
+		{
+			title: 'Account',
+			href: '/next/account/manage'
+		},
+		{
+			title: 'Appearance',
+			href: '/next/account/manage/appearance'
+		},
+		{
+			title: 'Notifications',
+			href: '/next/account/manage/notifications'
+		}
+	];
+</script>
+
+<svelte:head>
+	<title>My Account - Exceptionless</title>
+</svelte:head>
+
+<Card.Root>
+	<Card.Title tag="h2" class="p-6 pb-0 text-2xl">Settings</Card.Title>
+	<Card.Description class="pl-6"
+		>Manage your account settings and set e-mail preferences.</Card.Description
+	>
+
+	<Separator class="w-auto mx-6 my-6" />
+
+	<Card.Content>
+		<div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+			<aside class="-mx-4 lg:w-1/5">
+				<SidebarNav items={sidebarNavItems} />
+			</aside>
+			<div class="flex-1">
+				<slot />
+			</div>
+		</div>
+	</Card.Content>
+</Card.Root>
