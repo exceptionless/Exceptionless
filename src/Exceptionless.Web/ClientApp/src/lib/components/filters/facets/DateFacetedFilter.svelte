3572307d98050712637d679c9ecadaa458a8c1ab
Blake Niemyjski <bniemyjski@gmail.com>
2024-03-11 20:45:04 -0500

    Added Date Range filter (WIP)

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/filters/facets/DateFacetedFilter.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { DropdownFacetedFilter } from '$comp/faceted-filter';
+    import { DateFilter } from '$comp/filters/filters';
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    export let filter: DateFilter;
+    export let title: string = 'Date Range';
+
+    const options = [
+        { value: 'last hour', label: 'Last Hour' },
+        { value: 'last 24 hours', label: 'Last 24 Hours' },
+        { value: 'last week', label: 'Last Week' },
+        { value: 'last 30 days', label: 'Last 30 Days' },
+        { value: '', label: 'All Time' }
+    ];
+
+    let value = filter.value as string;
+    function onChanged() {
+        filter.value = value;
+        dispatch('changed', filter);
+    }
+
+    function onRemove() {
+        filter.value = value;
+        dispatch('remove', filter);
+    }
+</script>
+
+<DropdownFacetedFilter {title} bind:value {options} on:changed={onChanged} on:remove={onRemove}></DropdownFacetedFilter>
