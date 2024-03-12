84f38d770d1d0fbca28c298fdd6b32c2d2823c1f
Blake Niemyjski <bniemyjski@gmail.com>
2024-03-11 20:08:07 -0500

    Rearranged filters

--- src/Exceptionless.Web/ClientApp/src/lib/api/projectsApi.ts
+++ src/Exceptionless.Web/ClientApp/src/lib/api/projectsApi.ts
@@ -6,6 +6,8 @@ import { FetchClient, type FetchClientResponse, type ProblemDetails } from '$api
 export const queryKeys = {
     all: ['Project'] as const,
     allWithFilters: (filters: string) => [...queryKeys.all, { filters }] as const,
+    organization: (id: string | null) => [...queryKeys.all, 'organization', id] as const,
+    organizationWithFilters: (id: string | null, filters: string) => [...queryKeys.organization(id), { filters }] as const,
     id: (id: string | null) => [...queryKeys.all, id] as const
 };
 
@@ -22,6 +24,37 @@ export function getProjectByIdQuery(id: string | Readable<string | null>) {
                 });
 
                 if (response.ok) {
+                    return response.data!;
+                }
+
+                throw response.problem;
+            }
+        }))
+    );
+}
+
+export function getProjectsByOrganizationIdQuery(organizationId: string | Readable<string | null>, limit: number = 1000) {
+    const queryClient = useQueryClient();
+    const readableOrganizationId = typeof organizationId === 'string' || organizationId === null ? readable(organizationId) : organizationId;
+    return createQuery<ViewProject[], ProblemDetails>(
+        derived(readableOrganizationId, ($id) => ({
+            enabled: !!$id,
+            queryClient,
+            queryKey: queryKeys.organization($id),
+            queryFn: async ({ signal }: { signal: AbortSignal }) => {
+                const { getJSON } = new FetchClient();
+                const response = await getJSON<ViewProject[]>(`organizations/${$id}/projects`, {
+                    signal,
+                    params: {
+                        limit
+                    }
+                });
+
+                if (response.ok) {
+                    response.data?.forEach((project) => {
+                        queryClient.setQueryData(queryKeys.id(project.id!), project);
+                    });
+
                     return response.data!;
                 }
 
