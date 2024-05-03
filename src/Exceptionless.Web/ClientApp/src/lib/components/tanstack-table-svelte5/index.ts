3f23790baa52aa78876283995b29d7c6ce632424
Blake Niemyjski <bniemyjski@gmail.com>
2024-05-03 13:43:45 -0500

    added temp tanstack svelte5 adaptor from pr

---
+++ src/Exceptionless.Web/ClientApp/src/lib/components/tanstack-table-svelte5/index.ts
@@ -0,0 +1,4 @@
+export * from '@tanstack/table-core';
+export { default as FlexRender } from './flex-render.svelte';
+export { renderComponent } from './render-component';
+export { createSvelteTable } from './table.svelte';
