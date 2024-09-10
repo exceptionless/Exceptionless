34cc31da47b02a0269e75ba026163c1f98cb042f
Blake Niemyjski <bniemyjski@gmail.com>
2024-09-10 08:31:13 -0500

    Updated shadcn

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/badge/index.ts
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/badge/index.ts
@@ -2,12 +2,12 @@ import { type VariantProps, tv } from 'tailwind-variants';
 
 export { default as Badge } from './badge.svelte';
 export const badgeVariants = tv({
-    base: 'inline-flex select-none items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+    base: 'focus:ring-ring inline-flex select-none items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
     variants: {
         variant: {
-            default: 'border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
-            secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-            destructive: 'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+            default: 'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent shadow',
+            secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent',
+            destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/80 border-transparent shadow',
             outline: 'text-foreground'
         }
     },
