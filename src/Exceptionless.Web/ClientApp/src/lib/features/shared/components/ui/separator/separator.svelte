c2306f4a485e004414d7185a1cae1104563f189b
Blake Niemyjski <bniemyjski@gmail.com>
2024-09-10 08:31:13 -0500

    Updated shadcn

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/separator/separator.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/separator/separator.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <SeparatorPrimitive.Root
-    class={cn('shrink-0 bg-border', orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]', className)}
+    class={cn('shrink-0 bg-border', orientation === 'horizontal' ? 'h-[1px] w-full' : 'min-h-full w-[1px]', className)}
     {orientation}
     {decorative}
     {...$$restProps}
