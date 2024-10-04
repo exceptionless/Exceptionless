f07a1ac1bbaacc51d9511766c9895422709d73e1
Blake Niemyjski <bniemyjski@gmail.com>
2024-10-04 17:36:09 -0500

    Added user validation tests and tweaked existing validator

--- src/Exceptionless.Core/Validation/UserValidator.cs
+++ src/Exceptionless.Core/Validation/UserValidator.cs
@@ -8,9 +8,11 @@ public class UserValidator : AbstractValidator<User>
     public UserValidator()
     {
         RuleFor(u => u.FullName).NotEmpty().WithMessage("Please specify a valid full name.");
-        RuleFor(u => u.EmailAddress).NotEmpty().EmailAddress().WithMessage("Please specify a valid email address.");
+        RuleFor(u => u.EmailAddress).EmailAddress().WithMessage("Please specify a valid email address.");
         RuleFor(u => u.IsEmailAddressVerified).Equal(false).When(u => String.IsNullOrEmpty(u.EmailAddress)).WithMessage("An email address cannot be verified if it doesn't exist");
+        RuleFor(u => u.VerifyEmailAddressToken).Null().When(u => u.IsEmailAddressVerified).WithMessage("A verify email address token cannot be set if the email address has been verified.");
         RuleFor(u => u.VerifyEmailAddressToken).NotEmpty().When(u => !u.IsEmailAddressVerified).WithMessage("A verify email address token must be set if the email address has not been verified.");
+        RuleFor(u => u.VerifyEmailAddressTokenExpiration).Empty().When(u => u.IsEmailAddressVerified).WithMessage("A verify email address token expiration cannot be set if the email address has been verified.");
         RuleFor(u => u.VerifyEmailAddressTokenExpiration).NotEmpty().When(u => !u.IsEmailAddressVerified).WithMessage("A verify email address token expiration must be set if the email address has not been verified.");
     }
 }
