477a73afbac65222919617e0047e0dc75bfb204c
Blake Niemyjski <bniemyjski@gmail.com>
2024-10-14 14:52:52 -0500

    Added serialization attributes so we get better client gen.

--- src/Exceptionless.Web/Models/Auth/ExternalAuthInfo.cs
+++ src/Exceptionless.Web/Models/Auth/ExternalAuthInfo.cs
@@ -1,14 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace Exceptionless.Web.Models;
 
 public record ExternalAuthInfo
 {
     [Required]
+    [JsonProperty("clientId")]
     public required string ClientId { get; init; }
+
     [Required]
+    [JsonProperty("code")]
     public required string Code { get; init; }
+
     [Required]
+    [JsonProperty("redirectUri")]
     public required string RedirectUri { get; init; }
+
+    [JsonProperty("inviteToken")]
     public string? InviteToken { get; init; }
 }
