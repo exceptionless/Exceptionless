c241c7c8079b9bbc460730fb07d0914c11a9818e
Blake Niemyjski <bniemyjski@gmail.com>
2024-10-31 22:09:44 -0500

    next: allow custom styling on filter components

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/filters/ClickableStringFilter.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/filters/ClickableStringFilter.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { A, type AProps } from '$comp/typography';
+    import { cn } from '$lib/utils';
     import IconFilter from '~icons/mdi/filter';
 
     import { StringFilter } from './filters.svelte';
@@ -9,12 +10,12 @@
         term: string;
         value?: null | string;
     } & AProps;
-    let { changed, children, term, value, ...props }: Props = $props();
+    let { changed, children, class: className, term, value, ...props }: Props = $props();
 
     const title = `Search ${term}:${value}`;
 </script>
 
-<A class="cursor-pointer" onclick={() => changed(new StringFilter(term, value ?? undefined))} {title} {...props}>
+<A class={cn('cursor-pointer', className)} onclick={() => changed(new StringFilter(term, value ?? undefined))} {title} {...props}>
     {#if children}
         {@render children()}
     {:else}
