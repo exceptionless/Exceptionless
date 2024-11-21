2521b693be5e669d8031a1a3d6d67c5e50f610c9
Blake Niemyjski <bniemyjski@gmail.com>
2024-11-20 21:03:32 -0600

    Updated shadcn

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/dropdown-menu/dropdown-menu-checkbox-item.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/dropdown-menu/dropdown-menu-checkbox-item.svelte
@@ -30,9 +30,9 @@
 	{#snippet children({ checked, indeterminate })}
 		<span class="absolute left-2 flex size-3.5 items-center justify-center">
 			{#if indeterminate}
-				<Minus class="size-3.5" />
+				<Minus class="size-4" />
 			{:else}
-				<Check class={cn("size-3.5", !checked && "text-transparent")} />
+				<Check class={cn("size-4", !checked && "text-transparent")} />
 			{/if}
 		</span>
 		{@render childrenProp?.()}
