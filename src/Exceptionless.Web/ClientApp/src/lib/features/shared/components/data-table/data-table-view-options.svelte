3a55615689e97f893b8b66ad40a1a8ca5162ac29
Blake Niemyjski <bniemyjski@gmail.com>
2024-11-21 19:06:08 -0600

    next: hide column selection on mobile layouts

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/data-table/data-table-view-options.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/data-table/data-table-view-options.svelte
@@ -18,22 +18,22 @@
 
 <DropdownMenu.Root>
     <DropdownMenu.Trigger>
-        {#snippet children()}
-            <Button class="h-8" size="sm" variant="outline">
-                <IconViewColumn class="mr-2 h-4 w-4" />
-                View
-            </Button>
-        {/snippet}
+        <Button class="ml-auto hidden h-8 lg:flex" size="sm" variant="outline">
+            <IconViewColumn class="mr-2 h-4 w-4" />
+            View
+        </Button>
     </DropdownMenu.Trigger>
     <DropdownMenu.Content>
-        <DropdownMenu.Label>Toggle columns</DropdownMenu.Label>
-        <DropdownMenu.Separator />
-        {#each table.getAllLeafColumns() as column (column.id)}
-            {#if column.getCanHide()}
-                <DropdownMenu.CheckboxItem checked={column.getIsVisible()} onclick={() => column.toggleVisibility()}>
-                    {column.columnDef.header}
-                </DropdownMenu.CheckboxItem>
-            {/if}
-        {/each}
+        <DropdownMenu.Group>
+            <DropdownMenu.Label>Toggle columns</DropdownMenu.Label>
+            <DropdownMenu.Separator />
+            {#each table.getAllLeafColumns() as column (column.id)}
+                {#if column.getCanHide()}
+                    <DropdownMenu.CheckboxItem checked={column.getIsVisible()} onclick={() => column.toggleVisibility()}>
+                        {column.columnDef.header}
+                    </DropdownMenu.CheckboxItem>
+                {/if}
+            {/each}
+        </DropdownMenu.Group>
     </DropdownMenu.Content>
 </DropdownMenu.Root>
