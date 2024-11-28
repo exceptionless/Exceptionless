99b673794f283c72671771336089eafe203667c2
Blake Niemyjski <bniemyjski@gmail.com>
2024-11-27 19:26:03 -0600

    Fixed grid filtering

--- src/Exceptionless.Web/ClientApp/src/lib/features/events/components/table/EventsDataTable.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/events/components/table/EventsDataTable.svelte
@@ -2,137 +2,18 @@
     import type { Snippet } from 'svelte';
 
     import * as DataTable from '$comp/data-table';
-    import { getKeywordFilter, getOrganizationFilter, getProjectFilter, getStackFilter, type IFilter } from "$comp/filters/filters.svelte";
-    import { ChangeType, type WebSocketMessageValue } from '$features/websockets/models';
-    import { DEFAULT_LIMIT } from '$shared/api';
-    import { type FetchClientResponse, useFetchClient } from '@exceptionless/fetchclient';
-    import { createTable } from '@tanstack/svelte-table';
-    import { useEventListener } from 'runed';
-    import { debounce } from 'throttle-debounce';
+    import { type Table } from '@tanstack/svelte-table';
 
-    import type { GetEventsMode } from '../../api.svelte';
     import type { EventSummaryModel, SummaryTemplateKeys } from '../summary/index';
 
-    import { getTableContext } from './options.svelte';
-
     interface Props {
-        filter: string;
-        filters: IFilter[];
         limit: number;
-        mode?: GetEventsMode;
-        pageFilter?: string;
         rowclick?: (row: EventSummaryModel<SummaryTemplateKeys>) => void;
-        time: string;
+        table: Table<EventSummaryModel<SummaryTemplateKeys>>;
         toolbarChildren?: Snippet;
     }
 
-    let { filter, filters, limit = $bindable(DEFAULT_LIMIT), mode = 'summary', pageFilter = undefined, rowclick, time, toolbarChildren }: Props = $props();
-    const context = getTableContext<EventSummaryModel<SummaryTemplateKeys>>({ limit, mode });
-    const table = createTable(context.options);
-
-    const client = useFetchClient();
-    let response: FetchClientResponse<EventSummaryModel<SummaryTemplateKeys>[]>;
-
-    $effect(() => {
-        limit = Number(context.limit);
-        loadData();
-    });
-
-    async function loadData() {
-        if (client.loading) {
-            return;
-        }
-
-        response = await client.getJSON<EventSummaryModel<SummaryTemplateKeys>[]>('events', {
-            params: {
-                ...context.parameters,
-                filter: [pageFilter, filter].filter(Boolean).join(' '),
-                time
-            }
-        });
-
-        if (response.ok) {
-            context.data = response.data || [];
-            context.meta = response.meta;
-            table.resetRowSelection();
-        }
-    }
-    const debouncedLoadData = debounce(10000, loadData);
-
-    async function onPersistentEvent(message: WebSocketMessageValue<'PersistentEventChanged'>) {
-        const shouldRefresh = () => {
-            if (!filter) {
-                return true;
-            }
-
-            const { id, organization_id, project_id, stack_id } = message;
-            if (id) {
-                // Check to see if any records on the page match
-                if (mode === "summary" && table.options.data.some((doc) => doc.id === id)) {
-                    return true;
-                }
-
-                // This could match any kind of lucene query (even must not filtering)
-                const keywordFilter = getKeywordFilter(filters);
-                if (keywordFilter && !keywordFilter.isEmpty()) {
-                    if (keywordFilter.value!.includes(id)) {
-                        return true;
-                    }
-                }
-            }
-
-            if (stack_id) {
-                // Check to see if any records on the page match
-                if (mode !== "summary" && table.options.data.some((doc) => doc.id === stack_id)) {
-                    return true;
-                }
-
-                const stackFilter = getStackFilter(filters);
-                if (stackFilter && !stackFilter.isEmpty()) {
-                    return stackFilter.value === stack_id;
-                }
-            }
-
-            if (project_id) {
-                const projectFilter = getProjectFilter(filters);
-                if (projectFilter && !projectFilter.isEmpty()) {
-                    return projectFilter.value.includes(project_id);
-                }
-            }
-
-            if (organization_id) {
-                const organizationFilter = getOrganizationFilter(filters);
-                if (organizationFilter && !organizationFilter.isEmpty()) {
-                    return organizationFilter.value === organization_id;
-                }
-            }
-
-            return true;
-        };
-
-        switch (message.change_type) {
-            case ChangeType.Added:
-            case ChangeType.Saved:
-                if (shouldRefresh()) {
-                    await debouncedLoadData();
-                }
-
-                break;
-            case ChangeType.Removed:
-                if (shouldRefresh()) {
-                    if (message.id && mode === "summary") {
-                        table.options.data = table.options.data.filter((doc) => doc.id !== message.id);
-                    }
-
-                    await debouncedLoadData();
-                }
-
-                break;
-        }
-    }
-
-    useEventListener(document, 'refresh', async () => await loadData());
-    useEventListener(document, 'PersistentEventChanged', async (event) => await onPersistentEvent((event as CustomEvent).detail));
+    let { limit = $bindable(), rowclick, table, toolbarChildren }: Props = $props();
 </script>
 
 <DataTable.Root>
@@ -143,6 +24,6 @@
     </DataTable.Toolbar>
     <DataTable.Body {rowclick} {table}></DataTable.Body>
     <DataTable.Pagination {table}>
-        <DataTable.PageSize bind:value={context.limit} {table}></DataTable.PageSize>
+        <DataTable.PageSize bind:value={limit} {table}></DataTable.PageSize>
     </DataTable.Pagination>
 </DataTable.Root>
--- src/Exceptionless.Web/ClientApp/src/routes/(app)/+page.svelte
+++ src/Exceptionless.Web/ClientApp/src/routes/(app)/+page.svelte
@@ -8,8 +8,15 @@
     import * as Card from '$comp/ui/card';
     import * as Sheet from '$comp/ui/sheet';
     import EventsDrawer from '$features/events/components/EventsDrawer.svelte';
+    import { shouldRefreshPersistentEventChanged } from '$features/events/components/filters';
     import EventsDataTable from '$features/events/components/table/EventsDataTable.svelte';
+    import { getTableContext } from '$features/events/components/table/options.svelte';
+    import { ChangeType, type WebSocketMessageValue } from '$features/websockets/models';
     import { persisted } from '$shared/persisted.svelte';
+    import { type FetchClientResponse, useFetchClient } from '@exceptionless/fetchclient';
+    import { createTable } from '@tanstack/svelte-table';
+    import { useEventListener } from 'runed';
+    import { debounce } from 'throttle-debounce';
     import IconOpenInNew from '~icons/mdi/open-in-new';
 
     let selectedEventId: null | string = $state(null);
@@ -38,13 +45,72 @@
     function onFilterRemoved(filter?: IFilter): void {
         persistedFilters.value = filterRemoved(persistedFilters.value, defaultFilters, filter);
     }
+
+    const context = getTableContext<EventSummaryModel<SummaryTemplateKeys>>({ limit: limit.value, mode: 'summary' });
+    const table = createTable(context.options);
+
+    const client = useFetchClient();
+    let response: FetchClientResponse<EventSummaryModel<SummaryTemplateKeys>[]>;
+
+    async function loadData() {
+        if (client.loading) {
+            return;
+        }
+
+        response = await client.getJSON<EventSummaryModel<SummaryTemplateKeys>[]>('events', {
+            params: {
+                ...context.parameters,
+                filter,
+                time
+            }
+        });
+
+        if (response.ok) {
+            context.data = response.data || [];
+            context.meta = response.meta;
+            table.resetRowSelection();
+        }
+    }
+    const debouncedLoadData = debounce(10000, loadData);
+
+    async function onPersistentEvent(message: WebSocketMessageValue<'PersistentEventChanged'>) {
+        const shouldRefresh = () =>
+            shouldRefreshPersistentEventChanged(persistedFilters.value, filter, message.organization_id, message.project_id, message.stack_id, message.id);
+
+        switch (message.change_type) {
+            case ChangeType.Added:
+            case ChangeType.Saved:
+                if (shouldRefresh()) {
+                    await debouncedLoadData();
+                }
+
+                break;
+            case ChangeType.Removed:
+                if (shouldRefresh()) {
+                    if (message.id) {
+                        table.options.data = table.options.data.filter((doc) => doc.id !== message.id);
+                    }
+
+                    await debouncedLoadData();
+                }
+
+                break;
+        }
+    }
+
+    useEventListener(document, 'refresh', async () => await loadData());
+    useEventListener(document, 'PersistentEventChanged', async (event) => await onPersistentEvent((event as CustomEvent).detail));
+
+    $effect(() => {
+        loadData();
+    });
 </script>
 
 <div class="flex flex-col space-y-4">
     <Card.Root>
         <Card.Title class="p-6 pb-0 text-2xl" level={2}>Events</Card.Title>
         <Card.Content>
-            <EventsDataTable bind:limit={limit.value} filters={persistedFilters.value} {filter} {rowclick} {time}>
+            <EventsDataTable bind:limit={limit.value} {rowclick} {table}>
                 {#snippet toolbarChildren()}
                     <FacetedFilter.Root changed={onFilterChanged} {facets} remove={onFilterRemoved}></FacetedFilter.Root>
                 {/snippet}
