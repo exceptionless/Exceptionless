b7285b44d72a74d276d02e5f4d3a088603f0862f
Blake Niemyjski <bniemyjski@gmail.com>
2024-11-30 12:53:53 -0600

    next: fixed extended data and stack trace formatting

--- src/Exceptionless.Web/ClientApp/src/lib/features/events/components/SimpleStackTrace.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/events/components/SimpleStackTrace.svelte
@@ -22,5 +22,6 @@
 <pre class="whitespace-pre-wrap break-words border p-2"><Code
         ><StackTraceHeader {errors}></StackTraceHeader>{#each errors.reverse() as error, index}{#if error.stack_trace}<div class="pl-[10px]">{cleanStackTrace(
                         error.stack_trace
-                    )}</div>{#if index < errors.length - 1}<div>--- End of inner error stack trace ---</div>{/if}{/if}{/each}</Code
+                    )}</div>{#if index < errors.length - 1}<div
+                        class="text-muted-foreground">--- End of inner error stack trace ---</div>{/if}{/if}{/each}</Code
     ></pre>
