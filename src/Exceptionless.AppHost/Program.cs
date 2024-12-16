4794fc87a06e9fb15b433de7232c66798c18a50e
Eric J. Smith <eric@codesmithtools.com>
2024-12-16 17:42:39 -0600

    Update Elasticsearch

--- src/Exceptionless.AppHost/Program.cs
+++ src/Exceptionless.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var elastic = builder.AddElasticsearch("Elasticsearch", port: 9200)
-    .WithImageTag("8.16.1")
+    .WithImageTag("8.17.0")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithContainerName("Exceptionless-Elasticsearch")
     .WithDataVolume("exceptionless.data.v1")
