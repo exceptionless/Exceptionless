9a6c16a3ee4db458b9c7e1fcdd92ef03a0725b5f
Eric J. Smith <eric@codesmithtools.com>
2024-12-18 13:37:05 -0600

    Switch to using Aspire (#1784)
    * Working on migrating to aspire
    
    * Progress
    
    * Some aspire progress
    
    * Fix bad merge
    
    * Update to Aspire 9
    
    * Fix duplicate project refs
    
    * Update elasticsearch to 8.16.1
    
    * Add storage to Aspire
    
    * Update Elasticsearch
    
    * Fix tests
    
    * Revert some changes. Fix linting.
    
    * Revert more changes
    
    * Cleanup
    
    * Use the right Elasticsearch docker image
    
    * Use explicit minio version
    
    * Fixed launch setting
    
    * Removed start and stop services
    
    * Use fixed web client ports
    
    * Use S3 storage when running local
    
    * Fixed an issue where code could throw due to CurrentUser
    
    * Fix S3
    
    * [BREAKING] Remove scope prefix from bucket names and instead use scoped file storage for app scopes
    
    * Only poll queue metrics in the same process that is running the stack event count job
    
    * Reverted some of the breaking changes around storage.
    
    ---------
    
    Co-authored-by: Blake Niemyjski <bniemyjski@gmail.com>

---
+++ src/Exceptionless.AppHost/Extensions/KibanaResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Aspire.Hosting;
+
+/// <summary>
+/// A resource that represents a Kibana container.
+/// </summary>
+/// <param name="name">The name of the resource.</param>
+public class KibanaResource(string name) : ContainerResource(name)
+{
+}
