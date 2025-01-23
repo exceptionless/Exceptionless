d240ef0e7169e9dd5c2d18c9739a3751fb49032f
Blake Niemyjski <bniemyjski@gmail.com>
2025-01-22 21:22:48 -0600

    Fixed [svelte] transition_slide_display The `slide` transition does not work correctly for elements with `display: table-row` https://svelte.dev/e/transition_slide_display

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/table/table-row.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/table/table-row.svelte
@@ -10,16 +10,31 @@
 		children,
 		...restProps
 	}: WithElementRef<HTMLAttributes<HTMLTableRowElement>> = $props();
+
+    const shouldAnimate = $derived(className?.includes('only:table-row') !== true);
 </script>
 
-<tr
-	bind:this={ref}
-	class={cn(
-		"hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
-		className
-	)}
-	{...restProps}
-	transition:slide={{ delay: 0, duration: 250, axis: 'y' }}
->
-	{@render children?.()}
-</tr>
+{#if shouldAnimate}
+    <tr
+        bind:this={ref}
+        class={cn(
+            "hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+            className
+        )}
+        {...restProps}
+        transition:slide={{ delay: 0, duration: 250, axis: 'y' }}
+    >
+        {@render children?.()}
+    </tr>
+{:else}
+    <tr
+        bind:this={ref}
+        class={cn(
+            "hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+            className
+        )}
+        {...restProps}
+        >
+        {@render children?.()}
+    </tr>
+{/if}
