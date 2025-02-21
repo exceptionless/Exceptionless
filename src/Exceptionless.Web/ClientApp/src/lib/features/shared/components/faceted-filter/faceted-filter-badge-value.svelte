1c49821c99c24d0d92b43abc53a4008aeaad0a0f
Blake Niemyjski <bniemyjski@gmail.com>
2025-02-21 08:35:03 -0600

    next: tweaked filter badge sizes

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/faceted-filter/faceted-filter-badge-value.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/faceted-filter/faceted-filter-badge-value.svelte
@@ -11,10 +11,10 @@
 </script>
 
 <Badge class="rounded-sm px-1 font-normal lg:hidden" variant="secondary">
-    <span class="max-w-24 truncate">{@render children()}</span>
+    <span class="max-w-24 truncate md:max-w-48">{@render children()}</span>
 </Badge>
 <div class="hidden space-x-1 lg:flex">
     <Badge class="rounded-sm px-1 font-normal" variant="secondary">
-        <span class="max-w-60 truncate">{@render children()}</span>
+        <span class="md:max-w-120 max-w-60 truncate">{@render children()}</span>
     </Badge>
 </div>
