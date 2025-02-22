3c8e8d2958abe0a0597b2a7a1c6c71e49f2ad16e
Blake Niemyjski <bniemyjski@gmail.com>
2025-02-22 08:47:59 -0600

    Fixed unit test

--- src/Exceptionless.Web/ClientApp/e2e/index.test.ts
+++ src/Exceptionless.Web/ClientApp/e2e/index.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('default route should redirect to login page when unauthorized', async ({ page }) => {
-    await page.goto('/');
+    await page.goto('/next');
     await page.waitForURL('/next/login');
-    await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
 });
