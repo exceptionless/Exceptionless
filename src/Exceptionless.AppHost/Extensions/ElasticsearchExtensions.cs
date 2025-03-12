9c806986fc1d969c1cce009aad74f3c6749efc88
Blake Niemyjski <bniemyjski@gmail.com>
2025-03-12 06:37:51 -0500

    Upgraded Elasticsearch version to 8.17.3

--- src/Exceptionless.AppHost/Extensions/ElasticsearchExtensions.cs
+++ src/Exceptionless.AppHost/Extensions/ElasticsearchExtensions.cs
@@ -15,7 +15,7 @@ public static class ElasticsearchBuilderExtensions
     private const int KibanaPort = 5601;
 
     /// <summary>
-    /// Adds a Elasticsearch container to the application model. The default image is "docker.elastic.co/elasticsearch/elasticsearch". This version the package defaults to the 8.17.0 tag of the Elasticsearch container image
+    /// Adds a Elasticsearch container to the application model. The default image is "docker.elastic.co/elasticsearch/elasticsearch". This version the package defaults to the 8.17.3 tag of the Elasticsearch container image
     /// </summary>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
     /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
@@ -117,5 +117,5 @@ internal static class ElasticsearchContainerImageTags
     public const string Image = "exceptionless/elasticsearch";
     public const string KibanaRegistry = "docker.elastic.co";
     public const string KibanaImage = "kibana/kibana";
-    public const string Tag = "8.17.0";
+    public const string Tag = "8.17.3";
 }
