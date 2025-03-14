8012f96ecc4a9fd2b69697d0ab750729302c1bd5
Blake Niemyjski <bniemyjski@gmail.com>
2025-03-14 11:47:49 -0500

    next: updated deps

---
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/layouts/split-layout/content.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import type { HTMLAttributes } from 'svelte/elements';
+
+    import { cn } from '$lib/utils';
+
+    interface Props extends HTMLAttributes<HTMLDivElement> {
+        children: Snippet;
+        class?: string;
+    }
+
+    let { children, class: className, ...props }: Props = $props();
+</script>
+
+<div
+    class={cn(
+        "flex-1",
+        className
+    )}
+    {...props}
+>
+    {@render children()}
+</div>
---
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/layouts/split-layout/index.ts
@@ -0,0 +1,13 @@
+import Content from './content.svelte';
+import Root from './root.svelte';
+import Sidebar from './sidebar.svelte';
+
+export {
+    Content,
+    Root,
+    Sidebar,
+    //
+    Root as SplitLayout,
+    Content as SplitLayoutContent,
+    Sidebar as SplitLayoutSidebar
+};
