9358335b5b8635b94dfcb12d244c8889eeca8d94
Blake Niemyjski <bniemyjski@gmail.com>
2025-03-19 16:40:12 -0400

    next: wip added model code gen missed and added todo

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/models/index.ts
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/models/index.ts
@@ -1,3 +1,8 @@
 export { CountResult, type IAggregate, WorkInProgressResult } from '$generated/api';
 
+// TODO: Fix api code gen
+export class ValueFromBody<T> {
+    constructor(public value: T) {}
+}
+
 export * from './aggregations';
