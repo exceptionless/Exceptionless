587008273560a74869480ec63ee4315f05d64316
Blake Niemyjski <bniemyjski@gmail.com>
2025-04-13 21:49:37 -0500

    Fixed split layout menus

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/layouts/split-layout/root.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/layouts/split-layout/root.svelte
@@ -10,6 +10,6 @@
     let { children, class: className, ...props }: Props = $props();
 </script>
 
-<div class={['flex flex-col space-y-8 lg:flex-row lg:space-y-0 lg:space-x-12', className]} {...props}>
+<div class={['flex flex-col space-y-8 lg:flex-row lg:flex-nowrap lg:space-y-0 lg:space-x-12', className]} {...props}>
     {@render children()}
 </div>
