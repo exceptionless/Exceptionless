adda3d54d1e79fa87ca200d13fe8ab97999af406
Blake Niemyjski <bniemyjski@gmail.com>
2025-05-30 18:25:39 -0500

    next: updated shadcn

--- src/Exceptionless.Web/ClientApp/eslint.config.js
+++ src/Exceptionless.Web/ClientApp/eslint.config.js
@@ -1,10 +1,9 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import perfectionist from 'eslint-plugin-perfectionist';
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
@@ -43,5 +42,5 @@ export default ts.config(
             'perfectionist/sort-svelte-attributes': 'off'
         }
     },
-    storybook.configs["flat/recommended"]
+    storybook.configs['flat/recommended']
 );
