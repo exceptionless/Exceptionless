55219f4d39bc2575736b5e97f1d3bbc327f8797e
Blake Niemyjski <bniemyjski@gmail.com>
2025-06-01 21:34:00 -0500

    next: updated deps

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/logo.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/logo.svelte
@@ -9,4 +9,4 @@
 </script>
 
 <img alt="Exceptionless Logo" class={cn('mx-auto h-[100px] dark:hidden', className)} src={logo} {...props} />
-<img alt="Exceptionless Logo" class={cn('mx-auto h-[100px] hidden dark:block', className)} src={logoDark} {...props} />
+<img alt="Exceptionless Logo" class={cn('mx-auto hidden h-[100px] dark:block', className)} src={logoDark} {...props} />
