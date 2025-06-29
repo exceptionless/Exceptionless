f5e4a81f16d9a3549fb1ee68cc6f3766b6b1c7e3
Blake Niemyjski <bniemyjski@gmail.com>
2025-06-29 13:18:56 -0500

    Adds support for custom filter trigger content
    Allows custom content to be displayed within the filter trigger, rather than only the default filter icon.
    This enables more flexibility in representing different filter states or providing additional visual cues.

--- src/Exceptionless.Web/ClientApp/src/lib/features/events/components/filters/version-faceted-filter-trigger.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/events/components/filters/version-faceted-filter-trigger.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import type { Snippet } from 'svelte';
+
     import { A, type AProps } from '$comp/typography';
     import { cn } from '$lib/utils';
     import Filter from '@lucide/svelte/icons/filter';
@@ -7,14 +9,19 @@
 
     type Props = AProps & {
         changed: (filter: VersionFilter) => void;
+        children?: Snippet;
         term: string;
         value?: string;
     };
-    let { changed, class: className, term, value, ...props }: Props = $props();
+    let { changed, children, class: className, term, value, ...props }: Props = $props();
 
     const title = `Search ${term}:${value}`;
 </script>
 
 <A class={cn('cursor-pointer', className)} onclick={() => changed(new VersionFilter(term, value))} {title} {...props}>
-    <Filter class="text-muted-foreground text-opacity-50 hover:text-primary size-5" />
+    {#if children}
+        {@render children()}
+    {:else}
+        <Filter class="text-muted-foreground text-opacity-50 hover:text-primary size-5" />
+    {/if}
 </A>
