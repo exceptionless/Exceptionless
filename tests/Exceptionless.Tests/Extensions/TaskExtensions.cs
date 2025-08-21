feef570690e13dff07cd7832de47cab1758cc3a0
Blake Niemyjski <bniemyjski@gmail.com>
2025-08-20 21:26:12 -0500

    Simplifies async wait in tests
    Removes the need for TimeProvider in TaskExtensions.WaitAsync and updates related test calls.
    This simplifies the test code and relies on the default Task.Delay implementation.

--- tests/Exceptionless.Tests/Extensions/TaskExtensions.cs
+++ tests/Exceptionless.Tests/Extensions/TaskExtensions.cs
@@ -4,8 +4,8 @@ namespace Exceptionless.Tests.Extensions;
 
 public static class TaskExtensions
 {
-    public static Task WaitAsync(this AsyncCountdownEvent countdownEvent, TimeSpan timeout, TimeProvider timeProvider)
+    public static Task WaitAsync(this AsyncCountdownEvent countdownEvent, TimeSpan timeout)
     {
-        return Task.WhenAny(countdownEvent.WaitAsync(), Task.Delay(timeout, timeProvider));
+        return Task.WhenAny(countdownEvent.WaitAsync(), Task.Delay(timeout));
     }
 }
