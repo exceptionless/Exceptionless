ef7b051f9a577fb5813a003c2426fbc263db8a58
Blake Niemyjski <bniemyjski@gmail.com>
2025-08-30 14:35:15 -0500

    Adds a TODO for sidebar menu on mobile
    Adds a TODO comment to investigate using the sidebar menu items and a tab control on mobile devices, suggesting a potential improvement for the navigation experience on smaller screens.

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/navigation/nav-container.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/navigation/nav-container.svelte
@@ -7,6 +7,7 @@
     }
 
     let { children, class: className, ...props }: Props = $props();
+    // TODO: Look into having this use the sidbar menu items and a tab control on mobile.
 </script>
 
 <nav class={['flex space-x-2 lg:flex-col lg:space-y-1 lg:space-x-0', className]} {...props}>
