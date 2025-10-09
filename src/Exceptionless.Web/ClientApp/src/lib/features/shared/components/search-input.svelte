fb6e0b3af6a4de8f7fcb24d065931e65bb5a598b
Blake Niemyjski <bniemyjski@gmail.com>
2025-10-08 21:37:54 -0500

    Replaces Input with InputGroup component
    Updates the search input component to use the InputGroup component instead of the Input component.
    
    This change improves the structure and styling of the search input field.

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/search-input.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/search-input.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
     import type { HTMLInputAttributes, HTMLInputTypeAttribute } from 'svelte/elements';
 
-    import { Input } from '$comp/ui/input';
-    import { cn, type WithElementRef } from '$lib/utils.js';
+    import * as InputGroup from '$comp/ui/input-group';
+    import { type WithElementRef } from '$lib/utils';
     import Search from '@lucide/svelte/icons/search';
 
     type InputType = Exclude<HTMLInputTypeAttribute, 'file'>;
 
-    type Props = WithElementRef<Omit<HTMLInputAttributes, 'type'> & ({ files?: FileList; type: 'file' } | { files?: undefined; type?: InputType })>;
+    type BaseProps = WithElementRef<Omit<HTMLInputAttributes, 'type'> & ({ files?: FileList; type: 'file' } | { files?: undefined; type?: InputType })>;
 
-    let { class: className, id = 'search', placeholder = 'Search...', value = $bindable(), ...props }: Props = $props();
+    type Props = BaseProps & {
+        groupClass?: string;
+    };
+
+    let { class: className, groupClass = '', id = 'search', placeholder = 'Search...', value = $bindable(), ...props }: Props = $props();
 </script>
 
-<div class="relative">
-    <label class="sr-only" for={id}>Search</label>
-    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <Search class="h-5 w-5" />
-    </div>
-    <Input bind:value class={cn('pl-10', className)} {id} name="search" {placeholder} type="text" {...props} />
-</div>
+<label class="sr-only" for={id}>Search</label>
+<InputGroup.Root class={groupClass}>
+    <InputGroup.Addon aria-hidden="true">
+        <Search class="size-5" />
+    </InputGroup.Addon>
+    <InputGroup.Input bind:value class={className} {id} name="search" {placeholder} type="text" {...props} />
+</InputGroup.Root>
