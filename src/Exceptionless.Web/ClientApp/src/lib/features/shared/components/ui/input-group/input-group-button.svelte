70cb15b94d90fdd1fc32ea53c68ca56a78f3c9cd
Blake Niemyjski <bniemyjski@gmail.com>
2025-10-28 21:45:38 -0500

    Upgraded shadcn

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/input-group/input-group-button.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/ui/input-group/input-group-button.svelte
@@ -25,6 +25,7 @@
 	import { Button } from "$comp/ui/button/index.js";
 
 	let {
+		ref = $bindable(null),
 		class: className,
 		children,
 		type = "button",
@@ -37,6 +38,7 @@
 </script>
 
 <Button
+	bind:ref
 	{type}
 	data-size={size}
 	{variant}
