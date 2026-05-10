87252c1c4d537bec18dfc2e9f795d40410934c31
Eric J. Smith <eric@codesmithtools.com>
2026-05-10 17:42:13 -0500

    Remove redirect E2E test

--- src/Exceptionless.Web/ClientApp/e2e/tests/anonymous-redirect.e2e.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '../fixtures/e2e-test';
-
-test('default app route redirects anonymous users to login', async ({ page }) => {
-    await page.goto('/next');
-
-    await page.waitForURL('**/next/login');
-    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
-});
