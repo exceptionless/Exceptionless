9a1a0b03a8b8bc4f7935ad920fd5878e438518a2
Eric J. Smith <eric@codesmithtools.com>
2026-05-10 17:41:27 -0500

    Remove E2E health test

--- src/Exceptionless.Web/ClientApp/e2e/tests/anonymous-redirect.e2e.ts
+++ src/Exceptionless.Web/ClientApp/e2e/tests/anonymous-redirect.e2e.ts
@@ -1,11 +1,5 @@
 import { expect, test } from '../fixtures/e2e-test';
 
-test('api health endpoint responds', async ({ e2eApi }) => {
-    const about = await e2eApi.getAbout();
-
-    expect(about).toBeTruthy();
-});
-
 test('default app route redirects anonymous users to login', async ({ page }) => {
     await page.goto('/next');
 
