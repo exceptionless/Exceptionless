202f7c2185ef41449164f8dcab3fb6b08395b3c8
Blake Niemyjski <bniemyjski@gmail.com>
2026-07-25 20:48:06 -0500

    Harden RabbitMQ message bus configuration parsing

--- tests/Exceptionless.Tests/Configuration/MessageBusOptionsTests.cs
+++ tests/Exceptionless.Tests/Configuration/MessageBusOptionsTests.cs
@@ -1,4 +1,5 @@
 using Exceptionless.Core;
+using Exceptionless.Core.Configuration;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 
@@ -6,65 +7,80 @@ namespace Exceptionless.Tests.Configuration;
 
 public class MessageBusOptionsTests
 {
-    [Fact]
-    public void ReadFromConfiguration_WithInlineRabbitMqUri_PreservesRawConnectionString()
+    [Theory]
+    [InlineData("provider=rabbitmq;amqp://localhost/%2F", "amqp://localhost/%2F")]
+    [InlineData("provider=rabbitmq;\"amqp://localhost/%2F\"", "amqp://localhost/%2F")]
+    [InlineData("provider=rabbitmq;'amqp://localhost/%2F'", "amqp://localhost/%2F")]
+    [InlineData(
+        " PROVIDER = \"RABBITMQ\" ; 'amqps://user:p%40ss@rabbit.example.com:5671/team%2Fprod?heartbeat=30&connection_timeout=10000' ",
+        "amqps://user:p%40ss@rabbit.example.com:5671/team%2Fprod?heartbeat=30&connection_timeout=10000")]
+    public void ReadFromConfiguration_WithInlineRabbitMqUri_PreservesRawConnectionString(string configuredConnectionString, string expectedConnectionString)
     {
-        // Arrange
-        const string rabbitMqConnectionString = "amqp://localhost/%2F";
-        var configuration = CreateConfiguration(new Dictionary<string, string?>
+        var options = ReadOptions(new Dictionary<string, string?>
         {
-            ["BaseURL"] = "http://localhost:7110/#!",
-            ["ConnectionStrings:MessageBus"] = $"provider=rabbitmq;{rabbitMqConnectionString}"
+            ["ConnectionStrings:MessageBus"] = configuredConnectionString
         });
 
-        // Act
-        var options = AppOptions.ReadFromConfiguration(configuration);
-
-        // Assert
-        Assert.Equal("rabbitmq", options.MessageBusOptions.Provider);
-        Assert.Equal(rabbitMqConnectionString, options.MessageBusOptions.ConnectionString);
-    }
-
-    [Fact]
-    public void ReadFromConfiguration_WithQuotedRabbitMqUri_PreservesRawConnectionString()
-    {
-        // Arrange
-        const string rabbitMqConnectionString = "amqp://localhost/%2F";
-        var configuration = CreateConfiguration(new Dictionary<string, string?>
-        {
-            ["BaseURL"] = "http://localhost:7110/#!",
-            ["ConnectionStrings:MessageBus"] = $"provider=rabbitmq;\"{rabbitMqConnectionString}\""
-        });
-
-        // Act
-        var options = AppOptions.ReadFromConfiguration(configuration);
-
-        // Assert
-        Assert.Equal("rabbitmq", options.MessageBusOptions.Provider);
-        Assert.Equal(rabbitMqConnectionString, options.MessageBusOptions.ConnectionString);
+        Assert.Equal("rabbitmq", options.Provider);
+        Assert.Equal(expectedConnectionString, options.ConnectionString);
+        Assert.Equal(expectedConnectionString, options.Data["server"]);
     }
 
     [Theory]
-    [InlineData("amqp://localhost/%2F")]
-    [InlineData("\"amqp://localhost/%2F\"")]
-    [InlineData("'amqp://localhost/%2F'")]
-    public void ReadFromConfiguration_WithRabbitMqProviderConnectionString_PreservesRawConnectionString(string configuredConnectionString)
+    [InlineData("provider=rabbitmq", "amqp://localhost/%2F", "amqp://localhost/%2F")]
+    [InlineData("provider=rabbitmq;", "'amqp://localhost/%2F'", "amqp://localhost/%2F")]
+    [InlineData(
+        "provider=RaBbItMq",
+        "\"amqps://user:p%40ss@rabbit.example.com:5671/team%2Fprod?heartbeat=30\"",
+        "amqps://user:p%40ss@rabbit.example.com:5671/team%2Fprod?heartbeat=30")]
+    public void ReadFromConfiguration_WithNamedRabbitMqUri_PreservesRawConnectionString(string selector, string configuredConnectionString, string expectedConnectionString)
     {
-        // Arrange
-        const string rabbitMqConnectionString = "amqp://localhost/%2F";
-        var configuration = CreateConfiguration(new Dictionary<string, string?>
+        var options = ReadOptions(new Dictionary<string, string?>
         {
-            ["BaseURL"] = "http://localhost:7110/#!",
-            ["ConnectionStrings:MessageBus"] = "provider=rabbitmq",
+            ["ConnectionStrings:MessageBus"] = selector,
             ["ConnectionStrings:rabbitmq"] = configuredConnectionString
         });
 
-        // Act
-        var options = AppOptions.ReadFromConfiguration(configuration);
+        Assert.Equal("rabbitmq", options.Provider);
+        Assert.Equal(expectedConnectionString, options.ConnectionString);
+        Assert.Equal(expectedConnectionString, options.Data["server"]);
+    }
 
-        // Assert
-        Assert.Equal("rabbitmq", options.MessageBusOptions.Provider);
-        Assert.Equal(rabbitMqConnectionString, options.MessageBusOptions.ConnectionString);
+    [Fact]
+    public void ReadFromConfiguration_WithRedisProviderSettings_MergesLegacyKeyValueData()
+    {
+        var options = ReadOptions(new Dictionary<string, string?>
+        {
+            ["ConnectionStrings:MessageBus"] = "provider=redis;ssl=true",
+            ["ConnectionStrings:redis"] = "server=localhost:6379;abortConnect=false"
+        });
+
+        Assert.Equal("redis", options.Provider);
+        Assert.Equal("ssl=true;server=localhost:6379;abortConnect=false", options.ConnectionString);
+        Assert.Equal("true", options.Data["ssl"]);
+        Assert.Equal("localhost:6379", options.Data["server"]);
+        Assert.Equal("false", options.Data["abortConnect"]);
+    }
+
+    [Fact]
+    public void ReadFromConfiguration_WithInlineRedisConnectionString_PreservesLegacyFormatting()
+    {
+        var options = ReadOptions(new Dictionary<string, string?>
+        {
+            ["ConnectionStrings:MessageBus"] = "provider=redis;server=localhost:6379,abortConnect=false"
+        });
+
+        Assert.Equal("redis", options.Provider);
+        Assert.Equal("server=localhost:6379,abortConnect=false", options.ConnectionString);
+        Assert.Equal("localhost:6379,abortConnect=false", options.Data["server"]);
+    }
+
+    private static MessageBusOptions ReadOptions(Dictionary<string, string?> values)
+    {
+        var configuration = CreateConfiguration(values);
+        var appOptions = new AppOptions { AppScope = "production" };
+
+        return MessageBusOptions.ReadFromConfiguration(configuration, appOptions);
     }
 
     private static IConfiguration CreateConfiguration(Dictionary<string, string?> values)
