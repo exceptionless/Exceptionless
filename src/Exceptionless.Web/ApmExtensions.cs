6f93f3c23f3d5955b8c1c70b0dfae30ea51c56bb
Eric J. Smith <eric@codesmithtools.com>
2026-07-27 23:46:20 -0500

    Reduce OpenTelemetry ingestion volume

--- src/Exceptionless.Web/ApmExtensions.cs
+++ src/Exceptionless.Web/ApmExtensions.cs
@@ -128,6 +128,12 @@ public static partial class ApmExtensions
                 b.AddMeter("System.Runtime");
                 b.AddRuntimeInstrumentation();
                 b.AddProcessInstrumentation();
+                b.AddView(
+                    "http.server.request.duration",
+                    new ExplicitBucketHistogramConfiguration
+                    {
+                        Boundaries = [0.005, 0.025, 0.1, 0.5, 1, 5]
+                    });
 
                 if (config.Console)
                     b.AddConsoleExporter((_, metricReaderOptions) =>
