8b269f64d41a7d278e62d710fdcfd8ec2f061bbd
Blake Niemyjski <bniemyjski@gmail.com>
2026-09-15 21:37:07 -0500

    Show full timestamps in relative-time tooltips

--- src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/formatters/time-ago.svelte
+++ src/Exceptionless.Web/ClientApp/src/lib/features/shared/components/formatters/time-ago.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import * as Tooltip from '$comp/ui/tooltip';
+    import { formatDateTime } from '$shared/dates';
     import Time from 'svelte-time';
 
     interface Props {
@@ -6,6 +8,33 @@
     }
 
     let { value }: Props = $props();
+
+    const date = $derived.by(() => {
+        if (!value) {
+            return undefined;
+        }
+
+        const parsedDate = value instanceof Date ? value : new Date(value);
+        return isNaN(parsedDate.getTime()) ? undefined : parsedDate;
+    });
+
+    const fullTimestamp = $derived(date ? formatDateTime(date) : '');
 </script>
 
-<Time live={true} relative={true} timestamp={value}></Time>
+{#if date}
+    <Tooltip.Provider>
+        <Tooltip.Root>
+            <Tooltip.Trigger>
+                {#snippet child({ props })}
+                    <span
+                        {...props}
+                        class="focus-visible:ring-ring inline cursor-help rounded-sm outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
+                    >
+                        <Time live={true} relative={true} timestamp={date}></Time>
+                    </span>
+                {/snippet}
+            </Tooltip.Trigger>
+            <Tooltip.Content role="tooltip">{fullTimestamp}</Tooltip.Content>
+        </Tooltip.Root>
+    </Tooltip.Provider>
+{/if}
